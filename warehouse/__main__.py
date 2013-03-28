9cabdb6b99fa1228e42e06c88a84f10cf6b86e5e
Donald Stufft <donald@stufft.io>
2013-03-28 09:30:37 -0400

    Switch to YAML configuration with warehouse.conf.loader

--- warehouse/__main__.py
+++ warehouse/__main__.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-import os
 import sys
 
 from django.core.management import execute_from_command_line
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warehouse.settings")
     execute_from_command_line(sys.argv)
+
 
 if __name__ == "__main__":
     main()
---
+++ warehouse/conf/loader.py
@@ -0,0 +1,32 @@
+import os
+import yaml
+
+
+def load_yaml_config(conffile, environment):
+    config = {}
+
+    if conffile:
+        # Open the given YAML file with UTF-8 and safely load it
+        with open(conffile, "r", encoding="utf-8") as cfile:
+            loaded = yaml.safe_load(cfile)
+
+        # Pull out the environment specific configuration and sanitize for
+        #   UPPERCASSED attributes only.
+        config = {
+                key: value
+                for key, value in loaded.get(environment, {}).items()
+                if key == key.upper()
+            }
+
+    return config
+
+# Load the default settings
+from warehouse.conf.defaults import *
+
+# Load the configuration from WAREHOUSE_CONF
+globals().update(
+    load_yaml_config(
+        os.environ.get("WAREHOUSE_CONF", None),
+        os.environ.get("WAREHOUSE_ENV", "DEVELOPMENT"),
+    )
+)
