6e8022a61074e75938b69e79acd64025d8ee9d4f
Donald Stufft <donald@stufft.io>
2013-03-30 14:45:29 -0400

    Refactor warehouse.conf.loader to be more testable

--- warehouse/conf/loader.py
+++ warehouse/conf/loader.py
@@ -2,6 +2,19 @@ import os
 import yaml
 
 
+def envvar_settings(environ):
+    config = {}
+
+    # This is a hack to make `warehouse init` work. We need a SECRET_KEY
+    #   defined but we don't want to declare a default one. This will only
+    #   declare one if WAREHOUSE_CONF is none (E.g. we are running on strictly
+    #   default settings).
+    if "WAREHOUSE_CONF" not in environ:
+        config["SECRET_KEY"] = "INSECURE, DO NOT USE"
+
+    return config
+
+
 def load_yaml_config(conffile, environment):
     config = {}
 
@@ -23,6 +36,9 @@ def load_yaml_config(conffile, environment):
 # Load the default settings
 from warehouse.conf.defaults import *
 
+# Load configuration that depends on envars
+globals().update(envvar_settings(os.environ))
+
 # Load the configuration from WAREHOUSE_CONF
 globals().update(
     load_yaml_config(
@@ -30,10 +46,3 @@ globals().update(
         os.environ.get("WAREHOUSE_ENV", "DEVELOPMENT"),
     )
 )
-
-
-# This is a hack to make `warehouse init` work. We need a SECRET_KEY defined but
-#   we don't want to declare a default one. This will only declare one if
-#   WAREHOUSE_CONF is none (E.g. we are running on strictly default settings).
-if "WAREHOUSE_CONF" not in os.environ:
-    SECRET_KEY = "INSECURE, DO NOT USE"
