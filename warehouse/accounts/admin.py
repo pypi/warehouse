74ebcc25a70cda61368d38eb57f57e5c943c529b
Donald Stufft <donald@stufft.io>
2013-04-03 08:01:12 -0400

    Add our custom user model to the Django admin

---
+++ warehouse/accounts/admin.py
@@ -0,0 +1,33 @@
+from django.utils.translation import ugettext_lazy as _
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from warehouse.accounts.forms import UserChangeForm
+from warehouse.accounts.models import User
+
+
+class UserAdmin(BaseUserAdmin):
+
+    form = UserChangeForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["username", "email", "name", "is_staff"]
+    fieldsets = (
+        (None, {"fields": ["username", "password"]}),
+        (_("Personal info"), {"fields": ["name"]}),
+        (_("Permissions"), {"fields": [
+                                        "is_active",
+                                        "is_staff",
+                                        "is_superuser",
+                                        "groups",
+                                        "user_permissions",
+        ]}),
+        (_("Important dates"), {"fields": ["last_login", "date_joined"]}),
+    )
+    search_fields = ["username", "name", "emails__email"]
+
+
+admin.site.register(User, UserAdmin)
