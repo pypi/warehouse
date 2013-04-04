86e546570d4ae55f357708c134da28b63ccee21a
Donald Stufft <donald@stufft.io>
2013-04-03 22:17:02 -0400

    Test the User and Email models

---
+++ tests/accounts/test_models.py
@@ -0,0 +1,81 @@
+import pytest
+
+from warehouse.accounts.models import User, Email
+
+
+def test_create_user(db):
+    u = User.objects.create_user("testuser", "testpassword")
+
+    assert u
+    assert u.is_active
+    assert not u.is_staff
+    assert not u.is_superuser
+
+
+def test_create_user_no_username():
+    with pytest.raises(ValueError):
+        User.objects.create_user(None, None)
+
+
+def test_create_superuser(db):
+    su = User.objects.create_superuser("testsuperuser", "testsuperpassword")
+
+    assert su
+    assert su.is_active
+    assert su.is_staff
+    assert su.is_superuser
+
+
+@pytest.mark.parametrize(("kwargs", "expected"), [
+    ({"username": "testuser1", "name": "Test User #1"}, "Test User #1"),
+    ({"username": "testuser2"}, "testuser2"),
+])
+def test_user_fullname(kwargs, expected, db):
+    u = User.objects.create(**kwargs)
+    assert u.get_full_name() == expected
+
+
+@pytest.mark.parametrize(("kwargs", "expected"), [
+    ({"username": "testuser1", "name": "Test User #1"}, "Test User #1"),
+    ({"username": "testuser2"}, "testuser2"),
+])
+def test_user_shortname(kwargs, expected, db):
+    u = User.objects.create(**kwargs)
+    assert u.get_short_name() == expected
+
+
+def test_users_primary_email_address_verified_primary(db):
+    # Create our user
+    u = User.objects.create_user("testuser", "testpassword")
+
+    # Create several email addresses for this user
+    Email.objects.create(
+        user=u, email="test1@example.com", verified=True, primary=False)
+    Email.objects.create(
+        user=u, email="test2@example.com", verified=True, primary=True)
+
+    assert u.email == "test2@example.com"
+
+
+def test_users_primary_email_address_unverified_primary(db):
+    # Create our user
+    u = User.objects.create_user("testuser", "testpassword")
+
+    # Create several email addresses for this user
+    Email.objects.create(
+        user=u, email="test1@example.com", verified=True, primary=False)
+    Email.objects.create(
+        user=u, email="test2@example.com", verified=False, primary=True)
+
+    assert u.email is None
+
+
+def test_users_primary_email_address_no_primary(db):
+    # Create our user
+    u = User.objects.create_user("testuser", "testpassword")
+
+    # Create several email addresses for this user
+    Email.objects.create(
+        user=u, email="test1@example.com", verified=True, primary=False)
+
+    assert u.email is None
