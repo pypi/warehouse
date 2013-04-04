0e89c3023c6f7fe14508b993f998a3a076a8a599
Donald Stufft <donald@stufft.io>
2013-04-03 22:24:49 -0400

    Enable warehouse configuration via py.test cli and add defaults

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import os
+#import pytest
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("warehouse tests")
+    group.addoption(
+        "--config",
+        dest="config",
+        help="A .yaml file with config",
+    )
+    group.addoption(
+        "--environment",
+        dest="environment",
+        help="A Warehouse environment name (default: TEST)",
+        default="TEST",
+    )
+
+
+def pytest_configure(config):
+    configfile = config.getoption("config")
+    if configfile:
+        os.environ["WAREHOUSE_CONF"] = configfile
+
+    os.environ["WAREHOUSE_ENV"] = config.getoption("environment")
