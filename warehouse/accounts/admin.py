c563e6473663fc66518ca66c82f5b5922c5ad42e
Donald Stufft <donald@stufft.io>
2013-04-10 22:18:52 -0400

    Add emails to the User admin page

--- warehouse/accounts/admin.py
+++ warehouse/accounts/admin.py
@@ -4,12 +4,21 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from warehouse.accounts.forms import UserChangeForm
-from warehouse.accounts.models import User
+from warehouse.accounts.models import User, Email
+
+
+class EmailInline(admin.TabularInline):
+    model = Email
+    extra = 0
 
 
 class UserAdmin(BaseUserAdmin):
 
     form = UserChangeForm
+
+    inlines = [
+        EmailInline,
+    ]
 
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
