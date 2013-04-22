df1e4b5002c51fb475c9ea7e47d02a701ff539f0
Donald Stufft <donald@stufft.io>
2013-04-21 21:33:05 -0400

    Use invoke to enable running different test "suites"

---
+++ tasks.py
@@ -0,0 +1,53 @@
+import sys
+
+import coverage as _coverage
+import invoke
+import pytest
+
+
+@invoke.task
+def tests(suite=None, coverage=None, pdb=False):
+    if suite is None:
+        coverage = True
+        markers = []
+    elif suite == "unit":
+        markers = ["unit"]
+    elif suite == "functional":
+        markers = ["functional"]
+    elif suite == "coverage":
+        coverage = True
+        markers = ["unit"]
+    else:
+        raise ValueError("Invalid name for suite. Must be one of unit, "
+                                                        "functional, coverage")
+
+    args = []
+
+    # Add markers to the arguments
+    if markers:
+        args += ["-m", " and ".join(markers)]
+
+    # Add coverage to the arguments
+    if coverage:
+        args += ["--cov", "warehouse"]
+
+    # Add pdb to the arguments
+    if pdb:
+        args += ["--pdb"]
+
+    exitcode = pytest.main(args)
+    if exitcode:
+        sys.exit(exitcode)
+
+    if suite == "coverage":
+        # When testing for coverage we want to fail the test run if we do not
+        #   have 100% coverage.
+        cov = _coverage.coverage(config_file="coverage.cfg")
+        cov.load()
+
+        with open("/dev/null", "w") as devnull:
+            covered = cov.report(file=devnull)
+
+        if int(covered) < 100:
+            print("")
+            sys.exit("[FAILED] Coverage is less than 100%")
