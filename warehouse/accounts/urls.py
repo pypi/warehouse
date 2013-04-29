a6990bbd34fa583fbea20861da2b7b259f5a1f8e
Donald Stufft <donald@stufft.io>
2013-04-28 21:54:15 -0400

    Add the ability for a user to log in

--- warehouse/accounts/urls.py
+++ warehouse/accounts/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from warehouse.accounts import views
 
 urlpatterns = patterns("",
+    url(r"^login/$", views.LoginView.as_view(), name="accounts.login"),
     url(r"^signup/$", views.signup, name="accounts.signup"),
 )
