2a85bf4a6a6a650920367ba7d7757a42f858ec70
Donald Stufft <donald@stufft.io>
2013-04-29 12:29:48 -0400

    Ensure that inactive users cannot log in

--- tests/functional/accounts/test_login.py
+++ tests/functional/accounts/test_login.py
@@ -43,6 +43,23 @@ def test_login_invalid_user(webtest):
 
 
 @pytest.mark.django_db(transaction=True)
+def test_login_inactive_user(webtest):
+    u = User.objects.create_user(username="testuser", password="password!")
+    u.is_active = False
+    u.save()
+
+    form = webtest.get(reverse("accounts.login") + "?next=/").form
+    form["username"] = "testuser"
+    form["password"] = "password!"
+    response = form.submit()
+
+    pq = response.pyquery
+    assert response.status_code == 200
+    assert pq("div.alert")[0].text == ("This account is inactive or has been "
+                                        "locked by an administrator.")
+
+
+@pytest.mark.django_db(transaction=True)
 def test_login_already_logged_in(webtest):
     response = webtest.get(reverse("accounts.login"), user="testuser")
     assert response.status_code == 303
