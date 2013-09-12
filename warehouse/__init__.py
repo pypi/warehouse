566ad4c77bfb8c6c5d4ba79fcf2b787b537a56a2
Donald Stufft <donald@stufft.io>
2013-09-12 03:01:35 -0400

    Import from __about__ in warehouse.__init__

--- warehouse/__init__.py
+++ warehouse/__init__.py
@@ -16,14 +16,7 @@ __all__ = [
     "__email__", "__license__", "__copyright__",
 ]
 
-__title__ = "warehouse"
-__summary__ = "Next Generation Python Package Index"
-__uri__ = "https://github.com/dstufft/warehouse"
-
-__version__ = "0.1dev1"
-
-__author__ = "Donald Stufft"
-__email__ = "donald@stufft.io"
-
-__license__ = "Apache License, Version 2.0"
-__copyright__ = "Copyright 2013 Donald Stufft"
+from warehouse.__about__ import (
+    __title__, __summary__, __uri__, __version__, __author__, __email__,
+    __license__, __copyright__,
+)
