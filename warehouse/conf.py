220e6df7966acbeff6ff028e9cb4159d54470080
Donald Stufft <donald@stufft.io>
2013-09-17 18:06:52 -0400

    Trim down the list of hashers to only the secure ones

--- warehouse/conf.py
+++ warehouse/conf.py
@@ -49,10 +49,6 @@ class Settings(BaseSettings):
         "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
         "django.contrib.auth.hashers.BCryptPasswordHasher",
         "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-        "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-        "django.contrib.auth.hashers.SHA1PasswordHasher",
-        "django.contrib.auth.hashers.MD5PasswordHasher",
-        "django.contrib.auth.hashers.CryptPasswordHasher",
     ]
 
     ROOT_URLCONF = "warehouse.urls"
