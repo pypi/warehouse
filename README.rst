78672568535b99239913a5b344e7016bb893cc71
Donald Stufft <donald@stufft.io>
2013-09-22 12:03:46 -0400

    Initial packaging

---
+++ README.rst
@@ -0,0 +1,8 @@
+Warehouse
+=========
+
+.. image:: https://travis-ci.org/dstufft/warehouse.png?branch=master
+   :target: https://travis-ci.org/dstufft/warehouse
+
+.. image:: https://coveralls.io/repos/dstufft/warehouse/badge.png?branch=master
+   :target: https://coveralls.io/r/dstufft/warehouse?branch=master
---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Copyright 2013 Donald Stufft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import absolute_import, division, print_function
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+
+about = {}
+with open("warehouse/__about__.py") as fp:
+    exec(fp.read(), about)
+
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+
+    description=about["__summary__"],
+    long_description=open("README.rst").read(),
+    license=about["__license__"],
+    url=about["__uri__"],
+
+    author=about["__author__"],
+    author_email=about["__email__"],
+
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+
+    packages=find_packages(exclude=["tests", "tests.*"]),
+)
