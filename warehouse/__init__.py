b11ad52eab62859bab1499cee97992e1fed6a477
Donald Stufft <donald@stufft.io>
2013-09-23 20:41:30 -0400

    Expose the values from __about__ in the warehouse namespace

--- warehouse/__init__.py
+++ warehouse/__init__.py
@@ -13,3 +13,13 @@
 # limitations under the License.
 from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
+
+from warehouse.__about__ import (
+    __title__, __summary__, __uri__, __version__, __author__, __email__,
+    __license__, __copyright__,
+)
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
