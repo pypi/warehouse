4b117ac020d3c99e6e0a00bd5ab3ad2581adebd7
Donald Stufft <donald@stufft.io>
2013-10-02 13:45:24 -0400

    Enable passing the database into tests and a flag for verbosity

--- tasks.py
+++ tasks.py
@@ -27,8 +27,13 @@ def _out(name, message):
 
 
 @invoke.task(name="test")
-def release_test(**kwargs):
+def release_test(database=None, verbose=False, **kwargs):
     out = functools.partial(_out, "release.test")
+
+    hide = "out" if not verbose else None
+
+    if database:
+        os.environ["WAREHOUSE_DATABASE_URL"] = database
 
     out("Creating a temporary directory to export Warehouse to")
     curdir = os.getcwd()
@@ -37,34 +42,36 @@ def release_test(**kwargs):
 
     try:
         invoke.run("git checkout-index -f -a --prefix={}".format(tmpdir),
-            hide="out",
+            hide=hide,
         )
         os.chdir(tmpdir)
 
         # Run all our various tests one last time before
-        envs = set(invoke.run("tox -l", hide="out").stdout.split())
+        envs = set(invoke.run("tox -l", hide=hide).stdout.split())
         envs -= {"packaging"}  # Packaging tests require a git clone
         out("Running tests: {}".format(", ".join(envs)))
 
         for env in envs:
             out("Running the {} tests".format(env))
-            invoke.run("tox -e {}".format(env), hide="out")
+            invoke.run("tox -e {}".format(env), hide=hide)
     finally:
         os.chdir(curdir)
         shutil.rmtree(tmpdir, ignore_errors=True)
 
 
 @invoke.task(name="build")
-def release_build(**kwargs):
+def release_build(verbose=False, **kwargs):
     """
     Builds the source distribution.
     """
     out = functools.partial(_out, "release.build")
 
+    hide = "out" if not verbose else None
+
     # Determine the next version number using git tags
     version_series = datetime.datetime.utcnow().strftime("%y.%m")
     version_series = ".".join([str(int(x)) for x in version_series.split(".")])
-    tags = invoke.run("git tag -l 'v{}.*'".format(version_series), hide="out")
+    tags = invoke.run("git tag -l 'v{}.*'".format(version_series), hide=hide)
     versions = sorted(tags.stdout.split())
     version_num = int(versions[-1].rsplit(".")[-1]) + 1 if versions else 0
     version = ".".join([version_series, str(version_num)])
@@ -72,7 +79,7 @@ def release_build(**kwargs):
     out("New release will be version {}".format(version))
 
     # Determine our build number (It's equal to our current git revision)
-    build_tag = invoke.run("git rev-parse HEAD", hide="out").stdout[:7]
+    build_tag = invoke.run("git rev-parse HEAD", hide=hide).stdout[:7]
     out("Using build tag: '{}'".format(build_tag))
 
     # Create a template for the warehouse/__about__.py that we'll use
@@ -126,10 +133,10 @@ def release_build(**kwargs):
             version=version,
             build=build_tag,
         ).lstrip())
-    invoke.run("git add warehouse/__about__.py", hide="out")
+    invoke.run("git add warehouse/__about__.py", hide=hide)
     invoke.run("git commit -m 'Generate the release __about__.py (version={} "
                "build={})'".format(version, build_tag),
-        hide="out",
+        hide=hide,
     )
     out("warehouse/__about__.py generated with release values")
 
@@ -137,7 +144,7 @@ def release_build(**kwargs):
     invoke.run("git tag -s -m 'Released version v{0} ({1})' v{0}".format(
         version,
         build_tag,
-    ), hide=True)
+    ), hide=True if not verbose else None)
 
     out("Creating a temporary directory to export Warehouse to")
     curdir = os.getcwd()
@@ -146,19 +153,25 @@ def release_build(**kwargs):
     try:
         # Store our current branch
         current_branch = invoke.run("git rev-parse --abbrev-ref HEAD",
-            hide="out",
+            hide=hide,
         ).stdout.strip()
 
         # Checkout our tag
-        invoke.run("git checkout v{}".format(version), hide=True)
+        invoke.run(
+            "git checkout v{}".format(version),
+            hide=True if not verbose else None,
+        )
 
         # Export our repository into a temporary directory
         invoke.run("git checkout-index -f -a --prefix={}".format(tmpdir),
-            hide="out",
+            hide=hide,
         )
 
         # Switch back to our original branch
-        invoke.run("git checkout {}".format(current_branch), hide=True)
+        invoke.run(
+            "git checkout {}".format(current_branch),
+            hide=True if not verbose else None,
+        )
 
         # Change to the temporary directory
         os.chdir(tmpdir)
@@ -167,7 +180,7 @@ def release_build(**kwargs):
         os.makedirs("dist")
         for dist_type in ["sdist", "bdist_wheel"]:
             filenames = set(os.listdir("dist"))
-            invoke.run("python setup.py {}".format(dist_type), hide="out")
+            invoke.run("python setup.py {}".format(dist_type), hide=hide)
             filename = (set(os.listdir("dist")) - filenames).pop()
             out("Generated {} ({})".format(dist_type, filename))
 
@@ -194,25 +207,25 @@ def release_build(**kwargs):
             version=next_version,
             build=build_tag,
         ).lstrip())
-    invoke.run("git add warehouse/__about__.py", hide="out")
+    invoke.run("git add warehouse/__about__.py", hide=hide)
     invoke.run(
         "git commit -m 'Generate the development __about__.py'".format(
             next_version,
             build_tag,
         ),
-        hide="out",
+        hide=hide,
     )
     out("warehouse/__about__.py generated with development values")
 
 
 @invoke.task(name="upload")
-def release_upload(repository=None, **kwargs):
+def release_upload(repository=None, verbose=False, **kwargs):
     out = functools.partial(_out, "release.upload")
 
     out("Uploading to {}".format("PyPI" if repository is None else repository))
     invoke.run("twine upload --sign{} dist/*".format(
         "" if repository is None else " -r {}".format(repository)
-    ), hide="both")
+    ), hide="both" if not verbose else None)
     out("Uploaded.")
 
 
@@ -221,7 +234,7 @@ def release_upload(repository=None, **kwargs):
     name="all",
     pre=["release.test", "release.build", "release.upload"],
 )
-def release_all(repository=None, **kwargs):
+def release_all(repository=None, database=None, verbose=False, **kwargs):
     pass
 
 
