b8744cfec65fac75971cd13f437db5e5ba06336b
Donald Stufft <donald@stufft.io>
2013-10-21 01:30:19 -0400

    Generate the development __about__.py

--- warehouse/__about__.py
+++ warehouse/__about__.py
@@ -26,8 +26,8 @@ __title__ = "warehouse"
 __summary__ = "Next Generation Python Package Repository"
 __uri__ = "https://github.com/dstufft/warehouse"
 
-__version__ = "13.10.8"
-__build__ = "3e66e10"
+__version__ = "13.10.9.dev0"
+__build__ = "<development>"
 
 __author__ = "Donald Stufft"
 __email__ = "donald@stufft.io"
