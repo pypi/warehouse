e8134cad2c1687d5dcdeb62c11d35e26d313344a
Donald Stufft <donald@stufft.io>
2013-11-06 07:07:26 -0500

    Use relative urls

--- compass.rb
+++ compass.rb
@@ -7,5 +7,6 @@ javascripts_dir = "warehouse/static/warehouse/js"
 fonts_dir = "warehouse/static/warehouse/fonts"
 
 # Output options.
+relative_assets = true
 line_comments = false
 output_style = :compact
