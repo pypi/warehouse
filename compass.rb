0220389482378e42e4e93a0ba9098022f0fb9341
Donald Stufft <donald@stufft.io>
2013-11-06 06:38:37 -0500

    Switch the assets to use compass instead of less

---
+++ compass.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+
+css_dir = "warehouse/static/warehouse/css"
+sass_dir = "warehouse/static/warehouse/sass"
+images_dir = "warehouse/static/warehouse/images"
+javascripts_dir = "warehouse/static/warehouse/js"
+fonts_dir = "warehouse/static/warehouse/fonts"
+
+# Output options.
+line_comments = false
+output_style = :compact
