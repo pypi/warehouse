5ff33bdef64ab3d27cfd9b7394ecb7bc4411b35d
Jenkins <jenkins@apt.psf.io>
2013-11-10 00:53:41 +0000

    Generate the release (version=13.11.4 build=0bb914c)

--- warehouse/__about__.py
+++ warehouse/__about__.py
@@ -26,8 +26,8 @@ __title__ = "warehouse"
 __summary__ = "Next Generation Python Package Repository"
 __uri__ = "https://github.com/pypa/warehouse"
 
-__version__ = "13.11.4.dev0"
-__build__ = "<development>"
+__version__ = "13.11.4"
+__build__ = "0bb914c"
 
 __author__ = "Donald Stufft"
 __email__ = "donald@stufft.io"
