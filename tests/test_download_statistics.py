5bcec4503014e703233e75d0fee493388e45216c
Alex Gaynor <alex.gaynor@gmail.com>
2013-12-09 15:58:53 -0800

    [WIP] initial code for parsing user agents

---
+++ tests/test_download_statistics.py
@@ -0,0 +1,127 @@
+# Copyright 2013 Donald Stufft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
+import pytest
+
+from warehouse.download_statistics import ParsedUserAgent, parse_useragent
+
+
+class TestParsing(object):
+    @pytest.mark.parametrize(("ua", "expected"), [
+        (
+            "Python-urllib/2.7 setuptools/2.0",
+            ParsedUserAgent(
+                python_version="2.7",
+                python_release=None,
+                python_type=None,
+
+                installer_type="setuptools",
+                installer_version="2.0",
+
+                operating_system=None,
+                operating_system_version=None,
+            )
+        ),
+        (
+            "Python-urllib/2.6 distribute/0.6.10",
+            ParsedUserAgent(
+                python_version="2.6",
+                python_release=None,
+                python_type=None,
+
+                installer_type="distribute",
+                installer_version="0.6.10",
+
+                operating_system=None,
+                operating_system_version=None,
+            )
+        ),
+        (
+            "Python-urllib/2.7",
+            ParsedUserAgent(
+                python_version="2.7",
+                python_release=None,
+                python_type=None,
+
+                installer_type="pip",
+                installer_version=None,
+
+                operating_system=None,
+                operating_system_version=None,
+            )
+        ),
+        (
+            "pip/1.4.1 CPython/2.7.6 Darwin/12.5.0",
+            ParsedUserAgent(
+                python_version="2.7.6",
+                python_release=None,
+                python_type="cpython",
+
+                installer_type="pip",
+                installer_version="1.4.1",
+
+                operating_system="Darwin",
+                operating_system_version="12.5.0",
+            )
+        ),
+        (
+            "pip/1.5rc1 PyPy/2.2.1 Linux/2.6.32-042stab061.2",
+            ParsedUserAgent(
+                python_version="2.7.3",
+                python_release="2.2.1",
+                python_type="pypy",
+
+                installer_type="pip",
+                installer_version="1.5rc1",
+
+                operating_system="Linux",
+                operating_system_version="2.6.32-042stab061.2",
+            )
+        ),
+        (
+            "bandersnatch/1.1 (CPython 2.7.3-final0, Linux 3.8.0-31-generic x86_64)",
+            ParsedUserAgent(
+                python_version="2.7.3-final0",
+                python_release=None,
+                python_type="cpython",
+
+                installer_type="bandersnatch",
+                installer_version="1.1",
+
+                operating_system="Linux",
+                operating_system_version="3.8.0-31-generic x86_64",
+            )
+        ),
+        (
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8)",
+            ParsedUserAgent(
+                python_version=None,
+                python_release=None,
+                python_type=None,
+
+                installer_type="browser",
+                installer_version=None,
+
+                operating_system=None,
+                operating_system_version=None,
+            )
+
+        )
+    ])
+    def test_parse_useragent(self, ua, expected):
+        assert parse_useragent(ua) == expected
---
+++ warehouse/download_statistics.py
@@ -0,0 +1,98 @@
+# Copyright 2013 Donald Stufft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
+import json
+import logging
+import re
+from collections import namedtuple
+
+
+logger = logging.getLogger(__name__)
+
+ParsedUserAgent = namedtuple("ParsedUserAgent", [
+    "python_type",
+    "python_release",
+    "python_version",
+
+    "installer_type",
+    "installer_version",
+
+    "operating_system",
+    "operating_system_version",
+])
+
+PYTHON_IMPL_RELEASE_TO_VERSION = {
+    ("pypy", "2.2.1"): "2.7.3",
+}
+
+def parse_useragent(ua):
+    python_type = None
+    python_version = None
+    python_release = None
+    installer_type = None
+    installer_version = None
+    operating_system = None
+    operating_system_version = None
+
+    if ua.startswith("pip/"):
+        pip_part, python_part, system_part = ua.split(" ")
+        installer_type, installer_version = pip_part.split("/")
+        python_type, python_release = python_part.split("/")
+        operating_system, operating_system_version = system_part.split("/")
+    if "setuptools/" in ua or "distribute/" in ua:
+        urllib_parse, installer_part = ua.split(" ")
+        _, python_version = urllib_parse.split("/")
+        installer_type, installer_version = installer_part.split("/")
+    elif ua.startswith("Python-urllib"):
+        _, python_version = ua.split("/")
+        # Probably, technically it could just be a random urllib user
+        installer_type = "pip"
+    elif ua.startswith("bandersnatch"):
+        bander_part, rest = ua.split(" ", 1)
+        installer_type, installer_version = bander_part.split("/")
+        python_type, python_version, operating_system, operating_system_version = re.match(r"\((.*?) (.*?), (.*?) (.*?)\)", rest).groups()
+    elif "Mozilla" in ua:
+        installer_type = "browser"
+    else:
+        logger.info(json.dumps({
+            "event": "download_statitics.parse_useragent.ignore",
+            "user_agent": ua,
+        }))
+
+    if python_type is not None:
+        python_type = python_type.lower()
+    if python_type == "cpython" and python_release is not None:
+        python_version = python_release
+        python_release = None
+    if python_version is None:
+        python_version = PYTHON_IMPL_RELEASE_TO_VERSION.get(
+            (python_type, python_release)
+        )
+
+    return ParsedUserAgent(
+        python_type=python_type,
+        python_release=python_release,
+        python_version=python_version,
+
+        installer_type=installer_type,
+        installer_version=installer_version,
+
+        operating_system=operating_system,
+        operating_system_version=operating_system_version,
+    )
+
