0a707498dff188becf4624b087937a79ba4e59d8
Donald Stufft <donald@stufft.io>
2014-03-06 09:57:24 -0500

    Use the new deterministic gzip support in whitenoie 0.12

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ install_requires = [
     "SQLAlchemy",
     "sqlalchemy-citext>=1.2.0",
     "Werkzeug",
-    "whitenoise>=0.11",
+    "whitenoise>=0.12",
 ]
 
 if platform.python_implementation() == "PyPy":
