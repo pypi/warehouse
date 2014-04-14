ee0844f01b09bb55a6cbf23fcf285e7b6f35496e
Alex Grönholm <alex.gronholm@nextday.fi>
2014-04-14 14:41:06 -0400

    Replaced the uses of "mock" with the standard library's unittest.mock

--- tests/test_application.py
+++ tests/test_application.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from unittest import mock
 import os.path
 
 import guard
 import importlib
-import mock
 import pretend
 import pytest
 
