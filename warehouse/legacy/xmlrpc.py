d68bad79e5b6befa62469740a49ef81814a1d7f1
Donald Stufft <donald@stufft.io>
2014-04-14 20:27:46 -0400

    Remove no longer needed compatability code

--- warehouse/legacy/xmlrpc.py
+++ warehouse/legacy/xmlrpc.py
@@ -12,11 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from xmlrpc.server import SimpleXMLRPCDispatcher
+
 import arrow
 
 from werkzeug.exceptions import BadRequest
 
-from warehouse.compat import SimpleXMLRPCDispatcher
 from warehouse.http import Response
 
 
