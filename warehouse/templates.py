c1fde7b43d40ce459794fc31499f6d1e12c321b4
Donald Stufft <donald@stufft.io>
2014-04-16 15:22:06 -0400

    Document warehouse.templates to make it more obvious what is happening

--- warehouse/templates.py
+++ warehouse/templates.py
@@ -19,6 +19,22 @@ from warehouse.http import Response
 
 
 class TemplateResponse(Response):
+    """
+    TemplateResponse is small subclass of Response which will lazily render
+    a template when the response attribute is accessed.
+
+    This allows inspecting or even modifying the context of the template, or
+    even the template itself, at anytime prior to the template finally being
+    rendered. Specifically it allows tests to easily test that a view has
+    rendered a particular template with a particular context, but it also
+    additionally allows decorators to modify the response without having to
+    resort to parsing HTML.
+
+    It also allows you to conceptually separate a ``default_context`` from the
+    actual ``context``. This distinction is useful because ``render_response``
+    always adds some default context to the template but that isn't useful for
+    testing or modification of what the view itself has done.
+    """
 
     def __init__(self, template, context, *args, default_context=None,
                  **kwargs):
@@ -30,6 +46,12 @@ class TemplateResponse(Response):
 
     @property
     def response(self):
+        """
+        The actual content of the response. The first time this is accessed
+        we'll render the template and set are attributes to None so that
+        it cannot be mistakenly attempted to modify after it has already been
+        rendered.
+        """
         if not hasattr(self, "_response"):
             ctx = self.default_context.copy()
             ctx.update(self.context)
@@ -43,11 +65,20 @@ class TemplateResponse(Response):
 
     @response.setter
     def response(self, value):
+        """
+        If something sets a positive value to the response, go ahead and allow
+        that to replace our lazily evaluated response.
+        """
         if value:
             self._response = value
 
 
 def render_response(app, request, template, **context):
+    """
+    A simple helper that takes an app, request, template, and some context and
+    constructs a TemplateResponse that will lazily render the template with
+    the given context when the Response is evaluated.
+    """
     template = app.templates.get_template(template)
 
     default_context = {
