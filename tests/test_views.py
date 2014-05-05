4f6228f8afd847e3abfc3527e55088baa458c45a
Donald Stufft <donald@stufft.io>
2014-05-05 19:24:13 -0400

    Convert tests.test_views to use database generators

--- tests/test_views.py
+++ tests/test_views.py
@@ -16,34 +16,76 @@ import pretend
 
 from warehouse.views import index
 
+from .lib.db import ProjectFactory, ReleaseFactory, ReleaseFileFactory
 
-def test_index(app):
-    project_count = pretend.stub()
-    download_count = pretend.stub()
-    updated = pretend.stub()
 
-    app.db = pretend.stub(
-        packaging=pretend.stub(
-            get_project_count=pretend.call_recorder(
-                lambda: project_count,
-            ),
-            get_download_count=pretend.call_recorder(
-                lambda: download_count,
-            ),
-            get_recently_updated=pretend.call_recorder(lambda: updated),
-        ),
+def unique(iterable, key=None):
+    seen = set()
+    for item in iterable:
+        k = key(item) if key is not None else item
+        if k not in seen:
+            seen.add(k)
+            yield item
+
+
+def test_index(dbapp):
+    project1 = ProjectFactory.create(engine=dbapp.engine)
+    project2 = ProjectFactory.create(engine=dbapp.engine)
+
+    project1_release1 = ReleaseFactory.create(
+        name=project1["name"],
+        engine=dbapp.engine,
+    )
+    project1_release2 = ReleaseFactory.create(
+        name=project1["name"],
+        engine=dbapp.engine,
+    )
+    project2_release1 = ReleaseFactory.create(
+        name=project2["name"],
+        engine=dbapp.engine,
+    )
+
+    project1_release1_file1 = ReleaseFileFactory.create(
+        name=project1["name"],
+        version=project1_release1["version"],
+        engine=dbapp.engine,
+    )
+    project1_release2_file1 = ReleaseFileFactory.create(
+        name=project1["name"],
+        version=project1_release2["version"],
+        engine=dbapp.engine,
+    )
+    project2_release1_file1 = ReleaseFileFactory.create(
+        name=project2["name"],
+        version=project2_release1["version"],
+        engine=dbapp.engine,
     )
 
     request = pretend.stub()
 
-    resp = index(app, request)
+    resp = index(dbapp, request)
 
     assert resp.response.template.name == "index.html"
     assert resp.response.context == {
-        "project_count": project_count,
-        "download_count": download_count,
-        "recently_updated": updated,
+        "project_count": 2,
+        "download_count": sum(
+            x["downloads"]
+            for x in [
+                project1_release1_file1,
+                project1_release2_file1,
+                project2_release1_file1,
+            ]
+        ),
+        "recently_updated": list(unique(
+            sorted(
+                [
+                    project1_release1,
+                    project1_release2,
+                    project2_release1,
+                ],
+                key=lambda r: r["created"],
+                reverse=True,
+            ),
+            key=lambda r: r["name"],
+        )),
     }
-    assert app.db.packaging.get_project_count.calls == [pretend.call()]
-    assert app.db.packaging.get_download_count.calls == [pretend.call()]
-    assert app.db.packaging.get_recently_updated.calls == [pretend.call()]
