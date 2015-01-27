de538ea282c948bbde30216b42786c7122c8581e
Donald Stufft <donald@stufft.io>
2015-01-27 08:23:01 -0500

    Fix style issues

--- warehouse/__init__.py
+++ warehouse/__init__.py
@@ -11,12 +11,12 @@
 # limitations under the License.
 
 from warehouse.__about__ import (
-    __title__, __summary__, __uri__, __version__, __author__, __email__,
-    __license__, __copyright__,
+    __author__, __copyright__, __email__, __license__, __summary__, __title__,
+    __uri__, __version__,
 )
 
 
 __all__ = [
-    "__title__", "__summary__", "__uri__", "__version__", "__author__",
-    "__email__", "__license__", "__copyright__",
+    "__author__", "__copyright__", "__email__", "__license__", "__summary__",
+    "__title__", "__uri__", "__version__",
 ]
