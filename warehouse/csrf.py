2a9f509585cfb0fbea5bbf5f0265361a9c223c08
Donald Stufft <donald@stufft.io>
2015-02-09 11:13:27 -0500

    Refactor the CSRF check into it's own function

--- warehouse/csrf.py
+++ warehouse/csrf.py
@@ -57,6 +57,66 @@ def csrf_protect(view_or_scope):
         return inner
 
 
+def _check_csrf(request):
+    # Assume that anything not defined as 'safe' by RFC2616 needs protection
+    if request.method not in {"GET", "HEAD", "OPTIONS", "TRACE"}:
+        # Determine if this request has set itself so that it should be
+        # protected against CSRF. If it has not and it's gotten one of these
+        # methods, then we want to raise an error stating that this resource
+        # does not support this method.
+        if not getattr(request, "_process_csrf", None):
+            raise HTTPMethodNotAllowed
+
+        if request.scheme == "https":
+            # Determine the origin of this request
+            origin = request.headers.get("Origin")
+            if origin is None:
+                origin = request.headers.get("Referer")
+
+            # Fail if we were not able to locate an origin at all
+            if not origin:
+                raise InvalidCSRF(REASON_NO_ORIGIN)
+
+            # Parse the origin and host for comparison
+            originp = urllib.parse.urlparse(origin)
+            hostp = urllib.parse.urlparse(request.host_url)
+
+            # Actually check our Origin against our Current
+            # Host URL.
+            if ((originp.scheme, originp.hostname, originp.port)
+                    != (hostp.scheme, hostp.hostname, hostp.port)):
+                reason_origin = origin
+                if origin != "null":
+                    reason_origin = urllib.parse.urlunparse(
+                        originp[:2] + ("", "", "", ""),
+                    )
+
+                reason = REASON_BAD_ORIGIN.format(
+                    reason_origin, request.host_url,
+                )
+
+                raise InvalidCSRF(reason)
+
+        session = getattr(request, "_session", request.session)
+
+        # Get the provided CSRF token from the request.
+        request_token = request.POST.get("csrf_token", "")
+        if not request_token:
+            request_token = request.headers.get("CSRFToken", "")
+
+        # Get our CSRF token from the session, scoped or not
+        # depending on if our @csrf_protect header was registered
+        # with a scope or not.
+        scope = request._csrf_scope
+        if scope is None:
+            csrf_token = session.get_csrf_token()
+        else:
+            csrf_token = session.get_scoped_csrf_token(scope)
+
+        if not hmac.compare_digest(csrf_token, request_token):
+            raise InvalidCSRF(REASON_BAD_TOKEN)
+
+
 def csrf_mapper_factory(mapper):
     class CSRFMapper(mapper):
 
@@ -81,65 +141,8 @@ def csrf_mapper_factory(mapper):
                 if getattr(request, "_process_csrf", None):
                     innerview = add_vary("Cookie")(innerview)
 
-                # Assume that anything not defined as 'safe' by RFC2616 needs
-                # protection
-                if request.method not in {"GET", "HEAD", "OPTIONS", "TRACE"}:
-                    # Determine if this request has set itself so that it
-                    # should be protected against CSRF. If it has not and it's
-                    # gotten one of these methods, then we want to raise an
-                    # error stating that this resource does not support this
-                    # method.
-                    if not getattr(request, "_process_csrf", None):
-                        raise HTTPMethodNotAllowed
-
-                    if request.scheme == "https":
-                        # Determine the origin of this request
-                        origin = request.headers.get("Origin")
-                        if origin is None:
-                            origin = request.headers.get("Referer")
-
-                        # Fail if we were not able to locate an origin at all
-                        if not origin:
-                            raise InvalidCSRF(REASON_NO_ORIGIN)
-
-                        # Parse the origin and host for comparison
-                        originp = urllib.parse.urlparse(origin)
-                        hostp = urllib.parse.urlparse(request.host_url)
-
-                        # Actually check our Origin against our Current
-                        # Host URL.
-                        if ((originp.scheme, originp.hostname, originp.port)
-                                != (hostp.scheme, hostp.hostname, hostp.port)):
-                            reason_origin = origin
-                            if origin != "null":
-                                reason_origin = urllib.parse.urlunparse(
-                                    originp[:2] + ("", "", "", ""),
-                                )
-
-                            reason = REASON_BAD_ORIGIN.format(
-                                reason_origin, request.host_url,
-                            )
-
-                            raise InvalidCSRF(reason)
-
-                    session = getattr(request, "_session", request.session)
-
-                    # Get the provided CSRF token from the request.
-                    request_token = request.POST.get("csrf_token", "")
-                    if not request_token:
-                        request_token = request.headers.get("CSRFToken", "")
-
-                    # Get our CSRF token from the session, scoped or not
-                    # depending on if our @csrf_protect header was registered
-                    # with a scope or not.
-                    scope = request._csrf_scope
-                    if scope is None:
-                        csrf_token = session.get_csrf_token()
-                    else:
-                        csrf_token = session.get_scoped_csrf_token(scope)
-
-                    if not hmac.compare_digest(csrf_token, request_token):
-                        raise InvalidCSRF(REASON_BAD_TOKEN)
+                # Actually check our CSRF
+                _check_csrf(request)
 
                 return innerview(context, request)
 
