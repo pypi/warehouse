7eae4e8c79d5557bd93abbdba2edbd95bd93a52a
Donald Stufft <donald@stufft.io>
2015-02-19 08:44:33 -0500

    Add a filter to format numbers with SI symbols

--- warehouse/filters.py
+++ warehouse/filters.py
@@ -30,3 +30,15 @@ def readme_renderer(value, *, format):
         value = value.replace("\n", "<br>\n")
 
     return jinja2.Markup(value)
+
+
+_SI_SYMBOLS = ["k", "M", "G", "T", "P", "E", "Z", "Y"]
+
+
+def SI_symbol(value):
+    for i, symbol in enumerate(_SI_SYMBOLS):
+        magnitude = value / (1000**(i+1))
+        if magnitude >= 1 and magnitude < 1000:
+            return "{:.2g}{}".format(magnitude, symbol)
+
+    return value
