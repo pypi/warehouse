c75c659368761ee523b23f10b8c8c36a49da4c30
Donald Stufft <donald@stufft.io>
2015-03-24 21:57:57 -0400

    Add a system for doing the legacy routing based on ?:action params

---
+++ tests/legacy/test_action_routing.py
@@ -0,0 +1,61 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pretend
+import pytest
+
+from warehouse.legacy import action_routing
+
+
+@pytest.mark.parametrize(
+    ("action", "params", "expected"),
+    [
+        ("foo", {":action": "foo"}, True),
+        ("foo", {":action": "bar"}, False),
+        ("bar", {}, False),
+    ],
+)
+def test_pypi_action(action, params, expected):
+    res = action_routing.pypi_action(action)({}, pretend.stub(params=params))
+    assert res == expected
+
+
+def test_add_pypi_action_route(monkeypatch):
+    pred = pretend.stub()
+    pypi_action = pretend.call_recorder(lambda name: pred)
+    monkeypatch.setattr(action_routing, "pypi_action", pypi_action)
+
+    config = pretend.stub(
+        add_route=pretend.call_recorder(lambda *a, **k: None),
+    )
+
+    action_routing.add_pypi_action_route(config, "the name", "the action")
+
+    assert config.add_route.calls == [
+        pretend.call("the name", "/pypi", custom_predicates=[pred]),
+    ]
+
+
+def test_includeme():
+    config = pretend.stub(
+        add_directive=pretend.call_recorder(lambda name, f, action_wrap: None),
+    )
+
+    action_routing.includeme(config)
+
+    assert config.add_directive.calls == [
+        pretend.call(
+            "add_pypi_action_route",
+            action_routing.add_pypi_action_route,
+            action_wrap=False,
+        ),
+    ]
---
+++ warehouse/legacy/action_routing.py
@@ -0,0 +1,38 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def pypi_action(action):
+    def predicate(info, request):
+        if action == request.params.get(":action", None):
+            return True
+        return False
+    return predicate
+
+
+def add_pypi_action_route(config, name, action, **kwargs):
+    custom_predicates = kwargs.pop("custom_predicates", [])
+    custom_predicates += [pypi_action(action)]
+
+    config.add_route(
+        name, "/pypi",
+        custom_predicates=custom_predicates,
+        **kwargs
+    )
+
+
+def includeme(config):
+    config.add_directive(
+        "add_pypi_action_route",
+        add_pypi_action_route,
+        action_wrap=False,
+    )
