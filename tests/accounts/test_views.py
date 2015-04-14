dada61e8d62511f510863528fbc8c3a3a05c8917
Trishank Karthik Kuppusamy <trishank@nyu.edu>
2015-04-14 16:43:34 -0400

    Add test for safe URL redirect after login.

--- tests/accounts/test_views.py
+++ tests/accounts/test_views.py
@@ -161,6 +161,86 @@ class TestLogin:
         ]
         assert pyramid_request.session.new_csrf_token.calls == [pretend.call()]
 
+    @pytest.mark.parametrize("with_user", [True, False])
+    def test_post_validate_no_redirects(self, monkeypatch, pyramid_request,
+                                        with_user):
+        remember = pretend.call_recorder(
+            lambda request, user_id: [("foo", "bar")]
+        )
+        monkeypatch.setattr(views, "remember", remember)
+
+        new_session = {}
+
+        login_service = pretend.stub(
+            find_userid=pretend.call_recorder(lambda username: 1),
+        )
+        pyramid_request.find_service = pretend.call_recorder(
+            lambda iface, context: login_service
+        )
+        pyramid_request.method = "POST"
+        pyramid_request.session = pretend.stub(
+            items=lambda: [("a", "b"), ("foo", "bar")],
+            update=new_session.update,
+            invalidate=pretend.call_recorder(lambda: None),
+            new_csrf_token=pretend.call_recorder(lambda: None),
+        )
+
+        pyramid_request.set_property(
+            lambda r: 1234 if with_user else None,
+            name="unauthenticated_userid",
+        )
+
+        route_path = lambda route_name, username: "/accounts/{}/".\
+                                                  format(username)
+        # TODO: Why must I mock this request method? Why is the original
+        # route_path on the request object not already aware of routes?
+        pyramid_request.route_path = pretend.call_recorder(route_path)
+
+        username = "theuser"
+        form_obj = pretend.stub(
+            validate=pretend.call_recorder(lambda: True),
+            username=pretend.stub(data=username),
+        )
+        form_class = pretend.call_recorder(lambda d, login_service: form_obj)
+
+        # The set of all possible next URLs. Since this set is infinite, we
+        # test only a finite set of reasonable URLs.
+        nexts = ["/security/"]
+
+        for next in nexts:
+            pyramid_request.POST["next"] = next
+
+            result = views.login(pyramid_request, _form_class=form_class)
+
+            assert isinstance(result, HTTPSeeOther)
+
+            assert result.headers["Location"] == next
+            assert result.headers["foo"] == "bar"
+
+            assert form_class.calls == [
+                pretend.call(pyramid_request.POST,
+                             login_service=login_service),
+            ]
+            assert form_obj.validate.calls == [pretend.call()]
+
+            assert login_service.find_userid.calls == [pretend.call("theuser")]
+
+            if with_user:
+                assert new_session == {}
+            else:
+                assert new_session == {"a": "b", "foo": "bar"}
+
+            assert remember.calls == [pretend.call(pyramid_request, 1)]
+            assert pyramid_request.session.invalidate.calls == [pretend.call()]
+            assert pyramid_request.find_service.calls == [
+                pretend.call(ILoginService, context=None),
+            ]
+            # No call to route_path since warehouse.utils.is_safe_url(next)
+            # should be True.
+            assert pyramid_request.route_path.calls == []
+            assert pyramid_request.session.new_csrf_token.calls == \
+                [pretend.call()]
+
 
 class TestLogout:
 
