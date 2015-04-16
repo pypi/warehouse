894a3b1b2ffd515b4bae00788e67446b26da0363
Domen Kožar <domen@dev.si>
2015-04-16 18:42:25 -0400

    add a symbolic test for index view

--- tests/test_views.py
+++ tests/test_views.py
@@ -14,9 +14,31 @@ from pyramid.testing import DummyRequest
 
 from warehouse.views import index
 
+from ..common.db.packaging import (
+    ProjectFactory, ReleaseFactory, FileFactory, RoleFactory,
+)
+from ..common.db.accounts import UserFactory
+
 
 class TestIndex:
 
-    def test_index(self):
+    def test_index(self, db_request):
         request = DummyRequest()
-        assert index(request) == {}
+
+        project = ProjectFactory.create()
+        release1 = ReleaseFactory.create(project=project, version="2.0")
+        release2 = ReleaseFactory.create(project=project, version="1.0")
+        file_ = FileFactory.create(
+            release=release1,
+            filename="{}-{}.tar.gz".format(project.name, release1.version),
+            python_version="source",
+        )
+        user = UserFactory.create()
+
+        assert index(request) == {
+                                     'latest_updated_releases': [release2, release1],
+                                     'num_projects': 1,
+                                     'num_users': 1,
+                                     'num_releases': 2,
+                                     'num_files': 1,
+                                 }
