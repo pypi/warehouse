0bdebdc554392bcff9582cd1ff54a6e96bdca2ac
Domen Kožar <domen@dev.si>
2015-04-21 02:58:20 -0400

    use elasticsearch_dsl, move search logic into warehouse/search app

--- warehouse/packaging/elasticsearch.py
+++ warehouse/packaging/elasticsearch.py
@@ -1,57 +1,51 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
-Indexes sqlalchemy changes to elasticsearch. It does not handle
-db cascades so they should be avoided.
+Index sqlalchemy Session changes to ElasticSearch.
+
+It does not handle non-ORM cascades so they should be avoided.
 """
 
-import transaction
-from sqlalchemy import event
-from elasticsearch import Elasticsearch
-from pyramid.threadlocal import get_current_registry
+from elasticsearch_dsl import DocType, String, Date, Integer
 
-from warehouse.db import _Session
 from warehouse.packaging.models import Release
 
+# TODO: most basic documentation
+# TODO: add travis test for server-side cascades?
 
-release_defaults = dict(index="warehouse",
-                        doc_type="release")
 
-def handle_insert(elasticsearch_url, target):
-    es = Elasticsearch([es_url])
-    es.index(id=target.name,
-             body={"name": target.name,
-                   "version": target.version,
-                   "description": target.description,
-                   "summary": target.summary,
-                   "license": target.license,
-                   "download_url": target.download_url},
-             **release_defaults)
+class ReleaseDoc(DocType):
+    name = String(analyzer='snowball')
+    version = Integer()
+    body = String(analyzer='snowball')
+    published_from = Date()
+    # TODO: all about those fields
 
-def handle_delete(elasticsearch_url, target):
-    es = Elasticsearch([es_url])
-    es.delete(id=target.id, **release_defaults)
+    class Meta:
+        index = 'release'
+        model = Release
+
+    @classmethod
+    def from_model_instance(cls, obj):
+        return cls(
+            id=obj.name,
+            name=obj.name,
+            version=obj.version,
+            description=obj.description,
+            summary=obj.summary,
+            license=obj.license,
+            download_url=obj.download_url,
+        )
+
 
 def includeme(config):
-    """Register elasticsearch callbacks"""
-    elasticsearch_url = config.registry.settings["elasticsearch.url"]
-
-    @event.listens_for(Release, 'after_insert')
-    @event.listens_for(Release, 'after_update')
-    def release_insert_update(mapper, connection, target):
-        """Signal insert/update events for Release model"""
-        tx = transaction.get()
-        tx.addAfterCommitHook(handle_insert, args=(elasticsearch_url, target))
-
-    @event.listens_for(Release, 'before_delete')
-    def release_delete(mapper, connection, target):
-        """Signal idelete event for Release model"""
-        tx = transaction.get()
-        tx.addAfterCommitHook(handle_delete, args=(elasticsearch_url, target))
-
-    @event.listens_for(_Session, 'after_bulk_update')
-    def release_after_bulk_update(update_context):
-        import pdb;pdb.set_trace()
-
-    @event.listens_for(_Session, 'before_bulk_delete')
-    def release_after_bulk_delete(update_context):
-        """Get affected ids before they are deleted"""
-        import pdb;pdb.set_trace()
+    config.add_elasticsearch_doctype(ReleaseDoc)
---
+++ warehouse/search/views.py
@@ -0,0 +1,31 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from warehouse.packaging.elasticsearch import ReleaseDoc
+
+
+@view_config(
+    route_name="search.search",
+    renderer="search/search.html",
+)
+def search(request):
+    query = request.GET.get('q')
+    if query:
+        s = ReleaseDoc.search()
+        s = s.query('match', title=query).highlight('name').highlight('description')
+        response = s.execute()
+        return {
+            'num_results': response.hits.total,
+            'results': response,
+        }
+    else:
+        return {}
