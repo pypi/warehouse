0304d979d53c7f8dd9f7fae6d42528c4b13aa038
Donald Stufft <donald@stufft.io>
2015-10-09 17:01:37 -0400

    Switch from CPython 3.4.3 to 3.5.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
 
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
 
     packages=[
