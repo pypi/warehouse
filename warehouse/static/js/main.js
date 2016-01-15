7baf22bf3e354a758705b8899635836943014a88
Antonin Messinger <antonin.messinger@people-doc.com>
2016-01-15 22:32:42 +0100

    Refactoring vertical tab

--- warehouse/static/js/main.js
+++ warehouse/static/js/main.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
   $(".js-vertical-tab-content:first").show();
 
   /* if in tab mode */
-  $(".js-vertical-tab").click(function(event) {
+  $(".-js-vertical-tab").click(function(event) {
     event.preventDefault();
 
     $(".js-vertical-tab-content").hide();
     var activeTab = $(this).attr("rel");
     $("#"+activeTab).show();
 
-    $(".js-vertical-tab").removeClass("is-active");
+    $(".-js-vertical-tab").removeClass("is-active");
     $(this).addClass("is-active");
 
     $(".js-vertical-tab-accordion-heading").removeClass("is-active");
@@ -34,8 +34,8 @@ $(document).ready(function() {
     $(".js-vertical-tab-accordion-heading").removeClass("is-active");
     $(this).addClass("is-active");
 
-    $(".js-vertical-tab").removeClass("is-active");
-    $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
+    $(".-js-vertical-tab").removeClass("is-active");
+    $(".-js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
   });
 
   // Launch filter popover on mobile
