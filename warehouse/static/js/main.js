8cbaa978f4f643a959bde178992b6ed1fab08ff9
Dustin Ingram <di@users.noreply.github.com>
2016-02-15 23:36:57 -0500

    Link directly to subpages of a package

--- warehouse/static/js/main.js
+++ warehouse/static/js/main.js
@@ -10,37 +10,39 @@ $(document).ready(function() {
     $(this).toggleClass("expander-hidden");
   });
 
-  $(".js-vertical-tab-content").hide();
-  $(".js-vertical-tab-content:first").show();
+  function setTab(tab) {
+    if (tab) {
+      $(".js-vertical-tab-content").hide();
+      tab.show();
+      $(".is-active").removeClass("is-active");
+      $("a[href^='#"+tab[0].id+"']").addClass("is-active");
+    }
+  }
 
-  /* if in tab mode */
+  function getTab(selector) {
+    tab = $(".js-vertical-tab-content" + selector);
+    return tab.length ? tab : null;
+  }
+
+  window.onhashchange = function() {
+    setTab(getTab(location.hash));
+  };
+
+  // Set the tab if the hash is valid, otherwise show the first tab
+  setTab(getTab(location.hash) || getTab(":first"));
+
+  // If in tab mode
   $(".-js-vertical-tab").click(function(event) {
     event.preventDefault();
-
-    $(".js-vertical-tab-content").hide();
-    var activeTab = $(this).attr("rel");
-    $("#"+activeTab).show();
-
-    $(".-js-vertical-tab").removeClass("is-active");
-    $(this).addClass("is-active");
-
-    $(".-js-vertical-tab-accordion-heading").removeClass("is-active");
-    $(".-js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
+    history.pushState(null, '', $(this).attr("href"));
+    setTab(getTab(location.hash));
   });
 
-  /* if in accordion mode */
+  // If in accordion mode
   $(".-js-vertical-tab-accordion-heading").click(function(event) {
     event.preventDefault();
-
-    $(".js-vertical-tab-content").hide();
-    var accordion_activeTab = $(this).attr("rel");
-    $("#"+accordion_activeTab).show();
-
-    $(".-js-vertical-tab-accordion-heading").removeClass("is-active");
-    $(this).addClass("is-active");
-
-    $(".-js-vertical-tab").removeClass("is-active");
-    $(".-js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
+    history.pushState(null, '', $(this).attr("href"));
+    setTab(getTab(location.hash));
   });
 
   // Launch filter popover on mobile
