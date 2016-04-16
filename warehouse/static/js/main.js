5dc4589f3115c98278b89f58c80b27002e3fb9a6
Donald Stufft <donald@stufft.io>
2016-04-16 11:56:38 -0400

    Add a note to not add new code to main.js

--- warehouse/static/js/main.js
+++ warehouse/static/js/main.js
@@ -1,3 +1,8 @@
+// Note: This file only exists temporarily. Once the rest of this code gets
+//       ported to using the ES6 syntax and is part of the warehouse package
+//       this will be removed. New code should go in
+//       warehouse/static/js/warehouse and not here.
+
 import $ from "jquery";
 import "timeago";
 
