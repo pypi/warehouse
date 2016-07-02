218a602d77d28951a3a1078c918052d294b59210
Matt Layman <matthewlayman@gmail.com>
2016-07-02 18:41:41 -0400

    Set the license on the release detail page. (#1284)
    * Set the license on the release detail page.
    
    * Prefer a classifier when displaying the license.
    
    * Improve coverage and show preference of classifier over metadata.
    
    * Handle multiple license classifiers.
    
    * Move license label below pip command.
    
    * Remove the right alignment.

--- tests/unit/packaging/test_views.py
+++ tests/unit/packaging/test_views.py
@@ -17,6 +17,7 @@ from pyramid.httpexceptions import HTTPMovedPermanently, HTTPNotFound
 from warehouse.packaging import views
 
 from ...common.db.accounts import UserFactory
+from ...common.db.classifiers import ClassifierFactory
 from ...common.db.packaging import (
     ProjectFactory, ReleaseFactory, FileFactory, RoleFactory,
 )
@@ -131,4 +132,56 @@ class TestReleaseDetail:
                 (r.version, r.created) for r in reversed(releases)
             ],
             "maintainers": sorted(users, key=lambda u: u.username.lower()),
+            "license": None
         }
+
+    def test_license_from_classifier(self, db_request):
+        """A license label is added when a license classifier exists."""
+        other_classifier = ClassifierFactory.create(
+            classifier="Some :: Random :: Classifier")
+        classifier = ClassifierFactory.create(
+            classifier="License :: OSI Approved :: BSD License")
+        release = ReleaseFactory.create(
+            _classifiers=[other_classifier, classifier],
+            license="Will not be used")
+
+        result = views.release_detail(release, db_request)
+
+        assert result["license"] == "BSD License"
+
+    def test_license_with_no_classifier(self, db_request):
+        """With no classifier, a license is used from metadata."""
+        release = ReleaseFactory.create(license="MIT License")
+
+        result = views.release_detail(release, db_request)
+
+        assert result["license"] == "MIT License"
+
+    def test_multiline_license(self, db_request):
+        """When license metadata is longer than one line, the first is used."""
+        release = ReleaseFactory.create(
+            license="Multiline License\nhow terrible")
+
+        result = views.release_detail(release, db_request)
+
+        assert result["license"] == "Multiline License"
+
+    def test_no_license(self, db_request):
+        """With no license classifier or metadata, no license is in context."""
+        release = ReleaseFactory.create()
+
+        result = views.release_detail(release, db_request)
+
+        assert result["license"] is None
+
+    def test_multiple_licenses_from_classifiers(self, db_request):
+        """A license label is added when multiple license classifiers exist."""
+        license_1 = ClassifierFactory.create(
+            classifier="License :: OSI Approved :: BSD License")
+        license_2 = ClassifierFactory.create(
+            classifier="License :: OSI Approved :: MIT License")
+        release = ReleaseFactory.create(_classifiers=[license_1, license_2])
+
+        result = views.release_detail(release, db_request)
+
+        assert result["license"] == "BSD License, MIT License"
