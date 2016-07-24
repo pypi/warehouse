0fed0bd5094c555e83318c7b0584fe9e6af8e631
Berker Peksag <berker.peksag@gmail.com>
2016-07-24 13:23:33 +0300

    Delete main.js
    Closes #1068

--- warehouse/static/js/main.js
+++
@@ -1,5 +0,0 @@
-// Note: This file only exists temporarily. Once the rest of this code gets
-//       ported to using the ES6 syntax and is part of the warehouse package
-//       this will be removed. New code should go in
-//       warehouse/static/js/warehouse and not here.
-
