19e7275bcbe7c6ce85a208a464301d141eca402d
Matt Long <matt@lightmatter.com>
2016-08-05 17:48:35 -0400

    remove applied filter on click (#1404)

--- warehouse/static/js/warehouse/utils/forms.js
+++ warehouse/static/js/warehouse/utils/forms.js
@@ -11,26 +11,41 @@
  * limitations under the License.
  */
 
-
 let submitForm = (element) => {
   element.form.submit();
+};
+
+const removeFilter = (value, event) => {
+  event.preventDefault();
+  const element = document.querySelector(`#classifiers input[value="${value}"]`);
+  if (element) {
+    element.checked = false;
+    submitForm(element);
+  }
 };
 
 
 export function submitTriggers() {
   // Get all of the elements on the page with a -js-form-submit-trigger class
   // associated with them.
-  let elements = Array.from(
-      document.querySelectorAll(".-js-form-submit-trigger")
-  );
+  let elements = document.querySelectorAll(".-js-form-submit-trigger");
 
   // Add an on change event handler to each element that will trigger the
   // containing for to submit whenever it is called.
-  elements.forEach((element) => {
-    element.addEventListener(
-      "change",
-      submitForm.bind(undefined, element),
+
+  for (const element of elements) {
+    element.addEventListener("change", submitForm.bind(null, element), false);
+  }
+
+  const filters = document.querySelectorAll(".filter-badge");
+
+  for (const filter of filters) {
+    const [input, button] = [filter.firstElementChild, filter.lastElementChild];
+
+    button.addEventListener(
+      "click",
+      removeFilter.bind(null, input.value),
       false
     );
-  });
+  }
 }
