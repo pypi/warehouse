d906f4ada89e81e3ecaa7093bbcfe6c4ab29dbdc
Donald Stufft <donald@stufft.io>
2017-04-20 16:01:18 -0400

    Automatically run migrations during Heroku's "Release" Phase (#1931)
    * Only allow one alembic command to run at a time
    
    Two different alembic commands attempting to run concurrency may end
    up fighting over who gets to apply a migration. Since we use
    transactions typically what will happen is that they'll both run
    but the later one will fail to commit the transaction and return a
    error code.
    
    Instead, we would like it to wait until the other command has ran and
    then see nothing needs to be done, and return a success code.
    
    * Run our database migrations at the Heroku "Release" phase
    
    * Deploy the Fastly config at release time
    
    * Document requirements on migrations and VCL changes

--- tests/unit/cli/test_db.py
+++ tests/unit/cli/test_db.py
@@ -30,11 +30,24 @@ def test_branches_command(monkeypatch, cli, pyramid_config):
     alembic_branches = pretend.call_recorder(lambda config: None)
     monkeypatch.setattr(alembic.command, "branches", alembic_branches)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(branches, obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_branches.calls == [pretend.call(alembic_config)]
 
 
@@ -42,11 +55,24 @@ def test_current_command(monkeypatch, cli, pyramid_config):
     alembic_current = pretend.call_recorder(lambda config: None)
     monkeypatch.setattr(alembic.command, "current", alembic_current)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(current, obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_current.calls == [pretend.call(alembic_config)]
 
 
@@ -54,11 +80,24 @@ def test_downgrade_command(monkeypatch, cli, pyramid_config):
     alembic_downgrade = pretend.call_recorder(lambda config, revision: None)
     monkeypatch.setattr(alembic.command, "downgrade", alembic_downgrade)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(downgrade, ["--", "-1"], obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_downgrade.calls == [pretend.call(alembic_config, "-1")]
 
 
@@ -76,11 +115,24 @@ def test_heads_command(monkeypatch, cli, pyramid_config, args, ekwargs):
     )
     monkeypatch.setattr(alembic.command, "heads", alembic_heads)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(heads, args, obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_heads.calls == [pretend.call(alembic_config, **ekwargs)]
 
 
@@ -88,11 +140,24 @@ def test_history_command(monkeypatch, cli, pyramid_config):
     alembic_history = pretend.call_recorder(lambda config, range: None)
     monkeypatch.setattr(alembic.command, "history", alembic_history)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(history, ["foo:bar"], obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_history.calls == [pretend.call(alembic_config, "foo:bar")]
 
 
@@ -123,11 +188,24 @@ def test_merge_command(monkeypatch, cli, pyramid_config, args, eargs, ekwargs):
     )
     monkeypatch.setattr(alembic.command, "merge", alembic_merge)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(merge, args, obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_merge.calls == [
         pretend.call(alembic_config, *eargs, **ekwargs),
     ]
@@ -167,11 +245,24 @@ def test_revision_command(monkeypatch, cli, pyramid_config, args, ekwargs):
     )
     monkeypatch.setattr(alembic.command, "revision", alembic_revision)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(revision, args, obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_revision.calls == [pretend.call(alembic_config, **ekwargs)]
 
 
@@ -179,11 +270,24 @@ def test_show_command(monkeypatch, cli, pyramid_config):
     alembic_show = pretend.call_recorder(lambda config, revision: None)
     monkeypatch.setattr(alembic.command, "show", alembic_show)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(show, ["foo"], obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_show.calls == [pretend.call(alembic_config, "foo")]
 
 
@@ -191,11 +295,24 @@ def test_stamp_command(monkeypatch, cli, pyramid_config):
     alembic_stamp = pretend.call_recorder(lambda config, revision: None)
     monkeypatch.setattr(alembic.command, "stamp", alembic_stamp)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(stamp, ["foo"], obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_stamp.calls == [pretend.call(alembic_config, "foo")]
 
 
@@ -203,9 +320,22 @@ def test_upgrade_command(monkeypatch, cli, pyramid_config):
     alembic_upgrade = pretend.call_recorder(lambda config, revision: None)
     monkeypatch.setattr(alembic.command, "upgrade", alembic_upgrade)
 
-    alembic_config = pretend.stub()
+    alembic_config = pretend.stub(attributes={})
     pyramid_config.alembic_config = lambda: alembic_config
+
+    connection = pretend.stub(
+        __enter__=lambda: connection,
+        __exit__=lambda *a, **k: None,
+        execute=pretend.call_recorder(lambda sql: None),
+    )
+    engine = pretend.stub(begin=lambda: connection)
+    pyramid_config.registry["sqlalchemy.engine"] = engine
 
     result = cli.invoke(upgrade, ["foo"], obj=pyramid_config)
     assert result.exit_code == 0
+    assert alembic_config.attributes == {"connection": connection}
+    assert connection.execute.calls == [
+        pretend.call("SELECT pg_advisory_lock(hashtext('alembic'))"),
+        pretend.call("SELECT pg_advisory_unlock(hashtext('alembic'))"),
+    ]
     assert alembic_upgrade.calls == [pretend.call(alembic_config, "foo")]
