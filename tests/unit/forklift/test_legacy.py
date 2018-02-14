c07fbb30d43d2bb1f3e101aeae82fe0d59f8a913
Ernest W Durbin III <ewdurbin@gmail.com>
2018-02-14 15:36:06 -0500

    additional test cases for #2942

--- tests/unit/forklift/test_legacy.py
+++ tests/unit/forklift/test_legacy.py
@@ -883,6 +883,9 @@ class TestFileUpload:
                                       "encodings.utf_8_sig",
                                       "distutils.command.build_clib",
                                       "xmlrpc", "xmlrpc.server",
+                                      "xml.etree", "xml.etree.ElementTree",
+                                      "xml.parsers", "xml.parsers.expat",
+                                      "xml.parsers.expat.errors",
                                       "encodings.idna", "encodings",
                                       "CGIHTTPServer", "cgihttpserver"])
     def test_fails_with_stdlib_names(self, pyramid_config, db_request, name):
