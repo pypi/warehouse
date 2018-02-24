ace5aee5a5842c7e40e03912d3fc57fd679d92b1
Dustin Ingram <di@users.noreply.github.com>
2018-02-23 18:46:12 -0600

    Disable delete button until input matches

---
+++ warehouse/static/js/warehouse/controllers/confirm_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = [ "input", "button" ]
+
+  connect() {
+    this.buttonTarget.disabled = true;
+  }
+
+  check() {
+    if (this.inputTarget.value == this.buttonTarget.dataset.expected) {
+      this.buttonTarget.disabled = false;
+    } else {
+      this.buttonTarget.disabled = true;
+    }
+  }
+}
