6ba5766dddeed1ba11d1345d02e01ae3fdb43385
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2018-02-28 18:50:30 +0000

    Added password strength gauge in registration form. (#2542)
    * Added vendor JS gulp step to include zxcvbn.js
    Include it in the register template
    Added password strength gauge in registration form.
    
    * Added JS validation via tooltips.
    Ignore vendor scripts in lint.
    
    * Include `window.scroll` values in tooltip placement
    
    * Move SCSS block to its own file.
    Adapt to naming convention.
    Use existing tooltips.
    
    * Always show 'Confirm Password' label
    
    * Fix password-strench documentation
    
    * Split help text into two paragraphs
    
    * Fix typo
    
    * Add screenreader feedback

--- warehouse/static/js/warehouse/utils/forms.js
+++ warehouse/static/js/warehouse/utils/forms.js
@@ -49,3 +49,96 @@ export function submitTriggers() {
     );
   }
 }
+
+/* global zxcvbn */
+
+const tooltipClasses = ["tooltipped", "tooltipped-s", "tooltipped-immediate"];
+
+const passwordStrengthValidator = (value) => {
+  const zxcvbnResult = zxcvbn(value);
+  return zxcvbnResult.score < 2 ?
+    zxcvbnResult.feedback.suggestions.join(" ") : null;
+};
+
+const fieldRequiredValidator = (value) => {
+  return value === ""?
+    "Please fill out this field" : null;
+};
+
+const checkPasswordStrength = (event) => {
+  let result = document.querySelector(".password-strength__gauge");
+  if (event.target.value === "") {
+    result.setAttribute("class", "password-strength__gauge");
+    // Feedback for screen readers
+    result.querySelector(".sr-only").innerHTML = "Password field is empty";
+  } else {
+    // following recommendations on the zxcvbn JS docs
+    // the zxcvbn function is available by loading `vendor/zxcvbn.js`
+    // in the register page template only
+    let zxcvbnResult = zxcvbn(event.target.value);
+    result.setAttribute("class", `password-strength__gauge password-strength__gauge--${zxcvbnResult.score}`);
+
+    // Feedback for screen readers
+    result.querySelector(".sr-only").innerHTML = zxcvbnResult.feedback.suggestions.join(" ") || "Password is strong";
+  }
+};
+
+const setupPasswordStrengthGauge = () => {
+  let password = document.querySelector("#new_password");
+  if (password === null) return;
+  password.addEventListener(
+    "input",
+    checkPasswordStrength,
+    false
+  );
+};
+
+const attachTooltip = (field, message) => {
+  let parentNode = field.parentNode;
+  parentNode.classList.add.apply(parentNode.classList, tooltipClasses);
+  parentNode.setAttribute("aria-label", message);
+};
+
+const removeTooltips = () => {
+  let tooltippedNodes = document.querySelectorAll(".tooltipped");
+  for (let tooltippedNode of tooltippedNodes) {
+    tooltippedNode.classList.remove.apply(tooltippedNode.classList, tooltipClasses);
+    tooltippedNode.removeAttribute("aria-label");
+  }
+};
+
+const validateForm = (event) => {
+  removeTooltips();
+  let inputFields = document.querySelectorAll("input[required='required']");
+  for (let inputField of inputFields) {
+    let requiredMessage = fieldRequiredValidator(inputField.value);
+    if (requiredMessage !== null) {
+      attachTooltip(inputField, requiredMessage);
+      event.preventDefault();
+      return false;
+    }
+  }
+
+  let password = document.querySelector("#new_password");
+  let passwordConfirm = document.querySelector("#password_confirm");
+  if (password.value !== passwordConfirm.value) {
+    let message = "Passwords do not match";
+    attachTooltip(password, message);
+    event.preventDefault();
+    return false;
+  }
+
+  let passwordStrengthMessage = passwordStrengthValidator(password.value);
+  if (passwordStrengthMessage !== null) {
+    attachTooltip(password, passwordStrengthMessage);
+    event.preventDefault();
+    return false;
+  }
+};
+
+export function registerFormValidation() {
+  if (document.querySelector("#password_confirm") === null) return;
+  setupPasswordStrengthGauge();
+  const submitButton = document.querySelector("#content input[type='submit']");
+  submitButton.addEventListener("click", validateForm, false);
+}
