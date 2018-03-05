c0995e8819ded4a8e7d118fdf9c63d9a9f9c8705
Allan Almazan <allan.almazan@gmail.com>
2018-03-05 00:48:08 -0800

    Issue #3087: Re-indent HTML, lint fixes

--- warehouse/static/js/warehouse/controllers/password_controller.js
+++ warehouse/static/js/warehouse/controllers/password_controller.js
@@ -1,18 +1,17 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-    static targets = ["showPassword", "newPassword", "passwordConfirm"];
-    passwordFields = ["newPasswordTarget", "passwordConfirmTarget"];
+  static targets = ["showPassword", "newPassword", "passwordConfirm"];
+  passwordFields = ["newPasswordTarget", "passwordConfirmTarget"];
 
-    showPasswords() {
-        const showPassword = this.showPasswordTarget;
-        for (let field of this.passwordFields) {
-            if (showPassword.checked) {
-                this[field].type = "text";
-            } else {
-                this[field].type = "password";
-            }
-        }
-
+  showPasswords() {
+    const showPassword = this.showPasswordTarget;
+    for (let field of this.passwordFields) {
+      if (showPassword.checked) {
+        this[field].type = "text";
+      } else {
+        this[field].type = "password";
+      }
     }
+  }
 }
