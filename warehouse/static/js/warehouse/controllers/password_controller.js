7a0de6434f123c9337fd77599267cd6c0a4939fd
Allan Almazan <allan.almazan@gmail.com>
2018-03-05 22:19:48 -0800

    Requested fixes: Move additional code into controller, more concise targets, rename method/variables for clarity

--- warehouse/static/js/warehouse/controllers/password_controller.js
+++ warehouse/static/js/warehouse/controllers/password_controller.js
@@ -1,30 +1,34 @@
 /**
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["showPassword", "newPassword", "passwordConfirm"];
-  passwordFields = ["newPasswordTarget", "passwordConfirmTarget"];
+  static targets = ["showPassword", "password"];
 
-  showPasswords() {
-    const showPassword = this.showPasswordTarget;
-    for (let field of this.passwordFields) {
-      if (showPassword.checked) {
-        this[field].type = "text";
-      } else {
-        this[field].type = "password";
+  connect() {
+    if (this.passwordTargets && this.showPasswordTarget) {
+      // Reset these so they don't persist between page reloads
+      for (let field of this.passwordTargets) {
+        field.type = "password";
+        this.showPasswordTarget.checked = false;
       }
+    }
+  }
+
+  togglePasswords() {
+    for (let field of this.passwordTargets) {
+      field.type = this.showPasswordTarget.checked ? "text" : "password";
     }
   }
 }
--- warehouse/static/js/warehouse/index.js
+++ warehouse/static/js/warehouse/index.js
@@ -129,20 +129,6 @@ docReady(() => {
 });
 
 docReady(() => {
-  let passwordFields = document.querySelectorAll("#password, #password_confirm, #new_password");
-  let showPasswordCheck = document.querySelector("#show-password");
-
-  if (passwordFields && showPasswordCheck) {
-    // Reset these so they don't persist between page reloads
-    for (let field of passwordFields) {
-      field.type = "password";
-      showPasswordCheck.checked = false;
-    }
-
-  }
-});
-
-docReady(() => {
   let changeRoleForms = document.querySelectorAll("form.change-role");
 
   if (changeRoleForms) {
