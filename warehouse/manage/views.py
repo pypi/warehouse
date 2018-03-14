4f6d3c9496c7918f34d37b4dba6ae09f5f154e1a
Mariatta <Mariatta@users.noreply.github.com>
2018-03-14 12:45:42 -0700

    Send email when a new collaborator has been added to the project. (#3155)
    * Send email when a new collaborator has been added to the project.
    
    - send the email to the newly added collaborator
    - send the email to other owners
    - email not sent to the person who added the collaborator
    
    Closes https://github.com/pypa/warehouse/issues/1000
    
    * Remove prints
    
    * Make linter happy.
    
    * Added functionality to send email whenever primary email is changed (#3158)
    
    Addressed code reviews
    
    * - Send a separate welcome email to the new collaborator
    - Add owners emails in bcc field
    
    * PEP 8
    
    * Fix linters errors
    
    * - Deindent
    - Add footer to email
    
    * Rebased with master
    
    * Fix broken unit test.
    
    * Fix linter error
    
    * PEP 8
    
    * Actually send the email to bcc recipients

--- warehouse/manage/views.py
+++ warehouse/manage/views.py
@@ -22,7 +22,8 @@ from warehouse.accounts.interfaces import IUserService
 from warehouse.accounts.models import User, Email
 from warehouse.accounts.views import logout
 from warehouse.email import (
-    send_account_deletion_email, send_email_verification_email,
+    send_account_deletion_email, send_added_as_collaborator_email,
+    send_collaborator_added_email, send_email_verification_email,
     send_password_change_email, send_primary_email_change_email
 )
 from warehouse.manage.forms import (
@@ -551,6 +552,32 @@ def manage_project_roles(project, request, _form_class=CreateRoleForm):
                     submitted_from=request.remote_addr,
                 ),
             )
+
+            owners = (
+                request.db.query(Role)
+                .join(Role.user)
+                .filter(Role.role_name == 'Owner', Role.project == project)
+            )
+            owner_emails = [owner.user.email for owner in owners]
+            owner_emails.remove(request.user.email)
+
+            send_collaborator_added_email(
+                request,
+                user,
+                request.user,
+                project.name,
+                form.role_name.data,
+                owner_emails
+            )
+
+            send_added_as_collaborator_email(
+                request,
+                request.user,
+                project.name,
+                form.role_name.data,
+                user.email
+            )
+
             request.session.flash(
                 f"Added collaborator '{form.username.data}'",
                 queue="success"
