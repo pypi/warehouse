4136c1d655fcf5e4258289e5dfe66828c27fa6aa
Dustin Ingram <di@users.noreply.github.com>
2018-04-17 12:43:40 -0500

    Don't need to set initial-scale

--- warehouse/static/js/warehouse/controllers/viewport_toggle_controller.js
+++ warehouse/static/js/warehouse/controllers/viewport_toggle_controller.js
@@ -22,34 +22,35 @@ export default class extends Controller {
 
   connect() {
     // Check if localStorage has already been set
-    const originalWidth = localStorage.getItem("originalWidth");
-    if (originalWidth) {
+    const showDesktop = localStorage.getItem("showDesktop");
+    if (showDesktop) {
       // Already been switched to desktop before, so show the mobile button
       this.switchToMobileTarget.classList.remove("hidden");
       // And resize again
-      this._setViewport(DESKTOP_WIDTH, originalWidth/DESKTOP_WIDTH);
+      this._setViewport(DESKTOP_WIDTH);
     } else {
       // If we get here, JS is enabled, so show the "Switch To Desktop" button
       this.switchToDesktopTarget.classList.remove("hidden");
     }
   }
 
-  _setViewport(width, initialScale) {
-    document.getElementsByTagName("meta")["viewport"].content = (
-      `width=${width}, initial-scale=${initialScale}`
-    );
+  _setViewport(width) {
+    const content = `width=${width}`;
+    document.getElementsByTagName("meta")["viewport"].content = content;
   }
 
   switchToDesktop() { // Toggle to the desktop viewport.
     // Store the original width to reuse on the next page load to resize again.
     this.switchToMobileTarget.classList.remove("hidden");
-    localStorage.setItem("originalWidth", window.innerWidth);
-    this._setViewport(DESKTOP_WIDTH, window.innerWidth/DESKTOP_WIDTH);
+    this.switchToDesktopTarget.classList.add("hidden");
+    localStorage.setItem("showDesktop", 1);
+    this._setViewport(DESKTOP_WIDTH);
   }
 
   switchToMobile() { // Reset to the original viewport
     this.switchToMobileTarget.classList.add("hidden");
-    localStorage.removeItem("originalWidth");
-    this._setViewport("device-width", 1);
+    this.switchToDesktopTarget.classList.remove("hidden");
+    localStorage.removeItem("showDesktop");
+    this._setViewport("device-width");
   }
 }
