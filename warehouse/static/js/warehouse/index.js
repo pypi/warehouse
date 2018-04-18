b463af8aac4c778fe5fd1d7abe6e52c00bd06a13
Nicole Harris <n.harris@kabucreative.com.au>
2018-04-18 08:38:09 +0100

    Refactor dropdowns to follow WAI recommendations (#3287)

--- warehouse/static/js/warehouse/index.js
+++ warehouse/static/js/warehouse/index.js
@@ -166,6 +166,46 @@ docReady(() => {
   }
 });
 
+var bindDropdowns = function () {
+  // Bind click handlers to dropdowns for keyboard users
+  let dropdowns = document.querySelectorAll(".dropdown");
+  for (let dropdown of dropdowns) {
+    let trigger = dropdown.querySelector(".dropdown__trigger");
+    let content = dropdown.querySelector(".dropdown__content");
+
+    if (!trigger.dataset.dropdownBound) {
+      // If the user has clicked the trigger (either with a mouse or by pressing
+      // space/enter on the keyboard) show the content
+      trigger.addEventListener("click", function () {
+        // Toggle the visibility of the content
+        if (content.classList.contains("display-block")) {
+          content.classList.remove("display-block");
+        } else {
+          content.classList.add("display-block");
+        }
+      });
+
+      // If the user has moused onto the trigger and has happened to click it,
+      // remove the `display-block` class so that it doesn't stay visable when
+      // they mouse out
+      trigger.addEventListener("mouseout", function() {
+        content.classList.remove("display-block");
+      });
+
+      // Set the 'data-dropdownBound' attribute so we don't bind multiple
+      // handlers to the same trigger after the client-side-includes load
+      trigger.dataset.dropdownBound = true;
+    }
+  }
+};
+
+// Bind the dropdowns when the page is ready
+docReady(bindDropdowns);
+
+// Bind again when client-side includes have been loaded (for the logged-in
+// user dropdown)
+document.addEventListener("CSILoaded", bindDropdowns);
+
 const application = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
