77dcdb15cfc46360dc425e27f42456fc591b9b3b
Dustin Ingram <di@users.noreply.github.com>
2018-04-19 14:52:22 -0500

    Simplification

--- warehouse/static/js/warehouse/utils/doc-ready.js
+++ warehouse/static/js/warehouse/utils/doc-ready.js
@@ -11,31 +11,7 @@
  * limitations under the License.
  */
 
-let warningShown = false;
-
-function unsupportedBrowser() {
-  return navigator.appVersion.includes("MSIE 10");
-}
-
-function showUnsupportedBrowserWarning() {
-  if (document.getElementById("unsupported-browser") !== null) return;
-
-  let warning_html = "<div id='unsupported-browser' class='notification-bar notification-bar--danger'><span class='notification-bar__icon'><i class='fa fa-exclamation-triangle' aria-hidden='true'></i><span class='sr-only'>Warning:</span></span><span class='notification-bar__message'>You are using an unsupported browser, please upgrade to a newer version.</span></div>";
-  let warning_div = document.createElement("div");
-  warning_div.innerHTML = warning_html;
-
-  let warning_section = document.getElementById("sticky-notifications");
-  warning_section.appendChild(warning_div);
-  warningShown = true;
-}
-
 export default (fn) => {
-  if (unsupportedBrowser()) {
-    if (!warningShown) {
-      document.addEventListener("DOMContentLoaded", showUnsupportedBrowserWarning);
-    }
-    return;
-  }
   if (document.readyState != "loading") { fn(); }
   else { document.addEventListener("DOMContentLoaded", fn); }
 };
