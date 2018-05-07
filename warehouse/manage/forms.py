cd316568fcb7352eceb06288b1655786e95e2118
Yuan Huang <huangy22@users.noreply.github.com>
2018-05-07 14:44:14 -0400

    Make forms error message adhere to styleguide (#3846)
    * Refs #3167
    
    Removes instances of "Please"
    Removes . on short single sentences
    
    * fix the mistake of removing the space between two sentences.
    
    * address comments
    
    * address the comments
    rewind the tests/unit files

--- warehouse/manage/forms.py
+++ warehouse/manage/forms.py
@@ -45,7 +45,7 @@ class UsernameMixin:
 
         if userid is None:
             raise wtforms.validators.ValidationError(
-                "No user found with that username. Please try again."
+                "No user found with that username. Try again."
             )
 
 
