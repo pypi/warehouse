0c10030f56b27bdc71e87796f95a4018fb240f9f
Matt Bacchi <mbacchi@gmail.com>
2018-05-15 09:14:24 -0400

    Clean up code and set GitHub access token through environment variable.
    In order to use the WAREHOUSE_GITHUB_ACCESS_TOKEN environment variable in
    a development environment, set it in the warehouse/dev/environment file.

--- warehouse/credits/__init__.py
+++ warehouse/credits/__init__.py
@@ -4,14 +4,7 @@ from warehouse.credits.tasks import get_contributors
 
 
 def includeme(config):
-    # Add a periodic task to get contributors every 2 hours, to do this we
-    # require the Github access token owned by the pypa warehouse application.
-    # if config.get_settings().get("warehouse.github_access_token"):
-    #     config.add_periodic_task(crontab(minute=0, hour='*/2'), get_contributors)
-
-    # TEST FIXME
-    # for now do every 10 minutes for testing purposes
-    # if config.get_settings().get("warehouse.github_access_token"):
-    # config.add_periodic_task(crontab(), getcontrib)
-    config.add_periodic_task(crontab(minute='*/2'), get_contributors)
-    # pass
+    # Add a periodic task to get contributors every 24 hours
+    if config.get_settings().get("warehouse.github_access_token"):
+        config.add_periodic_task(crontab(minute='2', hour='2'),
+                                 get_contributors)
--- warehouse/credits/contributors.py
+++ warehouse/credits/contributors.py
@@ -7,8 +7,8 @@ class Contributor(db.Model):
 
     __tablename__ = 'warehouse_contributors'
 
-    # id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    contributor_login = Column(Text, primary_key=True, unique=True, nullable=False)
+    contributor_login = Column(Text, primary_key=True, unique=True,
+                               nullable=False)
     contributor_name = Column(Text, nullable=False)
     contributor_url = Column(Text, nullable=False)
 
