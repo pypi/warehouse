f62ecbaa653ec6179e8bd333bfefb522dcabbf26
Matt Bacchi <mbacchi@gmail.com>
2018-05-15 09:49:31 -0400

    more cleanup

--- warehouse/credits/views.py
+++ warehouse/credits/views.py
@@ -9,17 +9,7 @@ from .contributors import Contributor
 )
 def credits_page(request):
 
-    # test data mbacchi FIXME
-    # contributors = [{'contributor_login':'mbacchi',
-    #                  'contributor_name':'Matt Bacchi',
-    #                  'contributor_url': 'https://github.com/mbacchi'},
-    #                 {'contributor_login':'someone',
-    #                  'contributor_name':'Blank Name',
-    #                  'contributor_url': 'https://github.com/someone'},
-    #                 ]
-
     # get all items from Contributors table
     contributors = request.db.query(Contributor).order_by(Contributor.contributor_name)
-    # print(contributors)
 
     return {'contributors': contributors}
