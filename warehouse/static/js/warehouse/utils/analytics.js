e43f39940809832a94be411e0a68175acdbf9546
Ernest W Durbin III <ewdurbin@gmail.com>
2018-06-20 15:43:51 -0400

    short circuit remaining GA setup if DNT

--- warehouse/static/js/warehouse/utils/analytics.js
+++ warehouse/static/js/warehouse/utils/analytics.js
@@ -22,21 +22,21 @@ export default () => {
     var dnt = navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack;
     if (dnt == "1" || dnt == "yes") {
       window["ga-disable-" + element.dataset.gaId] = true;
-    }
+    } else {
+      // This is more or less taken straight from Google Analytics Control Panel
+      window.dataLayer = window.dataLayer || [];
+      var gtag = function(){ dataLayer.push(arguments); };
 
-    // This is more or less taken straight from Google Analytics Control Panel
-    window.dataLayer = window.dataLayer || [];
-    var gtag = function(){ dataLayer.push(arguments); };
+      gtag("js", new Date());
+      gtag("config", element.dataset.gaId, { "anonymize_ip": true });
 
-    gtag("js", new Date());
-    gtag("config", element.dataset.gaId, { "anonymize_ip": true });
-
-    // Determine if we have a user ID associated with this person, if so we'll
-    // go ahead and tell Google it to enable better tracking of individual
-    // users.
-    let cookies = cookie.parse(document.cookie);
-    if (cookies.user_id__insecure) {
-      gtag("set", {"user_id": cookies.user_id__insecure});
+      // Determine if we have a user ID associated with this person, if so we'll
+      // go ahead and tell Google it to enable better tracking of individual
+      // users.
+      let cookies = cookie.parse(document.cookie);
+      if (cookies.user_id__insecure) {
+        gtag("set", {"user_id": cookies.user_id__insecure});
+      }
     }
   }
 };
