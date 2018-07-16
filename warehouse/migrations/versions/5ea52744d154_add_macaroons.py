15531017b29f951451157c498a37ed97d6a1d35e
Donald Stufft <donald@stufft.io>
2018-07-16 01:17:37 -0400

    Add a database model for holding our macaroons

---
+++ warehouse/migrations/versions/5ea52744d154_add_macaroons.py
@@ -0,0 +1,58 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Add Macaroons
+
+Revision ID: 5ea52744d154
+Revises: f2a453c96ded
+Create Date: 2018-07-16 06:45:31.152291
+"""
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = "5ea52744d154"
+down_revision = "f2a453c96ded"
+
+
+def upgrade():
+    op.create_table(
+        "macaroons",
+        sa.Column(
+            "id",
+            postgresql.UUID(as_uuid=True),
+            server_default=sa.text("gen_random_uuid()"),
+            nullable=False,
+        ),
+        sa.Column("user_id", postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column(
+            "description", sa.String(length=100), server_default="", nullable=False
+        ),
+        sa.Column(
+            "created", sa.DateTime(), server_default=sa.text("now()"), nullable=False
+        ),
+        sa.Column("last_used", sa.DateTime(), nullable=True),
+        sa.Column(
+            "caveats",
+            postgresql.JSONB(astext_type=sa.Text()),
+            server_default=sa.text("'{}'"),
+            nullable=False,
+        ),
+        sa.Column("key", sa.LargeBinary(), nullable=False),
+        sa.ForeignKeyConstraint(["user_id"], ["accounts_user.id"]),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade():
+    op.drop_table("macaroons")
