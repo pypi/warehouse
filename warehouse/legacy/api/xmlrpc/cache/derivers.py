ae2d26190fadb23a76a9c12c3465930f7ab12527
Ernest W Durbin III <ewdurbin@gmail.com>
2018-07-26 15:21:39 -0400

    xmlrpc: cache list_packages methods, purge on all-projects keys. extend cache life of per project keys

--- warehouse/legacy/api/xmlrpc/cache/derivers.py
+++ warehouse/legacy/api/xmlrpc/cache/derivers.py
@@ -32,7 +32,7 @@ def cached_return_view(view, info):
                 return view(context, request)
             try:
                 key = json.dumps(request.rpc_args[slice_obj])
-                _tag = None
+                _tag = tag
                 if arg_index is not None:
                     _tag = tag % (tag_processor(request.rpc_args[arg_index]),)
                 return service.fetch(view, (context, request), {}, key, _tag, expires)
