90cd1af6ca420a3882f9936258d2f97ea5eace79
Donald Stufft <donald@stufft.io>
2018-08-09 11:31:42 -0400

    Fix email address (#4503)

--- warehouse/__about__.py
+++ warehouse/__about__.py
@@ -44,7 +44,7 @@ else:
     __commit__ = None
 
 __author__ = "The Python Packaging Authority"
-__email__ = "admin@mail.pypi.org"
+__email__ = "admin@pypi.org"
 
 __license__ = "Apache License, Version 2.0"
 __copyright__ = "2018 %s" % __author__
