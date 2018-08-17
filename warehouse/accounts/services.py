665ee19a2ba63038c0acaab098a2661fa9f04140
Donald Stufft <donald@stufft.io>
2018-08-17 12:23:24 -0400

    Formatting

--- warehouse/accounts/services.py
+++ warehouse/accounts/services.py
@@ -173,12 +173,7 @@ class DatabaseUserService:
         return False
 
     def create_user(
-        self,
-        username,
-        name,
-        password,
-        is_active=False,
-        is_superuser=False,
+        self, username, name, password, is_active=False, is_superuser=False
     ):
 
         user = User(
--- warehouse/migrations/versions/62f6becc7653_set_user_is_staff_to_nullable.py
+++ warehouse/migrations/versions/62f6becc7653_set_user_is_staff_to_nullable.py
@@ -20,13 +20,13 @@ Create Date: 2018-08-17 16:16:20.397865
 from alembic import op
 
 
-revision = '62f6becc7653'
-down_revision = '522918187b73'
+revision = "62f6becc7653"
+down_revision = "522918187b73"
 
 
 def upgrade():
-    op.alter_column('accounts_user', 'is_staff', nullable=True)
+    op.alter_column("accounts_user", "is_staff", nullable=True)
 
 
 def downgrade():
-    op.alter_column('accounts_user', 'is_staff', nullable=False)
+    op.alter_column("accounts_user", "is_staff", nullable=False)
