24c8b12e1d4f589115cf82329c6637bcdea306b4
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2018-11-18 15:59:29 +0000

    Fix Travis build

--- tests/frontend/spec/helpers.js
+++ tests/frontend/spec/helpers.js
@@ -18,9 +18,11 @@ import { Application } from "stimulus";
 function registerApplication(id, controllerClass, fixtureName) {
   fixtureName = fixtureName || id;
   fixture.load(`${fixtureName}.html`);
-  this._stimulusApp = Application.start();
+  this._stimulusApp = new Application();
+  this._stimulusApp.router.start();
   this._stimulusApp.register(id, controllerClass);
   this.controller = this._stimulusApp.controllers[0];
 }
 
 export { registerApplication };
+
