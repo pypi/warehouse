f1633d39f85ba3008acd03fc9d30f52401304898
Ernest W Durbin III <ewdurbin@gmail.com>
2018-11-20 15:26:04 -0500

    Revert "Revert "RFE: display date in project list #3626 (pypa/warehouse#3626)" (#5089)"
    This reverts commit 0e2309ba6819a658965aa9a9918e20b8700dbd03.

--- warehouse/views.py
+++ warehouse/views.py
@@ -12,6 +12,7 @@
 
 import collections
 import re
+from datetime import datetime
 
 import elasticsearch
 
@@ -350,6 +351,10 @@ def search(request):
 
     metrics = request.find_service(IMetricsService, context=None)
     metrics.histogram("warehouse.views.search.results", page.item_count)
+
+    if hasattr(page, "items"):
+        for item in page.items:
+            item.created = datetime.strptime(item.created, "%Y-%m-%dT%H:%M:%S.%f")
 
     return {
         "page": page,
