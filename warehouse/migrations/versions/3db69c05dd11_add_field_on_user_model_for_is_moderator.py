7e65656ae561f140b33c0bf3c93e6faf28bae536
Chris Wilcox <crwilcox@google.com>
2019-01-10 12:30:27 -0800

    modify migration post merge

--- warehouse/migrations/versions/3db69c05dd11_add_field_on_user_model_for_is_moderator.py
+++ warehouse/migrations/versions/3db69c05dd11_add_field_on_user_model_for_is_moderator.py
@@ -13,7 +13,7 @@
 Add field on User model for is_moderator
 
 Revision ID: 3db69c05dd11
-Revises: 2d6390eebe90
+Revises: 67f52a64a389
 Create Date: 2019-01-04 21:29:45.455607
 """
 
@@ -22,7 +22,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "3db69c05dd11"
-down_revision = "2d6390eebe90"
+down_revision = "67f52a64a389"
 
 
 def upgrade():
@@ -32,16 +32,7 @@ def upgrade():
             "is_moderator",
             sa.Boolean(),
             nullable=False,
-            server_default=sa.sql.expression.false(),
-        ),
-    )
-    op.alter_column(
-        "users",
-        sa.Column(
-            "is_superuser",
-            sa.Boolean(),
-            nullable=False,
-            server_default=sa.sql.expression.false(),
+            server_default=sa.sql.false(),
         ),
     )
 
