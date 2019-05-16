b6d2cb6edcd05585a0f9b5acf8d836b64b6b1430
William Woodruff <william@trailofbits.com>
2019-05-16 10:58:14 -0400

    warehouse: Reformat JS

--- warehouse/static/js/warehouse/utils/webauthn.js
+++ warehouse/static/js/warehouse/utils/webauthn.js
@@ -13,200 +13,200 @@
 
 
 const populateWebAuthnErrorList = (errors) => {
-    const errorList = document.getElementById("webauthn-errors");
-    if (errorList === null) {
-        return;
-    }
+  const errorList = document.getElementById("webauthn-errors");
+  if (errorList === null) {
+    return;
+  }
 
-    errors.forEach((error) => {
-        const errorItem = document.createElement("li");
-        errorItem.appendChild(document.createTextNode(error));
-        errorList.appendChild(errorItem);
-    });
-}
+  errors.forEach((error) => {
+    const errorItem = document.createElement("li");
+    errorItem.appendChild(document.createTextNode(error));
+    errorList.appendChild(errorItem);
+  });
+};
 
 const doWebAuthn = (buttonId, func) => {
-    const webAuthnButton = document.getElementById(buttonId);
-    if (webAuthnButton === null) {
-        return null;
-    }
+  const webAuthnButton = document.getElementById(buttonId);
+  if (webAuthnButton === null) {
+    return null;
+  }
 
-    const csrfToken = webAuthnButton.getAttribute("csrf-token");
-    if (csrfToken === null) {
-        return;
-    }
+  const csrfToken = webAuthnButton.getAttribute("csrf-token");
+  if (csrfToken === null) {
+    return;
+  }
 
-    if (!window.PublicKeyCredential) {
-        populateWebAuthnErrorList(["Your browser doesn't support WebAuthn."]);
-        return;
-    }
+  if (!window.PublicKeyCredential) {
+    populateWebAuthnErrorList(["Your browser doesn't support WebAuthn."]);
+    return;
+  }
 
-    webAuthnButton.disabled = false;
-    webAuthnButton.addEventListener("click", async () => { func(csrfToken); });
-}
+  webAuthnButton.disabled = false;
+  webAuthnButton.addEventListener("click", async () => { func(csrfToken); });
+};
 
 const hexEncode = (buf) => {
-    return Array.from(buf).map((x) => {
-        return ("0" + x.toString(16)).substr(-2);
-    }).join("");
-}
+  return Array.from(buf).map((x) => {
+    return ("0" + x.toString(16)).substr(-2);
+  }).join("");
+};
 
 const webAuthnBtoA = (encoded) => {
-    return btoa(encoded).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
-}
+  return btoa(encoded).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+};
 
 const webAuthnBase64Normalize = (encoded) => {
-    return encoded.replace(/\_/g, "/").replace(/\-/g, "+");
-}
+  return encoded.replace(/_/g, "/").replace(/-/g, "+");
+};
 
 const transformAssertionOptions = (assertionOptions) => {
-    let {challenge, allowCredentials} = assertionOptions;
+  let {challenge, allowCredentials} = assertionOptions;
 
-    challenge = Uint8Array.from(challenge, c => c.charCodeAt(0));
-    allowCredentials = allowCredentials.map(credentialDescriptor => {
-        let {id} = credentialDescriptor;
-        id = webAuthnBase64Normalize(id);
-        id = Uint8Array.from(atob(id), c => c.charCodeAt(0));
-        return Object.assign({}, credentialDescriptor, {id});
-    });
+  challenge = Uint8Array.from(challenge, c => c.charCodeAt(0));
+  allowCredentials = allowCredentials.map(credentialDescriptor => {
+    let {id} = credentialDescriptor;
+    id = webAuthnBase64Normalize(id);
+    id = Uint8Array.from(atob(id), c => c.charCodeAt(0));
+    return Object.assign({}, credentialDescriptor, {id});
+  });
 
-    const transformedOptions = Object.assign(
-        {},
-        assertionOptions,
-        {challenge, allowCredentials});
+  const transformedOptions = Object.assign(
+    {},
+    assertionOptions,
+    {challenge, allowCredentials});
 
-    return transformedOptions;
-}
+  return transformedOptions;
+};
 
 const transformAssertion = (assertion) => {
-    const authData = new Uint8Array(assertion.response.authenticatorData);
-    const clientDataJSON = new Uint8Array(assertion.response.clientDataJSON);
-    const rawId = new Uint8Array(assertion.rawId);
-    const sig = new Uint8Array(assertion.response.signature);
-    const assertionClientExtensions = assertion.getClientExtensionResults();
+  const authData = new Uint8Array(assertion.response.authenticatorData);
+  const clientDataJSON = new Uint8Array(assertion.response.clientDataJSON);
+  const rawId = new Uint8Array(assertion.rawId);
+  const sig = new Uint8Array(assertion.response.signature);
+  const assertionClientExtensions = assertion.getClientExtensionResults();
 
-    return {
-        id: assertion.id,
-        rawId: webAuthnBtoA(rawId),
-        type: assertion.type,
-        authData: webAuthnBtoA(String.fromCharCode(...authData)),
-        clientData: webAuthnBtoA(String.fromCharCode(...clientDataJSON)),
-        signature: hexEncode(sig),
-        assertionClientExtensions: JSON.stringify(assertionClientExtensions),
-    };
-}
+  return {
+    id: assertion.id,
+    rawId: webAuthnBtoA(rawId),
+    type: assertion.type,
+    authData: webAuthnBtoA(String.fromCharCode(...authData)),
+    clientData: webAuthnBtoA(String.fromCharCode(...clientDataJSON)),
+    signature: hexEncode(sig),
+    assertionClientExtensions: JSON.stringify(assertionClientExtensions),
+  };
+};
 
 const transformCredentialOptions = (credentialOptions) => {
-    let {challenge, user} = credentialOptions;
-    user.id = Uint8Array.from(credentialOptions.user.id, c => c.charCodeAt(0));
-    challenge = Uint8Array.from(credentialOptions.challenge, c => c.charCodeAt(0));
+  let {challenge, user} = credentialOptions;
+  user.id = Uint8Array.from(credentialOptions.user.id, c => c.charCodeAt(0));
+  challenge = Uint8Array.from(credentialOptions.challenge, c => c.charCodeAt(0));
 
-    const transformedOptions = Object.assign({}, credentialOptions, {challenge, user});
+  const transformedOptions = Object.assign({}, credentialOptions, {challenge, user});
 
-    return transformedOptions;
-}
+  return transformedOptions;
+};
 
 const transformCredential = (credential) => {
-    const attObj = new Uint8Array(credential.response.attestationObject);
-    const clientDataJSON = new Uint8Array(credential.response.clientDataJSON);
-    const rawId = new Uint8Array(credential.rawId);
-    const registrationClientExtensions = credential.getClientExtensionResults();
+  const attObj = new Uint8Array(credential.response.attestationObject);
+  const clientDataJSON = new Uint8Array(credential.response.clientDataJSON);
+  const rawId = new Uint8Array(credential.rawId);
+  const registrationClientExtensions = credential.getClientExtensionResults();
 
-    return {
-        id: credential.id,
-        rawId: webAuthnBtoA(rawId),
-        type: credential.type,
-        attObj: webAuthnBtoA(String.fromCharCode(...attObj)),
-        clientData: webAuthnBtoA(String.fromCharCode(...clientDataJSON)),
-        registrationClientExtensions: JSON.stringify(registrationClientExtensions),
-    };
-}
+  return {
+    id: credential.id,
+    rawId: webAuthnBtoA(rawId),
+    type: credential.type,
+    attObj: webAuthnBtoA(String.fromCharCode(...attObj)),
+    clientData: webAuthnBtoA(String.fromCharCode(...clientDataJSON)),
+    registrationClientExtensions: JSON.stringify(registrationClientExtensions),
+  };
+};
 
 const postCredential = async (credential, token) => {
-    const formData = new FormData();
-    formData.set("credential", JSON.stringify(credential));
-    formData.set("csrf_token", token);
+  const formData = new FormData();
+  formData.set("credential", JSON.stringify(credential));
+  formData.set("csrf_token", token);
 
-    const resp = await fetch(
-        "/manage/account/webauthn-provision/validate", {
-            method: "POST",
-            cache: "no-cache",
-            body: formData,
-        }
-    );
+  const resp = await fetch(
+    "/manage/account/webauthn-provision/validate", {
+      method: "POST",
+      cache: "no-cache",
+      body: formData,
+    }
+  );
 
-    return await resp.json();
-}
+  return await resp.json();
+};
 
 const postAssertion = async (assertion, token) => {
-    const formData = new FormData();
-    formData.set("credential", JSON.stringify(assertion));
-    formData.set("csrf_token", token);
+  const formData = new FormData();
+  formData.set("credential", JSON.stringify(assertion));
+  formData.set("csrf_token", token);
 
-    const resp = await fetch(
-        "/accounts/webauthn-authenticate/validate" + window.location.search, {
-            method: "POST",
-            cache: "no-cache",
-            body: formData,
-        }
-    );
+  const resp = await fetch(
+    "/accounts/webauthn-authenticate/validate" + window.location.search, {
+      method: "POST",
+      cache: "no-cache",
+      body: formData,
+    }
+  );
 
-    return await resp.json();
-}
+  return await resp.json();
+};
 
 export const ProvisionWebAuthn = () => {
-    doWebAuthn("webauthn-provision-begin", async (csrfToken) => {
-        // TODO(ww): Should probably find a way to use the route string here,
-        // not the actual endpoint.
-        const resp = await fetch(
-            "/manage/account/webauthn-provision/options", {
-                cache: "no-cache",
-            }
-        );
+  doWebAuthn("webauthn-provision-begin", async (csrfToken) => {
+    // TODO(ww): Should probably find a way to use the route string here,
+    // not the actual endpoint.
+    const resp = await fetch(
+      "/manage/account/webauthn-provision/options", {
+        cache: "no-cache",
+      }
+    );
 
-        const credentialOptions = await resp.json();
-        const transformedOptions = transformCredentialOptions(credentialOptions);
-        const credential = await navigator.credentials.create({
-            publicKey: transformedOptions,
-        });
-        const transformedCredential = transformCredential(credential);
-
-        const status = await postCredential(transformedCredential, csrfToken);
-        if (status.fail) {
-            populateWebAuthnErrorList(status.fail.errors);
-            return;
-        }
-
-        window.location.replace("/manage/account");
+    const credentialOptions = await resp.json();
+    const transformedOptions = transformCredentialOptions(credentialOptions);
+    const credential = await navigator.credentials.create({
+      publicKey: transformedOptions,
     });
+    const transformedCredential = transformCredential(credential);
+
+    const status = await postCredential(transformedCredential, csrfToken);
+    if (status.fail) {
+      populateWebAuthnErrorList(status.fail.errors);
+      return;
+    }
+
+    window.location.replace("/manage/account");
+  });
 };
 
 export const AuthenticateWebAuthn = () => {
-    doWebAuthn("webauthn-auth-begin", async (csrfToken) => {
-        const resp = await fetch(
-            "/accounts/webauthn-authenticate/options" + window.location.search, {
-                cache: "no-cache",
-            }
-        );
+  doWebAuthn("webauthn-auth-begin", async (csrfToken) => {
+    const resp = await fetch(
+      "/accounts/webauthn-authenticate/options" + window.location.search, {
+        cache: "no-cache",
+      }
+    );
 
-        const assertionOptions = await resp.json();
-        if (assertionOptions.fail) {
-            window.location.replace("/accounts/login");
-            return;
-        }
+    const assertionOptions = await resp.json();
+    if (assertionOptions.fail) {
+      window.location.replace("/accounts/login");
+      return;
+    }
 
-        const transformedOptions = transformAssertionOptions(assertionOptions);
-        const assertion = await navigator.credentials.get({
-            publicKey: transformedOptions,
-        });
-        const transformedAssertion = transformAssertion(assertion);
-
-        const status = await postAssertion(transformedAssertion, csrfToken);
-        if (status.fail) {
-            populateWebAuthnErrorList(status.fail.errors);
-            return;
-        }
-
-        window.location.replace(status.redirect_to);
+    const transformedOptions = transformAssertionOptions(assertionOptions);
+    const assertion = await navigator.credentials.get({
+      publicKey: transformedOptions,
     });
+    const transformedAssertion = transformAssertion(assertion);
+
+    const status = await postAssertion(transformedAssertion, csrfToken);
+    if (status.fail) {
+      populateWebAuthnErrorList(status.fail.errors);
+      return;
+    }
+
+    window.location.replace(status.redirect_to);
+  });
 };
