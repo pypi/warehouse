cd041b4bd219c6acd83a7ab0ea1df43dec95450c
William Woodruff <william@trailofbits.com>
2019-07-18 16:23:52 -0400

    warehouse: Add alert role to WebAuthn provisioning errors

--- warehouse/static/js/warehouse/utils/webauthn.js
+++ warehouse/static/js/warehouse/utils/webauthn.js
@@ -22,6 +22,7 @@ const populateWebAuthnErrorList = (errors) => {
 
   errors.forEach((error) => {
     const errorItem = document.createElement("li");
+    errorItem.setAttribute("role", "alert");
     errorItem.appendChild(document.createTextNode(error));
     errorList.appendChild(errorItem);
   });
