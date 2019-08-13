c6e390c4388ae7d972d228965d595549ffb5402d
William Woodruff <william@trailofbits.com>
2019-08-13 16:45:13 -0400

    warehouse: Auto-format

--- warehouse/manage/forms.py
+++ warehouse/manage/forms.py
@@ -31,9 +31,7 @@ class RoleNameMixin:
 
     role_name = wtforms.SelectField(
         "Select role",
-        choices=[
-            ("", "Select role"), ("Maintainer", "Maintainer"), ("Owner", "Owner")
-        ],
+        choices=[("", "Select role"), ("Maintainer", "Maintainer"), ("Owner", "Owner")],
         validators=[wtforms.validators.DataRequired(message="Select role")],
     )
 
