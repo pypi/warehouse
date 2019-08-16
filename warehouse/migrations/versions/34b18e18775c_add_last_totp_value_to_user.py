d44dbb550bdf6546f3225d8c1bc7eabf8eacc2d5
William Woodruff <william@trailofbits.com>
2019-08-16 10:09:28 -0400

    warehouse: Rebase migration

--- warehouse/migrations/versions/34b18e18775c_add_last_totp_value_to_user.py
+++ warehouse/migrations/versions/34b18e18775c_add_last_totp_value_to_user.py
@@ -13,7 +13,7 @@
 add last totp value to user
 
 Revision ID: 34b18e18775c
-Revises: d83f20495c10
+Revises: 0ac2f506ef2e
 Create Date: 2019-08-15 21:28:47.621282
 """
 
@@ -22,7 +22,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "34b18e18775c"
-down_revision = "d83f20495c10"
+down_revision = "0ac2f506ef2e"
 
 
 def upgrade():
