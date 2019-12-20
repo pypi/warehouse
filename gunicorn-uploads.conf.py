ecb90f7aac30ce07519a259f4ab5546be2d96526
Ernest W Durbin III <ewdurbin@gmail.com>
2019-12-20 11:03:53 -0500

    append .py extension for gunicorn configuration
    as of gunicorn 20.x:
    
      > The configuration file should be a valid Python source file with a python extension (e.g. gunicorn.conf.py).
    
    this complies, and silences warning at boot

--- gunicorn-uploads.conf.py
+++ gunicorn-uploads.conf.py
@@ -1,4 +1,4 @@
-bind = 'unix:/var/run/cabotage/cabotage.sock'
+bind = "unix:/var/run/cabotage/cabotage.sock"
 backlog = 512
 preload_app = True
 max_requests = 32
@@ -8,10 +8,11 @@ worker_connections = 256
 timeout = 240
 keepalive = 2
 
-errorlog = '-'
-loglevel = 'info'
-accesslog = '-'
+errorlog = "-"
+loglevel = "info"
+accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
+
 def when_ready(server):
-    open('/tmp/app-initialized', 'w').close()
+    open("/tmp/app-initialized", "w").close()
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,4 @@
-bind = 'unix:/var/run/cabotage/cabotage.sock'
+bind = "unix:/var/run/cabotage/cabotage.sock"
 backlog = 2048
 preload_app = True
 max_requests = 2048
@@ -8,10 +8,11 @@ worker_connections = 1000
 timeout = 60
 keepalive = 2
 
-errorlog = '-'
-loglevel = 'info'
-accesslog = '-'
+errorlog = "-"
+loglevel = "info"
+accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
+
 def when_ready(server):
-    open('/tmp/app-initialized', 'w').close()
+    open("/tmp/app-initialized", "w").close()
