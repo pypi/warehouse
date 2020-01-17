1ea08ddf44f3351dd9ab854957d91facd53ecba4
Ernest W Durbin III <ewdurbin@gmail.com>
2020-01-17 18:04:42 -0500

    tidy

--- warehouse/accounts/interfaces.py
+++ warehouse/accounts/interfaces.py
@@ -117,6 +117,11 @@ class IUserService(Interface):
         Returns True if the user has a security key provisioned.
         """
 
+    def has_recovery_codes(user_id):
+        """
+        Returns True if the user has at least one valid recovery code.
+        """
+
     def get_totp_secret(user_id):
         """
         Returns the user's TOTP secret as bytes.
