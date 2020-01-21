fed66228b81c12474a160847af650454397414e6
Dave Bonner <bonnerd@media.mit.edu>
2020-01-21 11:15:46 -0500

    change Email.public migration's parent

--- warehouse/migrations/versions/5c029d9ef925_add_email_public_column.py
+++ warehouse/migrations/versions/5c029d9ef925_add_email_public_column.py
@@ -13,7 +13,7 @@
 add email.public column
 
 Revision ID: 5c029d9ef925
-Revises: ee4c59b2ef3a
+Revises: b5bb5d08543d
 Create Date: 2020-01-19 22:25:53.901148
 """
 
@@ -22,7 +22,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "5c029d9ef925"
-down_revision = "ee4c59b2ef3a"
+down_revision = "b5bb5d08543d"
 
 # Note: It is VERY important to ensure that a migration does not lock for a
 #       long period of time and to ensure that each individual migration does
