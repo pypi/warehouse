3c6c050b20ff8a39dced060366011ce6b55eaa6d
Ernest W. Durbin III <ewdurbin@gmail.com>
2020-04-13 13:23:27 -0400

    update head for migration

--- warehouse/migrations/versions/b265ed9eeb8a_fully_deprecate_legacy_distribution_.py
+++ warehouse/migrations/versions/b265ed9eeb8a_fully_deprecate_legacy_distribution_.py
@@ -13,14 +13,14 @@
 Fully deprecate legacy distribution types
 
 Revision ID: b265ed9eeb8a
-Revises: d15f020ee3df
+Revises: c4cb2d15dada
 Create Date: 2020-03-12 17:51:08.447903
 """
 
 from alembic import op
 
 revision = "b265ed9eeb8a"
-down_revision = "d15f020ee3df"
+down_revision = "c4cb2d15dada"
 
 
 def upgrade():
