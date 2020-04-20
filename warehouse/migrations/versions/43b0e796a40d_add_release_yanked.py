b6894f7a7ef8865d6a93fe88f625602addc24744
Dustin Ingram <di@users.noreply.github.com>
2020-04-20 14:37:24 -0500

    Update migration

--- warehouse/migrations/versions/43b0e796a40d_add_release_yanked.py
+++ warehouse/migrations/versions/43b0e796a40d_add_release_yanked.py
@@ -13,7 +13,7 @@
 Add Release.yanked
 
 Revision ID: 43b0e796a40d
-Revises: d15f020ee3df
+Revises: b265ed9eeb8a
 Create Date: 2020-03-13 03:31:03.153039
 """
 
@@ -22,7 +22,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "43b0e796a40d"
-down_revision = "d15f020ee3df"
+down_revision = "b265ed9eeb8a"
 
 
 def upgrade():
