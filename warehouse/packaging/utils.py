9769dc5d28a88175cafddf4f58ffa0893e127249
Ernest W. Durbin III <ewdurbin@gmail.com>
2021-02-09 12:47:34 -0500

    use the existing Jinja2 environment

--- warehouse/packaging/utils.py
+++ warehouse/packaging/utils.py
@@ -1,7 +1,7 @@
 import hashlib
 import os.path
 
-from jinja2 import Environment, FileSystemLoader
+from pyramid_jinja2 import IJinja2Environment
 
 import warehouse
 
@@ -11,15 +11,7 @@ from warehouse.legacy.api.simple import _simple_detail
 def render_simple_detail(project, request, store=False):
     context = _simple_detail(project, request)
 
-    #  TODO: use pyramid_jinja2 "get_jinja2_environment" method instead:
-    #  https://docs.pylonsproject.org/projects/pyramid_jinja2/en/latest/api.html#pyramid_jinja2.get_jinja2_environment
-    dir_name = os.path.join(os.path.dirname(warehouse.__file__), "templates")
-    env = Environment(
-        loader=FileSystemLoader(dir_name),
-        extensions=[],
-        cache_size=0,
-    )
-
+    env = request.registry.queryUtility(IJinja2Environment, name=".jinja2")
     template = env.get_template("legacy/api/simple/detail.html")
     content = template.render(**context, request=request)
 
