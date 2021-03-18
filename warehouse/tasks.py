fa2c5f98d7b2629b131df8e1316932c392e9f07f
Joachim Jablon <ewjoachim@gmail.com>
2021-03-18 08:29:20 +0100

    Remove intermediate function time_ms
    Co-authored-by: Dustin Ingram <di@users.noreply.github.com>

--- warehouse/tasks.py
+++ warehouse/tasks.py
@@ -49,10 +49,6 @@ class TLSRedisBackend(celery.backends.redis.RedisBackend):
         return params
 
 
-def time_ms():
-    return time.time() * 1000
-
-
 class WarehouseTask(celery.Task):
     def __new__(cls, *args, **kwargs):
         obj = super().__new__(cls, *args, **kwargs)
@@ -95,7 +91,7 @@ class WarehouseTask(celery.Task):
             registry = self.app.pyramid_config.registry
             env = pyramid.scripting.prepare(registry=registry)
             env["request"].tm = transaction.TransactionManager(explicit=True)
-            env["request"].timings = {"new_request_start": time_ms()}
+            env["request"].timings = {"new_request_start": time.time() * 1000}
             env["request"].remote_addr = "127.0.0.1"
             self.request.update(pyramid_env=env)
 
