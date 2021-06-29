8060bfa3cb00c7f68bb4b10021b5361e92a04017
Ee Durbin <ewdurbin@gmail.com>
2021-06-29 12:22:32 -0400

    allow the ability to release a prohibited project name to a specified username (#9731)

--- warehouse/admin/views/prohibited_project_names.py
+++ warehouse/admin/views/prohibited_project_names.py
@@ -136,6 +136,69 @@ def confirm_prohibited_project_names(request):
 
 
 @view_config(
+    route_name="admin.prohibited_project_names.release",
+    permission="admin",
+    request_method="POST",
+    uses_session=True,
+    require_methods=False,
+)
+def release_prohibited_project_name(request):
+    project_name = request.POST.get("project_name")
+    if project_name is None:
+        request.session.flash("Provide a project name", queue="error")
+        return HTTPSeeOther(request.current_route_path())
+
+    prohibited_project_name = (
+        request.db.query(ProhibitedProjectName)
+        .filter(ProhibitedProjectName.name == func.normalize_pep426_name(project_name))
+        .first()
+    )
+    if prohibited_project_name is None:
+        request.session.flash(
+            f"{project_name!r} does not exist on prohibited project name list.",
+            queue="error",
+        )
+        return HTTPSeeOther(request.current_route_path())
+
+    project = (
+        request.db.query(Project)
+        .filter(Project.normalized_name == func.normalize_pep426_name(project_name))
+        .first()
+    )
+    if project is not None:
+        request.session.flash(
+            f"{project_name!r} exists and is not on the prohibited project name list.",
+            queue="error",
+        )
+        return HTTPSeeOther(request.current_route_path())
+
+    username = request.POST.get("username")
+    if not username:
+        request.session.flash("Provide a username", queue="error")
+        return HTTPSeeOther(request.current_route_path())
+
+    user = request.db.query(User).filter(User.username == username).first()
+    if user is None:
+        request.session.flash(f"Unknown username '{username}'", queue="error")
+        return HTTPSeeOther(request.current_route_path())
+
+    project = Project(name=project_name)
+    request.db.add(project)
+    request.db.add(Role(project=project, user=user, role_name="Owner"))
+    request.db.delete(prohibited_project_name)
+
+    request.session.flash(
+        f"{project.name!r} released to {user.username!r}.", queue="success"
+    )
+
+    request.db.flush()
+
+    return HTTPSeeOther(
+        request.route_path("admin.project.detail", project_name=project.normalized_name)
+    )
+
+
+@view_config(
     route_name="admin.prohibited_project_names.add",
     permission="admin",
     request_method="POST",
