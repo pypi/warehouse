3c54782dd542019c5fd4942861de060d5b421b78
Jake Sanders <dekka206@gmail.com>
2021-07-26 07:32:10 -0700

    Add package vulnerability reporting API (#9552)
    * Add package vulnerability reporting API
    
    Signed-off-by: Jake Sanders <jsand@google.com>
    
    * genericize vuln report handling, split osv into a subpackage
    
    Signed-off-by: Jake Sanders <jsand@google.com>
    
    * use kwargs instead of positionals for VulnerabilityReportRequest constructor
    
    Signed-off-by: Jake Sanders <jsand@google.com>
    
    * use tags rather than namespacing metrics by origin
    
    * also load all releases when loading vulnerability records
    
    * add description for vuln report model columns
    
    * remove comments from vulnerability migration
    
    Signed-off-by: Jake Sanders <jsand@google.com>
    
    * instrument `_analyze_vulnerability`
    
    Signed-off-by: Jake Sanders <jsand@google.com>
    
    Co-authored-by: Dustin Ingram <di@users.noreply.github.com>

---
+++ tests/unit/integration/vulnerabilities/test_tasks.py
@@ -0,0 +1,41 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pretend
+
+from warehouse.integrations.vulnerabilities import tasks, utils
+
+
+def test_analyze_disclosure_task(monkeypatch):
+    analyze_vulnerability = pretend.call_recorder(lambda *a, **k: None)
+    monkeypatch.setattr(utils, "analyze_vulnerability", analyze_vulnerability)
+
+    request = pretend.stub()
+    vulnerability_record = pretend.stub()
+    origin = pretend.stub()
+    metrics = pretend.stub()
+
+    tasks.analyze_vulnerability_task(
+        request=request,
+        vulnerability_record=vulnerability_record,
+        origin=origin,
+        metrics=metrics,
+    )
+
+    assert analyze_vulnerability.calls == [
+        pretend.call(
+            request=request,
+            vulnerability_record=vulnerability_record,
+            origin=origin,
+            metrics=metrics,
+        )
+    ]
