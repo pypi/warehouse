800aab3dd23f57ac54291e8f068b53b609f6e35f
Dustin Ingram <di@users.noreply.github.com>
2021-07-29 18:24:03 -0400

    Don't attempt to serialize the metrics object w/ the task

--- warehouse/integrations/vulnerabilities/tasks.py
+++ warehouse/integrations/vulnerabilities/tasks.py
@@ -13,10 +13,12 @@
 
 from warehouse import tasks
 from warehouse.integrations.vulnerabilities import utils
+from warehouse.metrics import IMetricsService
 
 
 @tasks.task(ignore_result=True, acks_late=True)
-def analyze_vulnerability_task(request, vulnerability_report, origin, metrics):
+def analyze_vulnerability_task(request, vulnerability_report, origin):
+    metrics = request.find_service(IMetricsService, context=None)
     utils.analyze_vulnerability(
         request=request,
         vulnerability_report=vulnerability_report,
