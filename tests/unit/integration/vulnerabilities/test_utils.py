dee316dab36040a13be7c89c5c0d877eea0232e0
Diego Ramirez <dr01191115@gmail.com>
2021-09-16 15:36:02 -0500

    Update test_utils.py
    Remove the "unknown error" metric.

--- tests/unit/integration/vulnerabilities/test_utils.py
+++ tests/unit/integration/vulnerabilities/test_utils.py
@@ -311,10 +311,6 @@ def test_analyze_vulnerability_release_not_found(db_request, metrics):
             "warehouse.vulnerabilities.error.release_not_found",
             ("origin:test_report_source",),
         ): 2,
-        (
-            "warehouse.vulnerabilities.error.unknown",
-            ("origin:test_report_source",),
-        ): 1,
     }
 
 
