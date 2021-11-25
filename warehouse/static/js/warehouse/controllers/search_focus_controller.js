52b38a8aaa27dae14e629295dcc77eaf35bccc06
Mike Fiedler <miketheman@gmail.com>
2021-11-24 21:30:32 -0500

    feat: add search focus to base template
    In order to prevent the feature from taking over other input fields,
    only activate when NOT in an input field, so the `/` key can be used in
    any input field and not interfere with inputs.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- warehouse/static/js/warehouse/controllers/search_focus_controller.js
+++ warehouse/static/js/warehouse/controllers/search_focus_controller.js
@@ -18,7 +18,9 @@ export default class extends Controller {
   static targets = ["searchField"];
 
   focusSearchField(event) {
-    if (event.code === "Slash") {
+    // When we receive a keydown event, check if it's `/` and that we're not
+    // already focused on the search field, or any other input field.
+    if (event.code === "Slash" && event.target.tagName !== "INPUT") {
       // Prevent the key from being handled as an actual input
       event.preventDefault();
       this.searchFieldTarget.focus();
