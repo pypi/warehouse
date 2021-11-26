9e77a69caa4fe59587a697da057e928654c1a2d6
Mike Fiedler <miketheman@gmail.com>
2021-11-25 23:57:29 -0500

    Fix DeprecationWarning for jinja.contextfilter
    A few instances of this warning are raised:
    
        DeprecationWarning: 'contextfilter' is renamed to 'pass_context',
        the old name will be removed in Jinja 3.1.
    
    Replace the usages accordingly.
    
    Refs: https://github.com/pallets/jinja/pull/1389
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- warehouse/i18n/filters.py
+++ warehouse/i18n/filters.py
@@ -19,26 +19,26 @@ import jinja2
 from pyramid.threadlocal import get_current_request
 
 
-@jinja2.contextfilter
+@jinja2.pass_context
 def format_date(ctx, *args, **kwargs):
     request = ctx.get("request") or get_current_request()
     kwargs.setdefault("locale", request.locale)
     return babel.dates.format_date(*args, **kwargs)
 
 
-@jinja2.contextfilter
+@jinja2.pass_context
 def format_datetime(ctx, *args, **kwargs):
     request = ctx.get("request") or get_current_request()
     kwargs.setdefault("locale", request.locale)
     return babel.dates.format_datetime(*args, **kwargs)
 
 
-@jinja2.contextfilter
+@jinja2.pass_context
 def format_rfc822_datetime(ctx, dt, *args, **kwargs):
     return email.utils.formatdate(dt.timestamp(), usegmt=True)
 
 
-@jinja2.contextfilter
+@jinja2.pass_context
 def format_number(ctx, number, locale=None):
     request = ctx.get("request") or get_current_request()
     if locale is None:
