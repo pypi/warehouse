0481ad7cd619017447986686bf5f1d94f1fa53e1
Dustin Ingram <di@users.noreply.github.com>
2022-01-21 18:04:59 -0500

    Remove admin-specific login/logout routes

--- tests/unit/admin/views/test_core.py
+++ tests/unit/admin/views/test_core.py
@@ -15,21 +15,6 @@ import pretend
 from warehouse.admin.views import core as views
 
 
-class TestForbidden:
-    def test_calls_real(self, monkeypatch):
-        response = pretend.stub()
-        forbidden_view = pretend.call_recorder(lambda *a, **kw: response)
-        monkeypatch.setattr(views, "forbidden_view", forbidden_view)
-
-        exc = pretend.stub()
-        request = pretend.stub()
-
-        assert views.forbidden(exc, request) is response
-        assert forbidden_view.calls == [
-            pretend.call(exc, request, redirect_to="admin.login")
-        ]
-
-
 class TestDashboard:
     def test_dashboard(self):
         assert views.dashboard(pretend.stub()) == {}
