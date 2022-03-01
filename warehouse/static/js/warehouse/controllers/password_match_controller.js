4c8c8daa05ad91507717360974bd2db7f60a90f0
Manoj Mohan <haimanojkumar@gmail.com>
2022-03-01 17:04:14 +0000

    Issue #9203 Disable mismatch warning when confirm password field is empty

--- warehouse/static/js/warehouse/controllers/password_match_controller.js
+++ warehouse/static/js/warehouse/controllers/password_match_controller.js
@@ -22,7 +22,7 @@ export default class extends Controller {
   }
 
   checkPasswordsMatch() {
-    if (this.passwordMatchTargets.every(field => field.value === "")) {
+    if (this.passwordMatchTargets.some(field => field.value === "")) {
       this.matchMessageTarget.classList.add("hidden");
       this.submitTarget.setAttribute("disabled", "");
     } else {
