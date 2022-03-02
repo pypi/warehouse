bff885c378150101402ee5c149a198eb52512e42
Dustin Ingram <di@users.noreply.github.com>
2022-03-01 23:32:55 -0500

    Add critical project notification email

--- warehouse/email/__init__.py
+++ warehouse/email/__init__.py
@@ -784,6 +784,11 @@ def send_oidc_provider_removed_email(request, user, project_name, provider):
     }
 
 
+@_email("two-factor-mandate")
+def send_two_factor_mandate_email(request, user):
+    return {"username": user.username, "has_two_factor": user.has_two_factor}
+
+
 def includeme(config):
     email_sending_class = config.maybe_dotted(config.registry.settings["mail.backend"])
     config.register_service_factory(email_sending_class.create_service, IEmailSender)
