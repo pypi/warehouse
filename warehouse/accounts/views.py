pypi/warehouse

commit a35c7589d243a71753d9cb1f246989c4f566a95c

Update itsdangerous to 2.1.0 (#10846)
* Bump itsdangerous from 1.1.0 to 2.1.0

Bumps [itsdangerous](https://github.com/pallets/itsdangerous) from 1.1.0 to 2.1.0.
- [Release notes](https://github.com/pallets/itsdangerous/releases)
- [Changelog](https://github.com/pallets/itsdangerous/blob/main/CHANGES.rst)
- [Commits](https://github.com/pallets/itsdangerous/compare/1.1.0...2.1.0)

---
updated-dependencies:
- dependency-name: itsdangerous
  dependency-type: direct:production
  update-type: version-update:semver-major
...

Signed-off-by: dependabot[bot] <support@github.com>

* Have User.last_login use a datetime acting as non-naive in business logic

  * Add test that triggers last_login check during 2fa_validation

  * update translations

* Have reset-password token compatibility with those generated before itsdangerous dependency update to 2.x

* Have User.password_date in localized form as well.

    Handle tokens created with naive representation os User.password_date as well

* Update warehouse/accounts/views.py

Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
Co-authored-by: Ee Durbin <ewdurbin@gmail.com>

warehouse/accounts/views.py
@@ -15,6 +15,8 @@ import hashlib
 import json
 import uuid
 
+import pytz
+
 from first import first
 from pyramid.httpexceptions import (
     HTTPMovedPermanently,
@@ -656,8 +658,12 @@ def reset_password(request, _form_class=ResetPasswordForm):
         return _error(request._("Invalid token: user not found"))
 
     # Check whether the user has logged in since the token was created
-    last_login = data.get("user.last_login")
-    if str(user.last_login) > last_login:
+    last_login = datetime.datetime.fromisoformat(data.get("user.last_login"))
+    # Before updating itsdangerous to 2.x the last_login was naive,
+    # now it's localized to UTC
+    if not last_login.tzinfo:
+        last_login = pytz.UTC.localize(last_login)
+    if user.last_login > last_login:
         # TODO: track and audit this, seems alertable
         return _error(
             request._(
@@ -666,8 +672,12 @@ def reset_password(request, _form_class=ResetPasswordForm):
         )
 
     # Check whether the password has been changed since the token was created
-    password_date = data.get("user.password_date")
-    if str(user.password_date) > password_date:
+    password_date = datetime.datetime.fromisoformat(data.get("user.password_date"))
+    # Before updating itsdangerous to 2.x the password_date was naive,
+    # now it's localized to UTC
+    if not password_date.tzinfo:
+        password_date = pytz.UTC.localize(password_date)
+    if user.password_date > password_date:
         return _error(
             request._(
                 "Invalid token: password has already been changed since this "

warehouse/locale/messages.pot
@@ -41,7 +41,7 @@ msgstr ""
 msgid "The password is invalid. Try again."
 msgstr ""
 
-#: warehouse/accounts/forms.py:146 warehouse/accounts/views.py:79
+#: warehouse/accounts/forms.py:146 warehouse/accounts/views.py:81
 msgid "There have been too many unsuccessful login attempts. Try again later."
 msgstr ""
 
@@ -93,135 +93,135 @@ msgstr ""
 msgid "No user found with that username or email"
 msgstr ""
 
-#: warehouse/accounts/views.py:96
+#: warehouse/accounts/views.py:98
 msgid ""
 "Too many emails have been added to this account without verifying them. "
 "Check your inbox and follow the verification links. (IP: ${ip})"
 msgstr ""
 
-#: warehouse/accounts/views.py:108
+#: warehouse/accounts/views.py:110
 msgid ""
 "Too many password resets have been requested for this account without "
 "completing them. Check your inbox and follow the verification links. (IP:"
 " ${ip})"
 msgstr ""
 
-#: warehouse/accounts/views.py:248 warehouse/accounts/views.py:312
-#: warehouse/accounts/views.py:314 warehouse/accounts/views.py:341
-#: warehouse/accounts/views.py:343 warehouse/accounts/views.py:409
+#: warehouse/accounts/views.py:250 warehouse/accounts/views.py:314
+#: warehouse/accounts/views.py:316 warehouse/accounts/views.py:343
+#: warehouse/accounts/views.py:345 warehouse/accounts/views.py:411
 msgid "Invalid or expired two factor login."
 msgstr ""
 
-#: warehouse/accounts/views.py:306
+#: warehouse/accounts/views.py:308
 msgid "Already authenticated"
 msgstr ""
 
-#: warehouse/accounts/views.py:385
+#: warehouse/accounts/views.py:387
 msgid "Successful WebAuthn assertion"
 msgstr ""
 
-#: warehouse/accounts/views.py:439 warehouse/manage/views.py:791
+#: warehouse/accounts/views.py:441 warehouse/manage/views.py:791
 msgid "Recovery code accepted. The supplied code cannot be used again."
 msgstr ""
 
-#: warehouse/accounts/views.py:525
+#: warehouse/accounts/views.py:527
 msgid ""
 "New user registration temporarily disabled. See https://pypi.org/help"
 "#admin-intervention for details."
 msgstr ""
 
-#: warehouse/accounts/views.py:643
+#: warehouse/accounts/views.py:645
 msgid "Expired token: request a new password reset link"
 msgstr ""
 
-#: warehouse/accounts/views.py:645
+#: warehouse/accounts/views.py:647
 msgid "Invalid token: request a new password reset link"
 msgstr ""
 
-#: warehouse/accounts/views.py:647 warehouse/accounts/views.py:732
-#: warehouse/accounts/views.py:828
+#: warehouse/accounts/views.py:649 warehouse/accounts/views.py:742
+#: warehouse/accounts/views.py:838
 msgid "Invalid token: no token supplied"
 msgstr ""
 
-#: warehouse/accounts/views.py:651
+#: warehouse/accounts/views.py:653
 msgid "Invalid token: not a password reset token"
 msgstr ""
 
-#: warehouse/accounts/views.py:656
+#: warehouse/accounts/views.py:658
 msgid "Invalid token: user not found"
 msgstr ""
 
-#: warehouse/accounts/views.py:663
+#: warehouse/accounts/views.py:669
 msgid "Invalid token: user has logged in since this token was requested"
 msgstr ""
 
-#: warehouse/accounts/views.py:672
+#: warehouse/accounts/views.py:682
 msgid ""
 "Invalid token: password has already been changed since this token was "
 "requested"
 msgstr ""
 
-#: warehouse/accounts/views.py:701
+#: warehouse/accounts/views.py:711
 msgid "You have reset your password"
 msgstr ""
 
-#: warehouse/accounts/views.py:728
+#: warehouse/accounts/views.py:738
 msgid "Expired token: request a new email verification link"
 msgstr ""
 
-#: warehouse/accounts/views.py:730
+#: warehouse/accounts/views.py:740
 msgid "Invalid token: request a new email verification link"
 msgstr ""
 
-#: warehouse/accounts/views.py:736
+#: warehouse/accounts/views.py:746
 msgid "Invalid token: not an email verification token"
 msgstr ""
 
-#: warehouse/accounts/views.py:745
+#: warehouse/accounts/views.py:755
 msgid "Email not found"
 msgstr ""
 
-#: warehouse/accounts/views.py:748
+#: warehouse/accounts/views.py:758
 msgid "Email already verified"
 msgstr ""
 
-#: warehouse/accounts/views.py:763
+#: warehouse/accounts/views.py:773
 msgid "You can now set this email as your primary address"
 msgstr ""
 
-#: warehouse/accounts/views.py:767
+#: warehouse/accounts/views.py:777
 msgid "This is your primary address"
 msgstr ""
 
-#: warehouse/accounts/views.py:772
+#: warehouse/accounts/views.py:782
 msgid "Email address ${email_address} verified. ${confirm_message}."
 msgstr ""
 
-#: warehouse/accounts/views.py:824
+#: warehouse/accounts/views.py:834
 msgid "Expired token: request a new project role invite"
 msgstr ""
 
-#: warehouse/accounts/views.py:826
+#: warehouse/accounts/views.py:836
 msgid "Invalid token: request a new project role invite"
 msgstr ""
 
-#: warehouse/accounts/views.py:832
+#: warehouse/accounts/views.py:842
 msgid "Invalid token: not a collaboration invitation token"
 msgstr ""
 
-#: warehouse/accounts/views.py:836
+#: warehouse/accounts/views.py:846
 msgid "Role invitation is not valid."
 msgstr ""
 
-#: warehouse/accounts/views.py:851
+#: warehouse/accounts/views.py:861
 msgid "Role invitation no longer exists."
 msgstr ""
 
-#: warehouse/accounts/views.py:863
+#: warehouse/accounts/views.py:873
 msgid "Invitation for '${project_name}' is declined."
 msgstr ""
 
-#: warehouse/accounts/views.py:930
+#: warehouse/accounts/views.py:940
 msgid "You are now ${role} of the '${project_name}' project."
 msgstr ""
 

tests/unit/accounts/test_services.py
@@ -17,6 +17,7 @@ import uuid
 import freezegun
 import pretend
 import pytest
+import pytz
 import requests
 
 from webauthn.helpers import bytes_to_base64url
@@ -933,7 +934,7 @@ class TestTokenService:
         assert token_service.loads(token) == {"foo": "bar"}
 
     def test_loads_return_timestamp(self, token_service):
-        sign_time = datetime.datetime.utcnow()
+        sign_time = pytz.UTC.localize(datetime.datetime.utcnow())
         with freezegun.freeze_time(sign_time):
             token = token_service.dumps({"foo": "bar"})
 

tests/unit/accounts/test_views.py
@@ -17,6 +17,7 @@ import uuid
 import freezegun
 import pretend
 import pytest
+import pytz
 
 from pyramid.httpexceptions import HTTPMovedPermanently, HTTPSeeOther
 from sqlalchemy.orm.exc import NoResultFound
@@ -37,6 +38,8 @@ from warehouse.accounts.interfaces import (
     TooManyFailedLogins,
     TooManyPasswordResetRequests,
 )
+from warehouse.accounts.models import User
+from warehouse.accounts.views import two_factor_and_totp_validate
 from warehouse.admin.flags import AdminFlag, AdminFlagValue
 from warehouse.packaging.models import Role, RoleInvitation
 from warehouse.rate_limiting.interfaces import IRateLimiter
@@ -413,6 +416,53 @@ class TestTwoFactor:
 
         with pytest.raises(TokenInvalid):
             views._get_two_factor_data(pyramid_request)
+
+    def test_two_factor_and_totp_validate_redirect_to_account_login(
+        self,
+        db_request,
+        token_service,
+        user_service,
+    ):
+        """
+        Checks redirect to the login page if the 2fa login got expired.
+
+        Given there's user in the database and has a token signed before last_login date
+        When the user calls accounts.two-factor view
+        Then the user is redirected to account/login page
+
+        ... warning::
+            This test has to use database and load the user from database
+            to make sure we always compare user.last_login as timezone-aware datetime.
+
+        """
+        user = User(
+            username="jdoe",
+            name="Joe",
+            password="any",
+            is_active=True,
+            last_login=datetime.datetime.utcnow() + datetime.timedelta(days=+1),
+        )
+        db_request.db.add(user)
+        db_request.db.commit()
+        # Make sure object is not in session,
+        # so sqlalchemy loads it fresh from database and type works it's magic
+        db_request.db.expunge(user)
+
+        token_data = {"userid": user.id}
+        token = token_service.dumps(token_data)
+        db_request.query_string = token
+        db_request.find_service = lambda interface, **kwargs: {
+            ITokenService: token_service,
+            IUserService: user_service,
+        }[interface]
+        db_request.route_path = pretend.call_recorder(lambda name: "/account/login/")
+
+        two_factor_and_totp_validate(db_request)
+        # This view is redirected to only during a TokenException recovery
+        # which is called in two instances:
+        # 1. No userid in token
+        # 2. The token has expired
+        assert db_request.route_path.calls == [pretend.call("accounts.login")]
 
     @pytest.mark.parametrize("redirect_url", [None, "/foo/bar/", "/wat/"])
     def test_get_returns_totp_form(self, pyramid_request, redirect_url):
@@ -1682,7 +1732,8 @@ class TestRequestPasswordReset:
 
 
 class TestResetPassword:
-    def test_get(self, db_request, user_service, token_service):
+    @pytest.mark.parametrize("dates_utc", (True, False))
+    def test_get(self, db_request, user_service, token_service, dates_utc):
         user = UserFactory.create()
         form_inst = pretend.stub()
         form_class = pretend.call_recorder(lambda *args, **kwargs: form_inst)
@@ -1690,12 +1741,18 @@ class TestResetPassword:
         breach_service = pretend.stub(check_password=lambda pw: False)
 
         db_request.GET.update({"token": "RANDOM_KEY"})
+        last_login = str(
+            user.last_login if dates_utc else user.last_login.replace(tzinfo=None)
+        )
+        password_date = str(
+            user.password_date if dates_utc else user.password_date.replace(tzinfo=None)
+        )
         token_service.loads = pretend.call_recorder(
             lambda token: {
                 "action": "password-reset",
                 "user.id": str(user.id),
-                "user.last_login": str(user.last_login),
-                "user.password_date": str(user.password_date),
+                "user.last_login": last_login,
+                "user.password_date": password_date,
             }
         )
         db_request.find_service = pretend.call_recorder(
@@ -1881,7 +1938,7 @@ class TestResetPassword:
         assert user_service.get_user.calls == [pretend.call(uuid.UUID(data["user.id"]))]
 
     def test_reset_password_last_login_changed(self, pyramid_request):
-        now = datetime.datetime.utcnow()
+        now = pytz.UTC.localize(datetime.datetime.utcnow())
         later = now + datetime.timedelta(hours=1)
         data = {
             "action": "password-reset",
@@ -1913,7 +1970,7 @@ class TestResetPassword:
         ]
 
     def test_reset_password_password_date_changed(self, pyramid_request):
-        now = datetime.datetime.utcnow()
+        now = pytz.UTC.localize(datetime.datetime.utcnow())
         later = now + datetime.timedelta(hours=1)
         data = {
             "action": "password-reset",

tests/unit/utils/db/test_types.py
@@ -1,0 +1,57 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from datetime import datetime
+
+import pytz
+
+from sqlalchemy.dialects.postgresql import dialect
+
+from warehouse.utils.db.types import TZDateTime
+
+
+def test_tzdatetime_bind_nonnaive_datetime():
+    dt_field = TZDateTime()
+    utc_datetime = pytz.UTC.localize(datetime.utcnow())
+    assert utc_datetime.tzinfo is not None
+
+    naive_datetime = dt_field.process_bind_param(utc_datetime, dialect)
+    assert naive_datetime
+    assert naive_datetime.tzinfo is None
+
+
+def test_tzdatetime_bind_nonnaive_datetime_naive():
+    dt_field = TZDateTime()
+    naive_datetime = datetime.utcnow()
+    assert naive_datetime.tzinfo is None
+
+    assert dt_field.process_bind_param(naive_datetime, dialect) is naive_datetime
+
+
+def test_tzdatetime_bind_nonnaive_datetime_none():
+    dt_field = TZDateTime()
+
+    assert dt_field.process_bind_param(None, dialect) is None
+
+
+def test_tzdatetime_process_result_value():
+    dt_field = TZDateTime()
+    naive_datetime = datetime.utcnow()
+
+    utc_datetime = dt_field.process_result_value(naive_datetime, dialect)
+    assert utc_datetime
+    assert utc_datetime.tzinfo is not None
+
+
+def test_tzdatetime_process_result_value_none():
+    dt_field = TZDateTime()
+
+    assert dt_field.process_result_value(None, dialect) is None

warehouse/accounts/models.py
@@ -37,6 +37,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from warehouse import db
 from warehouse.sitemap.models import SitemapMixin
 from warehouse.utils.attrs import make_repr
+from warehouse.utils.db.types import TZDateTime
 
 
 class UserFactory:
@@ -71,7 +72,7 @@ class User(SitemapMixin, db.Model):
     username = Column(CIText, nullable=False, unique=True)
     name = Column(String(length=100), nullable=False)
     password = Column(String(length=128), nullable=False)
-    password_date = Column(DateTime, nullable=True, server_default=sql.func.now())
+    password_date = Column(TZDateTime, nullable=True, server_default=sql.func.now())
     is_active = Column(Boolean, nullable=False, server_default=sql.false())
     is_superuser = Column(Boolean, nullable=False, server_default=sql.false())
     is_moderator = Column(Boolean, nullable=False, server_default=sql.false())
@@ -80,7 +81,7 @@ class User(SitemapMixin, db.Model):
         Boolean, nullable=False, server_default=sql.false()
     )
     date_joined = Column(DateTime, server_default=sql.func.now())
-    last_login = Column(DateTime, nullable=False, server_default=sql.func.now())
+    last_login = Column(TZDateTime, nullable=False, server_default=sql.func.now())
     disabled_for = Column(
         Enum(DisableReason, values_callable=lambda x: [e.value for e in x]),
         nullable=True,

warehouse/utils/db/types.py
@@ -1,0 +1,30 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import datetime
+
+from sqlalchemy import DateTime, TypeDecorator
+
+
+class TZDateTime(TypeDecorator):
+    impl = DateTime
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            if value.tzinfo:
+                value = value.astimezone(datetime.timezone.utc).replace(tzinfo=None)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = value.replace(tzinfo=datetime.timezone.utc)
+        return value

commit 79adff84a09d5a1b09488058596295914452de8d

Fix error with password reset when password_date is NULL
Introduced in #10846

Resolves https://sentry.io/share/issue/2da77324e51d4d28bee1b53c782f2a1f/

warehouse/accounts/views.py
@@ -677,7 +677,12 @@ def reset_password(request, _form_class=ResetPasswordForm):
     # now it's localized to UTC
     if not password_date.tzinfo:
         password_date = pytz.UTC.localize(password_date)
-    if user.password_date > password_date:
+    current_password_date = (
+        user.password_date
+        if user.password_date is not None
+        else datetime.datetime.min.replace(tzinfo=pytz.UTC)
+    )
+    if current_password_date > password_date:
         return _error(
             request._(
                 "Invalid token: password has already been changed since this "

warehouse/email/__init__.py
@@ -15,6 +15,8 @@ import functools
 
 from email.headerregistry import Address
 
+import pytz
+
 from celery.schedules import crontab
 from first import first
 from sqlalchemy.orm.exc import NoResultFound
@@ -193,7 +195,11 @@ def send_password_reset_email(request, user_and_email):
             "action": "password-reset",
             "user.id": str(user.id),
             "user.last_login": str(user.last_login),
-            "user.password_date": str(user.password_date),
+            "user.password_date": str(
+                user.password_date
+                if user.password_date is not None
+                else datetime.datetime.min.replace(tzinfo=pytz.UTC)
+            ),
         }
     )
 

warehouse/locale/messages.pot
@@ -138,8 +138,8 @@ msgstr ""
 msgid "Invalid token: request a new password reset link"
 msgstr ""
 
-#: warehouse/accounts/views.py:649 warehouse/accounts/views.py:742
-#: warehouse/accounts/views.py:838
+#: warehouse/accounts/views.py:649 warehouse/accounts/views.py:747
+#: warehouse/accounts/views.py:843
 msgid "Invalid token: no token supplied"
 msgstr ""
 
@@ -155,73 +155,73 @@ msgstr ""
 msgid "Invalid token: user has logged in since this token was requested"
 msgstr ""
 
-#: warehouse/accounts/views.py:682
+#: warehouse/accounts/views.py:687
 msgid ""
 "Invalid token: password has already been changed since this token was "
 "requested"
 msgstr ""
 
-#: warehouse/accounts/views.py:711
+#: warehouse/accounts/views.py:716
 msgid "You have reset your password"
 msgstr ""
 
-#: warehouse/accounts/views.py:738
+#: warehouse/accounts/views.py:743
 msgid "Expired token: request a new email verification link"
 msgstr ""
 
-#: warehouse/accounts/views.py:740
+#: warehouse/accounts/views.py:745
 msgid "Invalid token: request a new email verification link"
 msgstr ""
 
-#: warehouse/accounts/views.py:746
+#: warehouse/accounts/views.py:751
 msgid "Invalid token: not an email verification token"
 msgstr ""
 
-#: warehouse/accounts/views.py:755
+#: warehouse/accounts/views.py:760
 msgid "Email not found"
 msgstr ""
 
-#: warehouse/accounts/views.py:758
+#: warehouse/accounts/views.py:763
 msgid "Email already verified"
 msgstr ""
 
-#: warehouse/accounts/views.py:773
+#: warehouse/accounts/views.py:778
 msgid "You can now set this email as your primary address"
 msgstr ""
 
-#: warehouse/accounts/views.py:777
+#: warehouse/accounts/views.py:782
 msgid "This is your primary address"
 msgstr ""
 
-#: warehouse/accounts/views.py:782
+#: warehouse/accounts/views.py:787
 msgid "Email address ${email_address} verified. ${confirm_message}."
 msgstr ""
 
-#: warehouse/accounts/views.py:834
+#: warehouse/accounts/views.py:839
 msgid "Expired token: request a new project role invite"
 msgstr ""
 
-#: warehouse/accounts/views.py:836
+#: warehouse/accounts/views.py:841
 msgid "Invalid token: request a new project role invite"
 msgstr ""
 
-#: warehouse/accounts/views.py:842
+#: warehouse/accounts/views.py:847
 msgid "Invalid token: not a collaboration invitation token"
 msgstr ""
 
-#: warehouse/accounts/views.py:846
+#: warehouse/accounts/views.py:851
 msgid "Role invitation is not valid."
 msgstr ""
 
-#: warehouse/accounts/views.py:861
+#: warehouse/accounts/views.py:866
 msgid "Role invitation no longer exists."
 msgstr ""
 
-#: warehouse/accounts/views.py:873
+#: warehouse/accounts/views.py:878
 msgid "Invitation for '${project_name}' is declined."
 msgstr ""
 
-#: warehouse/accounts/views.py:940
+#: warehouse/accounts/views.py:945
 msgid "You are now ${role} of the '${project_name}' project."
 msgstr ""
 