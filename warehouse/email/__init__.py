6a6c0656fe4bd72404a1ec9c8610e7a87dd03afc
Chris Lei <chris@divbzero.com>
2022-04-08 12:20:58 -0700

    admin-new-organization-declined email template

--- warehouse/email/__init__.py
+++ warehouse/email/__init__.py
@@ -210,6 +210,17 @@ def send_admin_new_organization_approved_email(
     }
 
 
+@_email("admin-new-organization-declined")
+def send_admin_new_organization_declined_email(
+    request, user, *, organization_name, initiator_username, message=""
+):
+    return {
+        "initiator_username": initiator_username,
+        "message": message,
+        "organization_name": organization_name,
+    }
+
+
 # Email templates for users.
 
 
