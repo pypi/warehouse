063af8ff24270fb4650654a4b3ca2a2ad1f15324
Dustin Ingram <di@users.noreply.github.com>
2022-06-16 05:12:48 +0000

    Fix settings access

--- warehouse/views.py
+++ warehouse/views.py
@@ -539,14 +539,18 @@ class SecurityKeyGiveaway:
 
     @view_config(request_method="GET")
     def security_key_giveaway(self):
-        if not self.request.registry.settings("warehouse.two_factor_mandate.available"):
+        if not self.request.registry.settings.get(
+            "warehouse.two_factor_mandate.available"
+        ):
             raise HTTPNotFound
 
         return self.default_response
 
     @view_config(request_method="POST")
     def security_key_giveaway_submit(self):
-        if not self.request.registry.settings("warehouse.two_factor_mandate.available"):
+        if not self.request.registry.settings.get(
+            "warehouse.two_factor_mandate.available"
+        ):
             raise HTTPNotFound
 
         default_response = self.default_response
