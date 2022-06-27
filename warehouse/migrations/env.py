f86e35e11c94c78f90dcec661bb91ccc8f728b92
Donald Stufft <donald@stufft.io>
2022-06-27 18:38:28 -0400

    Timeout migrations that take too long to run

--- warehouse/migrations/env.py
+++ warehouse/migrations/env.py
@@ -50,6 +50,9 @@ def run_migrations_online():
         connectable = create_engine(url, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
+        connection.execute("SET statement_timeout = 5000")
+        connection.execute("SET lock_timeout = 4000")
+
         context.configure(
             connection=connection,
             target_metadata=db.metadata,
