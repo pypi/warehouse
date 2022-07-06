b3c9507ba7f8721a6e539a560547eff60a0ded8f
Chris Lei <chris@divbzero.com>
2022-07-06 15:03:48 -0700

    Fix tests after merge branch 'main'

--- tests/unit/manage/test_views.py
+++ tests/unit/manage/test_views.py
@@ -69,6 +69,7 @@ from ...common.db.accounts import EmailFactory
 from ...common.db.organizations import (
     OrganizationFactory,
     OrganizationInvitationFactory,
+    OrganizationProjectFactory,
     OrganizationRoleFactory,
 )
 from ...common.db.packaging import (
@@ -2632,7 +2633,9 @@ class TestManageOrganizationSettings:
         self, db_request, organization_service, enable_organizations, monkeypatch
     ):
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory.create(
+            organization=organization, project=ProjectFactory.create()
+        )
 
         save_organization_obj = pretend.stub()
         save_organization_cls = pretend.call_recorder(
@@ -2943,7 +2946,10 @@ class TestManageOrganizationSettings:
         monkeypatch,
     ):
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory.create(
+            organization=organization, project=ProjectFactory.create()
+        )
+
         db_request.POST = {"confirm_organization_name": organization.name}
         db_request.route_path = pretend.call_recorder(
             lambda *a, **kw: "/manage/organizations/"
@@ -2986,7 +2992,9 @@ class TestManageOrganizationProjects:
         monkeypatch,
     ):
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory.create(
+            organization=organization, project=ProjectFactory.create()
+        )
 
         add_organization_project_obj = pretend.stub()
         add_organization_project_cls = pretend.call_recorder(
@@ -3027,7 +3035,9 @@ class TestManageOrganizationProjects:
         monkeypatch,
     ):
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory.create(
+            organization=organization, project=ProjectFactory.create()
+        )
 
         project = ProjectFactory.create()
 
@@ -3049,7 +3059,9 @@ class TestManageOrganizationProjects:
         )
 
         def add_organization_project(*args, **kwargs):
-            organization.projects.append(project)
+            OrganizationProjectFactory.create(
+                organization=organization, project=project
+            )
 
         monkeypatch.setattr(
             organization_service, "add_organization_project", add_organization_project
@@ -3089,7 +3101,9 @@ class TestManageOrganizationProjects:
         monkeypatch,
     ):
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory.create(
+            organization=organization, project=ProjectFactory.create()
+        )
 
         project = ProjectFactory.create()
 
@@ -3110,7 +3124,9 @@ class TestManageOrganizationProjects:
         )
 
         def add_organization_project(*args, **kwargs):
-            organization.projects.append(project)
+            OrganizationProjectFactory.create(
+                organization=organization, project=project
+            )
 
         monkeypatch.setattr(
             organization_service, "add_organization_project", add_organization_project
@@ -3150,7 +3166,9 @@ class TestManageOrganizationProjects:
         monkeypatch,
     ):
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory.create(
+            organization=organization, project=ProjectFactory.create()
+        )
 
         project = ProjectFactory.create()
 
@@ -3172,7 +3190,9 @@ class TestManageOrganizationProjects:
         )
 
         def add_organization_project(*args, **kwargs):
-            organization.projects.append(project)
+            OrganizationProjectFactory.create(
+                organization=organization, project=project
+            )
 
         monkeypatch.setattr(
             organization_service, "add_organization_project", add_organization_project
@@ -3203,7 +3223,9 @@ class TestManageOrganizationProjects:
         db_request.help_url = lambda *a, **kw: ""
 
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory(
+            organization=organization, project=ProjectFactory.create()
+        )
 
         project = ProjectFactory.create()
 
@@ -3231,7 +3253,9 @@ class TestManageOrganizationProjects:
         monkeypatch.setattr(views, "add_project", add_project)
 
         def add_organization_project(*args, **kwargs):
-            organization.projects.append(project)
+            OrganizationProjectFactory.create(
+                organization=organization, project=project
+            )
 
         monkeypatch.setattr(
             organization_service, "add_organization_project", add_organization_project
@@ -3253,7 +3277,6 @@ class TestManageOrganizationProjects:
         assert result.headers["Location"] == db_request.path
         assert validate_project_name.calls == [pretend.call(project.name, db_request)]
         assert add_project.calls == [pretend.call(project.name, db_request)]
-        assert len(add_organization_project_cls.calls) == 1
         assert len(organization.projects) == 2
         assert send_organization_project_added_email.calls == [
             pretend.call(
@@ -3273,7 +3296,9 @@ class TestManageOrganizationProjects:
         monkeypatch,
     ):
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory.create(
+            organization=organization, project=ProjectFactory.create()
+        )
 
         project = ProjectFactory.create()
 
@@ -3324,7 +3349,9 @@ class TestManageOrganizationProjects:
         db_request.help_url = lambda *a, **kw: "help-url"
 
         organization = OrganizationFactory.create()
-        organization.projects = [ProjectFactory.create()]
+        OrganizationProjectFactory.create(
+            organization=organization, project=ProjectFactory.create()
+        )
 
         project = ProjectFactory.create()
 
