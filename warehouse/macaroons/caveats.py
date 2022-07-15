8e378dd19b6a6b83a95eb358c50693d1255c573a
William Woodruff <william@trailofbits.com>
2022-07-15 12:55:17 -0400

    Add a caveat for project IDs (#11857)

--- warehouse/macaroons/caveats.py
+++ warehouse/macaroons/caveats.py
@@ -105,6 +105,32 @@ class ExpiryCaveat(Caveat):
         return True
 
 
+class ProjectIDsCaveat(Caveat):
+    def verify(self, predicate):
+        try:
+            data = json.loads(predicate)
+            project_ids = data["project_ids"]
+        except (KeyError, ValueError, TypeError):
+            self.failure_reason = "malformatted predicate"
+            return False
+
+        if not project_ids:
+            self.failure_reason = "missing fields"
+            return False
+
+        if not isinstance(self.verifier.context, Project):
+            self.failure_reason = (
+                "project-scoped token used outside of a project context"
+            )
+            return False
+
+        if str(self.verifier.context.id) not in project_ids:
+            self.failure_reason = "current project does not matched scoped project IDs"
+            return False
+
+        return True
+
+
 class Verifier:
     def __init__(self, macaroon, context, principals, permission):
         self.macaroon = macaroon
@@ -116,6 +142,7 @@ class Verifier:
     def verify(self, key):
         self.verifier.satisfy_general(V1Caveat(self))
         self.verifier.satisfy_general(ExpiryCaveat(self))
+        self.verifier.satisfy_general(ProjectIDsCaveat(self))
 
         try:
             return self.verifier.verify(self.macaroon, key)
