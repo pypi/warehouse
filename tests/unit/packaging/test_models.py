pypi/warehouse

commit 6cb8bf933ca2d0ef5e66e115a615d3944bfe3d5d

Major Database Refactoring (#4958)
* Migrate to UUID Primary Key for Project and Release models

* fixup factories for Project and Release, sync Role model to migration

* make reformat

* fix at least one test

* Fix fallout of Role.package_name removal

* Remove Denormalized name and version fields from all models (#4974)

* Update for #5001

* Ensure model state matches db state

* Switch to using FKs to User.id isntead of User.username

* Fix test

* Fix linting

* Add Release.uploader as a real FK (#5015)

* Rename a number of tables to better fit in current scheme (#5016)

warehouse/packaging/models.py
@@ -25,7 +25,6 @@ from sqlalchemy import (
     Column,
     Enum,
     ForeignKey,
-    ForeignKeyConstraint,
     Index,
     Boolean,
     DateTime,
@@ -53,20 +52,17 @@ from warehouse.utils.attrs import make_repr
 class Role(db.Model):
 
     __tablename__ = "roles"
-    __table_args__ = (
-        Index("roles_pack_name_idx", "package_name"),
-        Index("roles_user_name_idx", "user_name"),
-    )
+    __table_args__ = (Index("roles_user_id_idx", "user_id"),)
 
     __repr__ = make_repr("role_name", "user_name", "package_name")
 
     role_name = Column(Text)
-    user_name = Column(
-        CIText,
-        ForeignKey("accounts_user.username", onupdate="CASCADE", ondelete="CASCADE"),
+    user_id = Column(
+        ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False
     )
-    package_name = Column(
-        Text, ForeignKey("packages.name", onupdate="CASCADE", ondelete="CASCADE")
+    project_id = Column(
+        ForeignKey("projects.id", onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=False,
     )
 
     user = orm.relationship(User, lazy=False)
@@ -98,19 +94,19 @@ class ProjectFactory:
             raise KeyError from None
 
 
-class Project(SitemapMixin, db.ModelBase):
+class Project(SitemapMixin, db.Model):
 
-    __tablename__ = "packages"
+    __tablename__ = "projects"
     __table_args__ = (
         CheckConstraint(
             "name ~* '^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$'::text",
-            name="packages_valid_name",
+            name="projects_valid_name",
         ),
     )
 
     __repr__ = make_repr("name")
 
-    name = Column(Text, primary_key=True, nullable=False)
+    name = Column(Text, nullable=False)
     normalized_name = orm.column_property(func.normalize_pep426_name(name))
     created = Column(
         DateTime(timezone=False), nullable=False, server_default=sql.func.now()
@@ -231,18 +227,14 @@ class Dependency(db.Model):
 
     __tablename__ = "release_dependencies"
     __table_args__ = (
-        Index("rel_dep_name_version_kind_idx", "name", "version", "kind"),
-        ForeignKeyConstraint(
-            ["name", "version"],
-            ["releases.name", "releases.version"],
-            onupdate="CASCADE",
-            ondelete="CASCADE",
-        ),
+        Index("release_dependencies_release_kind_idx", "release_id", "kind"),
     )
     __repr__ = make_repr("name", "version", "kind", "specifier")
 
-    name = Column(Text)
-    version = Column(Text)
+    release_id = Column(
+        ForeignKey("releases.id", onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=False,
+    )
     kind = Column(Integer)
     specifier = Column(Text)
 
@@ -251,15 +243,13 @@ def _dependency_relation(kind):
     return orm.relationship(
         "Dependency",
         primaryjoin=lambda: sql.and_(
-            Release.name == Dependency.name,
-            Release.version == Dependency.version,
-            Dependency.kind == kind.value,
+            Release.id == Dependency.release_id, Dependency.kind == kind.value
         ),
         viewonly=True,
     )
 
 
-class Release(db.ModelBase):
+class Release(db.Model):
 
     __tablename__ = "releases"
 
@@ -267,20 +257,19 @@ class Release(db.ModelBase):
     def __table_args__(cls):  # noqa
         return (
             Index("release_created_idx", cls.created.desc()),
-            Index("release_name_created_idx", cls.name, cls.created.desc()),
+            Index("release_project_created_idx", cls.project_id, cls.created.desc()),
             Index("release_version_idx", cls.version),
         )
 
-    __repr__ = make_repr("name", "version")
+    __repr__ = make_repr("project", "version")
     __parent__ = dotted_navigator("project")
     __name__ = dotted_navigator("version")
 
-    name = Column(
-        Text,
-        ForeignKey("packages.name", onupdate="CASCADE", ondelete="CASCADE"),
-        primary_key=True,
+    project_id = Column(
+        ForeignKey("projects.id", onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=False,
     )
-    version = Column(Text, primary_key=True)
+    version = Column(Text, nullable=False)
     canonical_version = Column(Text, nullable=False)
     is_prerelease = orm.column_property(func.pep440_is_prerelease(version))
     author = Column(Text)
@@ -327,7 +316,12 @@ class Release(db.ModelBase):
         passive_deletes=True,
     )
 
-    dependencies = orm.relationship("Dependency")
+    dependencies = orm.relationship(
+        "Dependency",
+        backref="release",
+        cascade="all, delete-orphan",
+        passive_deletes=True,
+    )
 
     _requires = _dependency_relation(DependencyKind.requires)
     requires = association_proxy("_requires", "specifier")
@@ -353,27 +347,12 @@ class Release(db.ModelBase):
     _project_urls = _dependency_relation(DependencyKind.project_url)
     project_urls = association_proxy("_project_urls", "specifier")
 
-    uploader = orm.relationship(
-        "User",
-        secondary=lambda: JournalEntry.__table__,
-        primaryjoin=lambda: (
-            (JournalEntry.name == orm.foreign(Release.name))
-            & (JournalEntry.version == orm.foreign(Release.version))
-            & (JournalEntry.action == "new release")
-        ),
-        secondaryjoin=lambda: (
-            (User.username == orm.foreign(JournalEntry._submitted_by))
-        ),
-        order_by=lambda: JournalEntry.id.desc(),
-        # TODO: We have uselist=False here which raises a warning because
-        # multiple items were returned. This should only be temporary because
-        # we should add a nullable FK to JournalEntry so we don't need to rely
-        # on ordering and implicitly selecting the first object to make this
-        # happen,
-        uselist=False,
-        viewonly=True,
+    uploader_id = Column(
+        ForeignKey("users.id", onupdate="CASCADE", ondelete="SET NULL"),
+        nullable=True,
+        index=True,
     )
-
+    uploader = orm.relationship(User)
     uploaded_via = Column(Text)
 
     @property
@@ -424,20 +403,11 @@ class File(db.Model):
     @declared_attr
     def __table_args__(cls):  # noqa
         return (
-            ForeignKeyConstraint(
-                ["name", "version"],
-                ["releases.name", "releases.version"],
-                onupdate="CASCADE",
-                ondelete="CASCADE",
-            ),
             CheckConstraint("sha256_digest ~* '^[A-F0-9]{64}$'"),
             CheckConstraint("blake2_256_digest ~* '^[A-F0-9]{64}$'"),
-            Index("release_files_name_version_idx", "name", "version"),
-            Index("release_files_version_idx", "version"),
             Index(
                 "release_files_single_sdist",
-                "name",
-                "version",
+                "release_id",
                 "packagetype",
                 unique=True,
                 postgresql_where=(
@@ -445,10 +415,13 @@ class File(db.Model):
                     & (cls.allow_multiple_sdist == False)  # noqa
                 ),
             ),
+            Index("release_files_release_id_idx", "release_id"),
         )
 
-    name = Column(Text)
-    version = Column(Text)
+    release_id = Column(
+        ForeignKey("releases.id", onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=False,
+    )
     python_version = Column(Text)
     requires_python = Column(Text)
     packagetype = Column(
@@ -503,18 +476,14 @@ class Filename(db.ModelBase):
 release_classifiers = Table(
     "release_classifiers",
     db.metadata,
-    Column("name", Text()),
-    Column("version", Text()),
-    Column("trove_id", Integer(), ForeignKey("trove_classifiers.id")),
-    ForeignKeyConstraint(
-        ["name", "version"],
-        ["releases.name", "releases.version"],
-        onupdate="CASCADE",
-        ondelete="CASCADE",
+    Column(
+        "release_id",
+        ForeignKey("releases.id", onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=False,
     ),
-    Index("rel_class_name_version_idx", "name", "version"),
+    Column("trove_id", Integer(), ForeignKey("trove_classifiers.id")),
     Index("rel_class_trove_id_idx", "trove_id"),
-    Index("rel_class_version_id_idx", "version"),
+    Index("rel_class_release_id_idx", "release_id"),
 )
 
 
@@ -539,7 +508,7 @@ class JournalEntry(db.ModelBase):
         DateTime(timezone=False), nullable=False, server_default=sql.func.now()
     )
     _submitted_by = Column(
-        "submitted_by", CIText, ForeignKey("accounts_user.username", onupdate="CASCADE")
+        "submitted_by", CIText, ForeignKey("users.username", onupdate="CASCADE")
     )
     submitted_by = orm.relationship(User)
     submitted_from = Column(Text)
@@ -562,7 +531,7 @@ class BlacklistedProject(db.Model):
     )
     name = Column(Text, unique=True, nullable=False)
     _blacklisted_by = Column(
-        "blacklisted_by", UUID(as_uuid=True), ForeignKey("accounts_user.id"), index=True
+        "blacklisted_by", UUID(as_uuid=True), ForeignKey("users.id"), index=True
     )
     blacklisted_by = orm.relationship(User)
     comment = Column(Text, nullable=False, server_default="")

tests/unit/packaging/test_models.py
@@ -260,8 +260,7 @@ class TestRelease:
         for urlspec in project_urls:
             db_session.add(
                 Dependency(
-                    name=release.project.name,
-                    version=release.version,
+                    release=release,
                     kind=DependencyKind.project_url.value,
                     specifier=urlspec,
                 )

warehouse/accounts/models.py
@@ -53,12 +53,12 @@ class DisableReason(enum.Enum):
 
 class User(SitemapMixin, db.Model):
 
-    __tablename__ = "accounts_user"
+    __tablename__ = "users"
     __table_args__ = (
-        CheckConstraint("length(username) <= 50", name="packages_valid_name"),
+        CheckConstraint("length(username) <= 50", name="users_valid_username_length"),
         CheckConstraint(
             "username ~* '^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$'",
-            name="accounts_user_valid_username",
+            name="users_valid_username",
         ),
     )
 
@@ -110,16 +110,16 @@ class UnverifyReasons(enum.Enum):
 
 class Email(db.ModelBase):
 
-    __tablename__ = "accounts_email"
+    __tablename__ = "user_emails"
     __table_args__ = (
-        UniqueConstraint("email", name="accounts_email_email_key"),
-        Index("accounts_email_user_id", "user_id"),
+        UniqueConstraint("email", name="user_emails_email_key"),
+        Index("user_emails_user_id", "user_id"),
     )
 
     id = Column(Integer, primary_key=True, nullable=False)
     user_id = Column(
         UUID(as_uuid=True),
-        ForeignKey("accounts_user.id", deferrable=True, initially="DEFERRED"),
+        ForeignKey("users.id", deferrable=True, initially="DEFERRED"),
         nullable=False,
     )
     email = Column(String(length=254), nullable=False)

tests/common/db/packaging.py
@@ -12,6 +12,7 @@
 
 import datetime
 import hashlib
+import uuid
 
 import factory
 import factory.fuzzy
@@ -36,6 +37,7 @@ class ProjectFactory(WarehouseFactory):
     class Meta:
         model = Project
 
+    id = factory.LazyFunction(uuid.uuid4)
     name = factory.fuzzy.FuzzyText(length=12)
 
 
@@ -43,7 +45,7 @@ class ReleaseFactory(WarehouseFactory):
     class Meta:
         model = Release
 
-    name = factory.LazyAttribute(lambda o: o.project.name)
+    id = factory.LazyFunction(uuid.uuid4)
     project = factory.SubFactory(ProjectFactory)
     version = factory.Sequence(lambda n: str(n) + ".0")
     canonical_version = factory.LazyAttribute(
@@ -58,7 +60,6 @@ class FileFactory(WarehouseFactory):
     class Meta:
         model = File
 
-    name = factory.LazyAttribute(lambda o: o.release.name)
     release = factory.SubFactory(ReleaseFactory)
     python_version = "source"
     md5_digest = factory.LazyAttribute(
@@ -96,8 +97,7 @@ class DependencyFactory(WarehouseFactory):
     class Meta:
         model = Dependency
 
-    name = factory.fuzzy.FuzzyText(length=12)
-    version = factory.Sequence(lambda n: str(n) + ".0")
+    release = factory.SubFactory(ReleaseFactory)
     kind = factory.fuzzy.FuzzyChoice(int(kind) for kind in DependencyKind)
     specifier = factory.fuzzy.FuzzyText(length=12)
 

tests/unit/admin/views/test_blacklist.py
@@ -211,9 +211,7 @@ class TestAddBlacklist:
 
         project = ProjectFactory.create(name="foo")
         release = ReleaseFactory.create(project=project)
-        FileFactory.create(
-            name=project.name, version=release.version, filename="who cares"
-        )
+        FileFactory.create(release=release, filename="who cares")
         RoleFactory.create(project=project, user=db_request.user)
 
         views.add_blacklist(db_request)

tests/unit/admin/views/test_projects.py
@@ -567,7 +567,8 @@ class TestDeleteRole:
 
         assert db_request.session.flash.calls == [
             pretend.call(
-                f"Removed '{role.user_name}' as '{role.role_name}' on '{project.name}'",
+                f"Removed '{role.user.username}' as '{role.role_name}' "
+                f"on '{project.name}'",
                 queue="success",
             )
         ]

tests/unit/legacy/api/test_json.py
@@ -194,8 +194,7 @@ class TestJSONRelease:
         for urlspec in project_urls:
             db_session.add(
                 Dependency(
-                    name=releases[3].project.name,
-                    version="3.0",
+                    release=releases[3],
                     kind=DependencyKind.project_url.value,
                     specifier=urlspec,
                 )
@@ -460,7 +459,9 @@ class TestJSONReleaseSlash:
         assert isinstance(resp, HTTPMovedPermanently)
         assert db_request.route_path.calls == [
             pretend.call(
-                "legacy.api.json.release", name=release.name, version=release.version
+                "legacy.api.json.release",
+                name=release.project.name,
+                version=release.version,
             )
         ]
         assert resp.headers["Location"] == "/project/the-redirect"

tests/unit/legacy/api/xmlrpc/test_xmlrpc.py
@@ -785,7 +785,7 @@ def test_browse(db_request):
     expected_release._classifiers = classifiers
 
     assert set(xmlrpc.browse(db_request, ["Environment :: Other Environment"])) == {
-        (r.name, r.version) for r in releases
+        (r.project.name, r.version) for r in releases
     }
     assert set(
         xmlrpc.browse(
@@ -795,7 +795,7 @@ def test_browse(db_request):
                 "Development Status :: 5 - Production/Stable",
             ],
         )
-    ) == {(expected_release.name, expected_release.version)}
+    ) == {(expected_release.project.name, expected_release.version)}
     assert set(
         xmlrpc.browse(
             db_request,
@@ -805,7 +805,7 @@ def test_browse(db_request):
                 "Programming Language :: Python",
             ],
         )
-    ) == {(expected_release.name, expected_release.version)}
+    ) == {(expected_release.project.name, expected_release.version)}
     assert set(
         xmlrpc.browse(
             db_request,
@@ -814,7 +814,7 @@ def test_browse(db_request):
                 "Programming Language :: Python",
             ],
         )
-    ) == {(expected_release.name, expected_release.version)}
+    ) == {(expected_release.project.name, expected_release.version)}
 
 
 def test_multicall(pyramid_request):

commit 20d136b4d2d38ee0d30c207b3d56bd18cdd7b595

packaging: Fix invalid 'make_repr' call
This clearly wasn't being called anywhere but we fix them all the same
(and add tests to prevent them breaking again). It has been broken since
commit 6cb8bf933 (there was one more but it was fixed in commit
a51119793)

Signed-off-by: Stephen Finucane <stephen@that.guru>

warehouse/packaging/models.py
@@ -344,7 +344,7 @@ class Dependency(db.Model):
     __table_args__ = (
         Index("release_dependencies_release_kind_idx", "release_id", "kind"),
     )
-    __repr__ = make_repr("name", "version", "kind", "specifier")
+    __repr__ = make_repr("release", "kind", "specifier")
 
     release_id = Column(
         ForeignKey("releases.id", onupdate="CASCADE", ondelete="CASCADE"),

tests/unit/packaging/test_models.py
@@ -26,11 +26,25 @@ from ...common.db.organizations import (
     OrganizationRoleFactory as DBOrganizationRoleFactory,
 )
 from ...common.db.packaging import (
+    DependencyFactory as DBDependencyFactory,
     FileFactory as DBFileFactory,
     ProjectFactory as DBProjectFactory,
     ReleaseFactory as DBReleaseFactory,
     RoleFactory as DBRoleFactory,
+    RoleInvitationFactory as DBRoleInvitationFactory,
 )
+
+
+class TestRole:
+    def test_repr(self, db_request):
+        role = DBRoleFactory()
+        assert isinstance(repr(role), str)
+
+
+class TestRoleInvitation:
+    def test_repr(self, db_request):
+        role_invitation = DBRoleInvitationFactory()
+        assert isinstance(repr(role_invitation), str)
 
 
 class TestProjectFactory:
@@ -146,6 +160,27 @@ class TestProject:
             ],
             key=lambda x: x[1],
         )
+
+    def test_repr(self, db_request):
+        project = DBProjectFactory()
+        assert isinstance(repr(project), str)
+
+
+class TestDependency:
+    def test_repr(self, db_session):
+        dependency = DBDependencyFactory.create()
+        assert isinstance(repr(dependency), str)
+
+
+class TestReleaseURL:
+    def test_repr(self, db_session):
+        release = DBReleaseFactory.create()
+        release_url = ReleaseURL(
+            release=release,
+            name="Homepage",
+            url="https://example.com/",
+        )
+        assert isinstance(repr(release_url), str)
 
 
 class TestRelease: