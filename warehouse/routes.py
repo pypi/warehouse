4a1b46d3458dc0b2f61f20906050b1ab6a9f7c8f
Donald Stufft <donald@stufft.io>
2022-07-25 11:58:40 -0400

    Move DB lookup to a special cased helper factory for legacy json (#11950)

--- warehouse/routes.py
+++ warehouse/routes.py
@@ -442,12 +442,14 @@ def includeme(config):
     config.add_route(
         "legacy.api.json.project",
         "/pypi/{name}/json",
+        factory="warehouse.legacy.api.json.latest_release_factory",
         read_only=True,
         domain=warehouse,
     )
     config.add_route(
         "legacy.api.json.project_slash",
         "/pypi/{name}/json/",
+        factory="warehouse.legacy.api.json.latest_release_factory",
         read_only=True,
         domain=warehouse,
     )
@@ -455,12 +457,14 @@ def includeme(config):
     config.add_route(
         "legacy.api.json.release",
         "/pypi/{name}/{version}/json",
+        factory="warehouse.legacy.api.json.release_factory",
         read_only=True,
         domain=warehouse,
     )
     config.add_route(
         "legacy.api.json.release_slash",
         "/pypi/{name}/{version}/json/",
+        factory="warehouse.legacy.api.json.release_factory",
         read_only=True,
         domain=warehouse,
     )
