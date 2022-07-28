f18468c394b32a0025d5f03cd6cfa5e3e77f393c
sterbo <matt.sterba@gmail.com>
2022-07-28 09:11:14 -0700

    Updates to billing API - Add support for new events - Updates to checkout.session.completed

--- warehouse/api/billing.py
+++ warehouse/api/billing.py
@@ -13,7 +13,7 @@
 
 import stripe
 
-from pyramid.httpexceptions import HTTPBadRequest, HTTPNoContent
+from pyramid.httpexceptions import HTTPBadRequest, HTTPNoContent, HTTPNotFound
 from pyramid.view import view_config
 
 from warehouse.subscriptions.interfaces import (
@@ -27,12 +27,13 @@ def handle_billing_webhook_event(request, event):
     subscription_service = request.find_service(ISubscriptionService, context=None)
 
     match event["type"]:
+        # Occurs when a Checkout Session has been successfully completed.
         case "checkout.session.completed":
-            session = event["data"]["object"]
-            status = session["status"]
-            customer_id = session["customer"]
-            subscription_id = session["subscription"]
-            if session["status"] != "complete":
+            checkout_session = event["data"]["object"]
+            status = checkout_session["status"]
+            customer_id = checkout_session["customer"]
+            subscription_id = checkout_session["subscription"]
+            if status != "complete":
                 raise HTTPBadRequest("Invalid checkout session status")
             if not customer_id:
                 raise HTTPBadRequest("Invalid customer ID")
@@ -45,7 +46,63 @@ def handle_billing_webhook_event(request, event):
                 )
             else:
                 # Activate subscription for customer.
-                subscription_service.add_subscription(customer_id, subscription_id)
+                subscription_service.add_subscription(
+                    request, customer_id, subscription_id
+                )
+        # Occurs whenever a customer’s subscription ends.
+        case "customer.subscription.deleted":
+            subscription = event["data"]["object"]
+            status = subscription["status"]
+            customer_id = subscription["customer"]
+            subscription_id = subscription["id"]
+            if not status or not SubscriptionStatus.has_value(status):
+                raise HTTPBadRequest("Invalid subscription status")
+            if not customer_id:
+                raise HTTPBadRequest("Invalid customer ID")
+            if not subscription_id:
+                raise HTTPBadRequest("Invalid subscription ID")
+            if id := subscription_service.find_subscriptionid(subscription_id):
+                # Set subscription status to canceled.
+                subscription_service.update_subscription_status(
+                    id, SubscriptionStatus.Canceled
+                )
+            else:
+                raise HTTPNotFound("Subscription not found")
+        # Occurs whenever a subscription changes e.g. status changes.
+        case "customer.subscription.updated":
+            subscription = event["data"]["object"]
+            status = subscription["status"]
+            customer_id = subscription["customer"]
+            subscription_id = subscription["id"]
+            if not status or not SubscriptionStatus.has_value(status):
+                raise HTTPBadRequest("Invalid subscription status")
+            if not customer_id:
+                raise HTTPBadRequest("Invalid customer ID")
+            if not subscription_id:
+                raise HTTPBadRequest("Invalid subscription ID")
+
+            if id := subscription_service.find_subscriptionid(subscription_id):
+                db_subscription = subscription_service.get_subscription(
+                    subscription_service.find_subscriptionid(id)
+                )
+                if db_subscription.status != status:
+                    # Update subscription status.
+                    subscription_service.update_subscription_status(
+                        db_subscription.id, status
+                    )
+            else:
+                raise HTTPNotFound("Subscription not found")
+        # Occurs whenever a customer is deleted.
+        case "customer.deleted":
+            customer = event["data"]["object"]
+            customer_id = customer["id"]
+            if not customer_id:
+                raise HTTPBadRequest("Invalid customer ID")
+            if subscription_service.get_subscriptions_by_customer(customer_id):
+                # Delete the customer and all associated subscription data
+                subscription_service.delete_customer(customer_id)
+            else:
+                raise HTTPNotFound("Customer subscription data not found")
 
 
 @view_config(
