98e1915a76847d0d406bb5897164d9ed7b1aa7a7
Mike Fiedler <miketheman@gmail.com>
2022-08-06 18:24:44 -0400

    feat: include mathjax
    Follows installation instructions from docs.
    See https://docs.mathjax.org/en/latest/web/start.html#using-mathjax-from-a-content-delivery-network-cdn
    Using the TeX+SVG variant to keep it lightweight.
    
    Loads from a CDN with SRI hashes, and restricts the Content Security
    Policy narrowly to prevent any other calls being made to anything
    unrelated to the MathJax package.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- tests/unit/test_csp.py
+++ tests/unit/test_csp.py
@@ -205,6 +205,7 @@ def test_includeme():
                         "*.fastly-insights.com",
                         "*.ethicalads.io",
                         "https://api.pwnedpasswords.com",
+                        "https://cdn.jsdelivr.net/npm/mathjax@3.2.2/",
                         "https://2p66nmmycsj3.statuspage.io",
                     ],
                     "default-src": ["'none'"],
@@ -226,6 +227,8 @@ def test_includeme():
                         "*.fastly-insights.com",
                         "*.ethicalads.io",
                         "'sha256-U3hKDidudIaxBDEzwGJApJgPEf2mWk6cfMWghrAa6i0='",
+                        "https://cdn.jsdelivr.net/npm/mathjax@3.2.2/",
+                        "'sha256-1CldwzdEg2k1wTmf7s5RWVd7NMXI/7nxxjJM2C4DqII='",
                     ],
                     "style-src": [
                         "'self'",
@@ -233,6 +236,10 @@ def test_includeme():
                         "*.ethicalads.io",
                         "'sha256-2YHqZokjiizkHi1Zt+6ar0XJ0OeEy/egBnlm+MDMtrM='",
                         "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
+                        "'sha256-JLEjeN9e5dGsz5475WyRaoA4eQOdNPxDIeUhclnJDCE='",
+                        "'sha256-mQyxHEuwZJqpxCw3SLmc4YOySNKXunyu2Oiz1r3/wAE='",
+                        "'sha256-OCf+kv5Asiwp++8PIevKBYSgnNLNUZvxAp4a7wMLuKA='",
+                        "'sha256-h5LOiLhk6wiJrGsG5ItM0KimwzWQH/yAcmoJDJL//bY='",
                     ],
                     "worker-src": ["*.fastly-insights.com"],
                 }
