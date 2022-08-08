1f47f9a815a0da19ab089f2627af0ff3557c998b
Chris Lei <chris@divbzero.com>
2022-08-08 07:10:53 -0700

    Fix add organization project (#12008)
    * Add "add:project" and "remove:project" permissions
    
    * Only org owners and managers can add projects
    
    Organization owners and managers have "add:project" permission.
    
    * Only org owners can remove projects
    
    * `SelectField` default when there are no choices
    
    Setting a default value for `SelectField` avoids a "Not a valid choice."
    error if there are no choices for the field.
    
    * Clearer error messages for remove/transfer project
    
    Added more descriptive error messages as @ewdurbin suggested.
    
    Co-authored-by: Ee Durbin <ewdurbin@gmail.com>

--- warehouse/organizations/models.py
+++ warehouse/organizations/models.py
@@ -246,6 +246,16 @@ class Organization(HasEvents, db.Model):
             # Allow all people in organization read access.
             # Allow write access depending on role.
             if role.role_name == OrganizationRoleType.Owner:
+                # Allowed:
+                # - View organization ("view:organization")
+                # - View team ("view:team")
+                # - Invite/remove organization member ("manage:organization")
+                # - Add/remove team member ("manage:team")
+                # - Manage billing ("manage:billing")
+                # - Create project ("add:project")
+                # - Delete project ("remove:project")
+                # Disallowed:
+                # - (none)
                 acls.append(
                     (
                         Allow,
@@ -255,10 +265,22 @@ class Organization(HasEvents, db.Model):
                             "view:team",
                             "manage:organization",
                             "manage:team",
+                            "manage:billing",
+                            "add:project",
+                            "remove:project",
                         ],
                     )
                 )
             elif role.role_name == OrganizationRoleType.BillingManager:
+                # Allowed:
+                # - View organization ("view:organization")
+                # - View team ("view:team")
+                # - Manage billing ("manage:billing")
+                # Disallowed:
+                # - Invite/remove organization member ("manage:organization")
+                # - Add/remove team member ("manage:team")
+                # - Create project ("add:project")
+                # - Delete project ("remove:project")
                 acls.append(
                     (
                         Allow,
@@ -267,15 +289,39 @@ class Organization(HasEvents, db.Model):
                     )
                 )
             elif role.role_name == OrganizationRoleType.Manager:
+                # Allowed:
+                # - View organization ("view:organization")
+                # - View team ("view:team")
+                # - Add/remove team member ("manage:team")
+                # - Create project ("add:project")
+                # Disallowed:
+                # - Invite/remove organization member ("manage:organization")
+                # - Manage billing ("manage:billing")
+                # - Delete project ("remove:project")
                 acls.append(
                     (
                         Allow,
                         f"user:{role.user.id}",
-                        ["view:organization", "view:team", "manage:team"],
+                        [
+                            "view:organization",
+                            "view:team",
+                            "manage:team",
+                            "add:project",
+                        ],
                     )
                 )
             else:
                 # No member-specific write access needed for now.
+
+                # Allowed:
+                # - View organization ("view:organization")
+                # - View team ("view:team")
+                # Disallowed:
+                # - Invite/remove organization member ("manage:organization")
+                # - Add/remove team member ("manage:team")
+                # - Manage billing ("manage:billing")
+                # - Create project ("add:project")
+                # - Delete project ("remove:project")
                 acls.append(
                     (Allow, f"user:{role.user.id}", ["view:organization", "view:team"])
                 )
