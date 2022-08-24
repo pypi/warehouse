2b88d73de9921fbba35ef8eb188a91ff1b9490d4
Dustin Ingram <di@users.noreply.github.com>
2022-08-24 13:05:28 +0000

    Add shortcut/redirect for /2fa

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -541,6 +541,7 @@ def test_routes(warehouse):
     assert config.add_redirect.calls == [
         pretend.call("/sponsor/", "/sponsors/", domain=warehouse),
         pretend.call("/u/{username}/", "/user/{username}/", domain=warehouse),
+        pretend.call("/2fa/", "/manage/account/two-factor/", domain=warehouse),
         pretend.call("/p/{name}/", "/project/{name}/", domain=warehouse),
         pretend.call("/pypi/{name}/", "/project/{name}/", domain=warehouse),
         pretend.call(
