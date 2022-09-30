6f8adbd5c51d721430eee076379fc875a2255f95
Mike Fiedler <miketheman@gmail.com>
2022-09-29 22:39:08 -0400

    chore: remove unnecessary mutationobserver-shim
    jsdom 13.2.0 added direct support for MutationObserver, and recommends
    removing all workarounds. We're on version 19.0.
    
    Refs: https://github.com/jsdom/jsdom/issues/639#issuecomment-816851899
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- tests/frontend/setup.js
+++ tests/frontend/setup.js
@@ -11,26 +11,6 @@
  * limitations under the License.
  */
 
-// Setup MutationObserver shim since jsdom doesn't
-// support it out of the box.
-
-const fs = require("fs");
-const path = require("path");
-
-const shim = fs.readFileSync(
-  path.resolve(
-    "node_modules",
-    "mutationobserver-shim",
-    "dist",
-    "mutationobserver.min.js"
-  ),
-  { encoding: "utf-8" }
-);
-const script = window.document.createElement("script");
-script.textContent = shim;
-
-window.document.body.appendChild(script);
-
 // Extend Jest with jest-dom https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 
