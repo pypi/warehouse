5c3e0ff6e09df8997c5e906d85106684defb7ed4
William Woodruff <william@trailofbits.com>
2022-10-12 15:22:42 -0400

    warehouse/migrations: rebase
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- warehouse/migrations/versions/43bf0b6badcb_make_users_optional_with_macaroons.py
+++ warehouse/migrations/versions/43bf0b6badcb_make_users_optional_with_macaroons.py
@@ -13,7 +13,7 @@
 Make users optional with Macaroons
 
 Revision ID: 43bf0b6badcb
-Revises: 7eaad728b806
+Revises: 90f6ee9298db
 Create Date: 2022-04-19 14:57:54.765006
 """
 
@@ -24,7 +24,7 @@ from citext import CIText
 from sqlalchemy.dialects import postgresql
 
 revision = "43bf0b6badcb"
-down_revision = "7eaad728b806"
+down_revision = "90f6ee9298db"
 
 
 def upgrade():
