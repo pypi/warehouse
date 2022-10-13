f4d41aae93fc8c16c1820fdec0beade86b539139
William Woodruff <william@trailofbits.com>
2022-10-13 12:08:02 -0400

    warehouse, tests: OIDC utils coverage
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- warehouse/oidc/utils.py
+++ warehouse/oidc/utils.py
@@ -56,4 +56,4 @@ def find_provider_by_issuer(session, issuer_url, signed_claims):
         )
     else:
         # Unreachable; same logic error as above.
-        return None
+        return None  # pragma: no cover
