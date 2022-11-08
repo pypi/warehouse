44a0243d1254379127ae3a3e7f02aa0192f25fb0
Ee Durbin <ewdurbin@gmail.com>
2022-11-08 13:10:47 -0500

    test bans

---
+++ tests/unit/admin/test_bans.py
@@ -0,0 +1,28 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import enum
+
+from ...common.db.ip_addresses import IpAddressFactory
+
+
+class TestAdminFlag:
+    def test_no_ip_not_banned(self, db_request):
+        assert not db_request.banned.by_ip("1.2.3.4")
+
+    def test_with_ip_not_banned(self, db_request):
+        IpAddressFactory(ip_address="1.2.3.4")
+        assert not db_request.banned.by_ip("1.2.3.4")
+
+    def test_with_ip_banned(self, db_request):
+        IpAddressFactory(ip_address="1.2.3.4", is_banned=True)
+        assert db_request.banned.by_ip("1.2.3.4")
