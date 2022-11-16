e97e8c50fbb7bad1580ab48588cfbe8b583f5b4d
Ee Durbin <ewdurbin@gmail.com>
2022-11-16 16:36:57 -0500

    sketch of a fallback translation extension
    ref: #12552

--- warehouse/i18n/extensions.py
+++ warehouse/i18n/extensions.py
@@ -10,7 +10,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from jinja2.ext import Extension
+import typing as t
+
+from jinja2.ext import (
+    Extension,
+    InternationalizationExtension,
+    _make_new_npgettext,
+    _make_new_pgettext,
+)
+from jinja2.runtime import Context
+from jinja2.utils import pass_context
+from markupsafe import Markup
 
 
 class TrimmedTranslatableTagsExtension(Extension):
@@ -20,3 +30,69 @@ class TrimmedTranslatableTagsExtension(Extension):
 
     def __init__(self, environment):
         environment.policies["ext.i18n.trimmed"] = True
+
+def _make_new_gettext(func: t.Callable[[str], str]) -> t.Callable[..., str]:
+    @pass_context
+    def gettext(__context: Context, __string: str, **variables: t.Any) -> str:
+        rv = __context.call(func, __string)
+        if __context.eval_ctx.autoescape:
+            rv = Markup(rv)
+        # Always treat as a format string, even if there are no
+        # variables. This makes translation strings more consistent
+        # and predictable. This requires escaping
+        try:
+            return rv % variables  # type: ignore
+        except KeyError:
+            return __string % variables
+
+    return gettext
+
+
+def _make_new_ngettext(func: t.Callable[[str, str, int], str]) -> t.Callable[..., str]:
+    @pass_context
+    def ngettext(
+        __context: Context,
+        __singular: str,
+        __plural: str,
+        __num: int,
+        **variables: t.Any,
+    ) -> str:
+        variables.setdefault("num", __num)
+        rv = __context.call(func, __singular, __plural, __num)
+        if __context.eval_ctx.autoescape:
+            rv = Markup(rv)
+        # Always treat as a format string, see gettext comment above.
+        try:
+            return rv % variables  # type: ignore
+        except KeyError:
+            if __num > 1:
+                return __plural % variables
+            return __singular % variables
+
+    return ngettext
+
+
+class FallbackInternationalizationExtension(InternationalizationExtension):
+    def _install_callables(
+        self,
+        gettext: t.Callable[[str], str],
+        ngettext: t.Callable[[str, str, int], str],
+        newstyle: t.Optional[bool] = None,
+        pgettext: t.Optional[t.Callable[[str, str], str]] = None,
+        npgettext: t.Optional[t.Callable[[str, str, str, int], str]] = None,
+    ) -> None:
+        if newstyle is not None:
+            self.environment.newstyle_gettext = newstyle  # type: ignore
+        if self.environment.newstyle_gettext:  # type: ignore
+            gettext = _make_new_gettext(gettext)
+            ngettext = _make_new_ngettext(ngettext)
+
+            if pgettext is not None:
+                pgettext = _make_new_pgettext(pgettext)
+
+            if npgettext is not None:
+                npgettext = _make_new_npgettext(npgettext)
+
+        self.environment.globals.update(
+            gettext=gettext, ngettext=ngettext, pgettext=pgettext, npgettext=npgettext
+        )
