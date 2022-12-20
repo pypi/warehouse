aa14f230f572f6eb282d3b773645eeed8f51f959
Dustin Ingram <di@users.noreply.github.com>
2022-12-20 14:13:13 +0000

    Update tests

--- tests/unit/admin/views/test_includes.py
+++ tests/unit/admin/views/test_includes.py
@@ -14,11 +14,16 @@ import pretend
 
 from warehouse.admin.views import includes
 
+from ....common.db.packaging import ProjectFactory
 
-def test_administer_project_include_returns_project():
-    project = pretend.stub()
-    assert includes.administer_project_include(project, pretend.stub()) == {
-        "project": project
+
+def test_administer_project_include_returns_project(db_request):
+    project = ProjectFactory.create()
+    db_request.matchdict = {"project_name": project.name}
+    assert includes.administer_project_include(db_request) == {
+        "project": project,
+        "prohibited": None,
+        "project_name": project.name,
     }
 
 
