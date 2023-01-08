f2ca29fa403df01e2c50c0ce4f66dc6237853de8
Mike Fiedler <miketheman@gmail.com>
2023-01-08 17:52:14 -0500

    lint: make whitespace deterministic
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- warehouse/sponsors/models.py
+++ warehouse/sponsors/models.py
@@ -52,8 +52,8 @@ class Sponsor(db.Model):
     def white_logo_img(self):
         if not self.white_logo_url:
             return ""
-        return f'<img class="sponsors__image" \
-                  src="{ self.white_logo_url }" alt="{ self.name }" loading="lazy">'
+        return '<img class="sponsors__image" ' + \
+            f'src="{ self.white_logo_url }" alt="{ self.name }" loading="lazy">'
 
     @property
     def activity(self):
