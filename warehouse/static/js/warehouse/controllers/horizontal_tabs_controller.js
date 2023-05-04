02e1ac65e112f5304573502557edc8a06ec207f9
Dustin Ingram <di@users.noreply.github.com>
2023-05-04 10:55:45 -0400

    Apply suggestions from code review
    Co-authored-by: Mike Fiedler <miketheman@gmail.com>

--- warehouse/static/js/warehouse/controllers/horizontal_tabs_controller.js
+++ warehouse/static/js/warehouse/controllers/horizontal_tabs_controller.js
@@ -17,8 +17,6 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["tab", "tabPanel"];
 
-  connect() {
-  }
 
   initialize() {
     this.showTab();
