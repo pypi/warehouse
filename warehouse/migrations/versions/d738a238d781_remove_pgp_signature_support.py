6e175bf7ea7132254a5ef78e1276806f1fbd889b
Donald Stufft <donald@stufft.io>
2023-05-23 10:11:43 -0400

    fix down revision

--- warehouse/migrations/versions/d738a238d781_remove_pgp_signature_support.py
+++ warehouse/migrations/versions/d738a238d781_remove_pgp_signature_support.py
@@ -13,14 +13,14 @@
 Remove PGP signature support
 
 Revision ID: d738a238d781
-Revises: c5f718cb98ac
+Revises: ab536b1853f0
 Create Date: 2023-05-21 14:46:11.845339
 """
 
 from alembic import op
 
 revision = "d738a238d781"
-down_revision = "c5f718cb98ac"
+down_revision = "ab536b1853f0"
 
 
 def upgrade():
