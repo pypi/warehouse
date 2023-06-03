396d609669f5e4ddfaa57457ea576d0c2545b81c
Donald Stufft <donald@stufft.io>
2023-06-03 13:25:22 -0400

    Restrict Security Policies based on route type

--- warehouse/forklift/__init__.py
+++ warehouse/forklift/__init__.py
@@ -31,7 +31,10 @@ def includeme(config):
 
     # Add the routes that we'll be using in Forklift.
     config.add_legacy_action_route(
-        "forklift.legacy.file_upload", "file_upload", domain=forklift
+        "forklift.legacy.file_upload",
+        "file_upload",
+        domain=forklift,
+        is_api=True,
     )
     config.add_legacy_action_route("forklift.legacy.submit", "submit", domain=forklift)
     config.add_legacy_action_route(
