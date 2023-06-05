641460b27773bc98361052891edaa67bf4e36454
Carey Hoffman <cgchoffman@gmail.com>
2023-06-05 10:43:18 -0700

    Make _check_claim_binary and _check_claim_invariant not private (#13811)
    * Make _check_claim_binary and _check_claim_invariant not private
    
    * Reusable callable type for claim checking code

--- warehouse/oidc/models/_core.py
+++ warehouse/oidc/models/_core.py
@@ -11,7 +11,7 @@
 # limitations under the License.
 
 from collections.abc import Callable
-from typing import Any
+from typing import Any, TypeVar
 
 import sentry_sdk
 
@@ -23,8 +23,12 @@ from warehouse.macaroons.models import Macaroon
 from warehouse.oidc.interfaces import SignedClaims
 from warehouse.packaging.models import Project
 
+C = TypeVar("C")
 
-def _check_claim_binary(binary_func):
+CheckClaimCallable = Callable[[C, C, SignedClaims], bool]
+
+
+def check_claim_binary(binary_func: Callable[[C, C], bool]) -> CheckClaimCallable[C]:
     """
     Wraps a binary comparison function so that it takes three arguments instead,
     ignoring the third.
@@ -33,13 +37,13 @@ def _check_claim_binary(binary_func):
     comparison checks like `str.__eq__`.
     """
 
-    def wrapper(ground_truth, signed_claim, all_signed_claims):
+    def wrapper(ground_truth: C, signed_claim: C, all_signed_claims: SignedClaims):
         return binary_func(ground_truth, signed_claim)
 
     return wrapper
 
 
-def _check_claim_invariant(value: Any):
+def check_claim_invariant(value: C) -> CheckClaimCallable[C]:
     """
     Wraps a fixed value comparison into a three-argument function.
 
@@ -47,7 +51,7 @@ def _check_claim_invariant(value: Any):
     comparison checks, like "claim x is always the literal `true` value".
     """
 
-    def wrapper(ground_truth, signed_claim, all_signed_claims):
+    def wrapper(ground_truth: C, signed_claim: C, all_signed_claims: SignedClaims):
         return ground_truth == signed_claim == value
 
     return wrapper
@@ -80,14 +84,10 @@ class OIDCPublisherMixin:
 
     # A map of claim names to "check" functions, each of which
     # has the signature `check(ground-truth, signed-claim, all-signed-claims) -> bool`.
-    __required_verifiable_claims__: dict[
-        str, Callable[[Any, Any, dict[str, Any]], bool]
-    ] = dict()
+    __required_verifiable_claims__: dict[str, CheckClaimCallable[Any]] = dict()
 
     # Simlar to __verificable_claims__, but these claims are optional
-    __optional_verifiable_claims__: dict[
-        str, Callable[[Any, Any, dict[str, Any]], bool]
-    ] = dict()
+    __optional_verifiable_claims__: dict[str, CheckClaimCallable[Any]] = dict()
 
     # Claims that have already been verified during the JWT signature
     # verification phase.
@@ -105,7 +105,7 @@ class OIDCPublisherMixin:
     __unchecked_claims__: set[str] = set()
 
     @classmethod
-    def all_known_claims(cls):
+    def all_known_claims(cls) -> set[str]:
         """
         Returns all claims "known" to this publisher.
         """
@@ -174,11 +174,11 @@ class OIDCPublisherMixin:
         return True
 
     @property
-    def publisher_name(self):  # pragma: no cover
+    def publisher_name(self) -> str:  # pragma: no cover
         # Only concrete subclasses are constructed.
         raise NotImplementedError
 
-    def publisher_url(self, claims=None):  # pragma: no cover
+    def publisher_url(self, claims=None) -> str:  # pragma: no cover
         """
         NOTE: This is **NOT** a `@property` because we pass `claims` to it.
         When calling, make sure to use `publisher_url()`
