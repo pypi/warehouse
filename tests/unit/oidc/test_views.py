d64b625d13efbfac7da8ecf630e65be64801acca
Keming <kemingy94@gmail.com>
2023-06-14 20:53:55 +0800

    fix test
    Signed-off-by: Keming <kemingy94@gmail.com>

--- tests/unit/oidc/test_views.py
+++ tests/unit/oidc/test_views.py
@@ -180,7 +180,11 @@ def test_mint_token_from_trusted_publisher_lookup_fails():
         "errors": [
             {
                 "code": "invalid-publisher",
-                "description": "valid token, but no corresponding publisher",
+                "description": (
+                    "valid token, but no corresponding publisher, "
+                    "either this publisher is not configured in PyPI "
+                    "or this publisher required environment is not met"
+                ),
             }
         ],
     }
