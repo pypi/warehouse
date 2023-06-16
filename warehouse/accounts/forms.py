74d3a4335acc3840f3f718554c7868d704a322eb
Mike Fiedler <miketheman@gmail.com>
2023-06-16 12:08:54 -0400

    fix: add an email length validator (#13954)

--- warehouse/accounts/forms.py
+++ warehouse/accounts/forms.py
@@ -232,6 +232,9 @@ class NewEmailMixin:
             wtforms.validators.Regexp(
                 r".+@.+\..+", message=_("The email address isn't valid. Try again.")
             ),
+            wtforms.validators.Length(
+                max=254, message=_("The email address is too long. Try again.")
+            ),
         ]
     )
 
