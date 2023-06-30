b9d6c0c93b99ee4781e52d38377715818585c695
Seth Michael Larson <sethmichaellarson@gmail.com>
2023-06-30 11:35:29 -0500

    Distinguish between OIDC being configured or successfully used

--- tests/unit/oidc/test_tasks.py
+++ tests/unit/oidc/test_tasks.py
@@ -10,12 +10,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import datetime
+
 import pretend
 
 from warehouse.oidc.tasks import compute_oidc_metrics
 
 from ...common.db.oidc import GitHubPublisherFactory
-from ...common.db.packaging import ProjectFactory
+from ...common.db.packaging import (
+    FileEventFactory,
+    FileFactory,
+    ProjectFactory,
+    ReleaseFactory,
+)
 
 
 def test_compute_oidc_metrics(db_request, metrics):
@@ -26,26 +33,79 @@ def test_compute_oidc_metrics(db_request, metrics):
     non_critical_project_oidc = ProjectFactory.create(
         name="non_critical_project_oidc",
     )
+    non_released_critical_project_oidc = ProjectFactory.create(
+        name="non_released_critical_project_oidc", pypi_mandates_2fa=True
+    )
+    non_released_project_oidc = ProjectFactory.create(
+        name="non_released_project_oidc",
+    )
 
     # Projects without OIDC
-    critical_project_no_oidc = ProjectFactory.create(
-        name="critical_project_no_oidc", pypi_mandates_2fa=True
-    )
-    non_critical_project_no_oidc = ProjectFactory.create(
+    ProjectFactory.create(name="critical_project_no_oidc", pypi_mandates_2fa=True)
+    ProjectFactory.create(
         name="non_critical_project_no_oidc",
     )
 
-    # Create OIDC publishers. One OIDCPublisher can be shared by multiple
-    # projects so 'oidc_publisher_1' represents that situation. Verify
-    # that metrics don't double-count projects using multiple OIDC publishers.
-    oidc_publisher_1 = GitHubPublisherFactory.create(
+    # Create an OIDC publisher that's shared by multiple projects.
+    GitHubPublisherFactory.create(
         projects=[critical_project_oidc, non_critical_project_oidc]
     )
-    oidc_publisher_2 = GitHubPublisherFactory.create(projects=[critical_project_oidc])
+
+    # Create an OIDC publisher that is only used by one project.
+    GitHubPublisherFactory.create(projects=[critical_project_oidc])
+
+    # Create OIDC publishers for projects which have no releases.
+    GitHubPublisherFactory.create(projects=[non_released_critical_project_oidc])
+    GitHubPublisherFactory.create(projects=[non_released_project_oidc])
+
+    # Create some files which have/have not been published
+    # using OIDC in different scenarios.
+
+    # Scenario: Same release, difference between files.
+    release_1 = ReleaseFactory.create(project=critical_project_oidc)
+    file_1_1 = FileFactory.create(release=release_1)
+    FileEventFactory.create(
+        source=file_1_1,
+        tag="fake:event",
+        time=datetime.datetime(2018, 2, 5, 17, 18, 18, 462_634),
+        additional={"publisher_url": "https://fake/url"},
+    )
+
+    release_1 = ReleaseFactory.create(project=critical_project_oidc)
+    file_1_2 = FileFactory.create(release=release_1)
+    FileEventFactory.create(
+        source=file_1_2,
+        tag="fake:event",
+        time=datetime.datetime(2018, 2, 5, 17, 18, 18, 462_634),
+    )
+
+    # Scenario: Same project, differences between releases.
+    release_2 = ReleaseFactory.create(project=non_critical_project_oidc)
+    file_2 = FileFactory.create(release=release_2)
+    FileEventFactory.create(
+        source=file_2,
+        tag="fake:event",
+        time=datetime.datetime(2018, 2, 5, 17, 18, 18, 462_634),
+        additional={"publisher_url": "https://fake/url"},
+    )
+
+    release_3 = ReleaseFactory.create(project=non_critical_project_oidc)
+    file_3 = FileFactory.create(release=release_3)
+    FileEventFactory.create(
+        source=file_3,
+        tag="fake:event",
+        time=datetime.datetime(2018, 2, 5, 17, 18, 18, 462_634),
+    )
 
     compute_oidc_metrics(db_request)
 
     assert metrics.gauge.calls == [
-        pretend.call("warehouse.oidc.total_projects_using_oidc_publishers", 2),
-        pretend.call("warehouse.oidc.total_critical_projects_using_oidc_publishers", 1),
+        pretend.call("warehouse.oidc.total_projects_configured_oidc_publishers", 4),
+        pretend.call(
+            "warehouse.oidc.total_critical_projects_configured_oidc_publishers", 2
+        ),
+        pretend.call("warehouse.oidc.total_projects_published_with_oidc_publishers", 2),
+        pretend.call(
+            "warehouse.oidc.total_critical_projects_published_with_oidc_publishers", 1
+        ),
     ]
--- warehouse/oidc/tasks.py
+++ warehouse/oidc/tasks.py
@@ -12,25 +12,48 @@
 
 from warehouse import tasks
 from warehouse.metrics import IMetricsService
-from warehouse.packaging.models import Project
+from warehouse.packaging.models import File, Project, Release
 
 
 @tasks.task(ignore_result=True, acks_late=True)
 def compute_oidc_metrics(request):
     metrics = request.find_service(IMetricsService, context=None)
 
-    projects_using_oidc = (
-        request.db.query(Project).distinct().join(Project.oidc_publishers)
+    projects_configured_oidc = (
+        request.db.query(Project.id).distinct().join(Project.oidc_publishers)
     )
 
-    # Metric for count of all projects using OIDC
+    # Metric for count of all projects that have configured OIDC.
     metrics.gauge(
-        "warehouse.oidc.total_projects_using_oidc_publishers",
-        projects_using_oidc.count(),
+        "warehouse.oidc.total_projects_configured_oidc_publishers",
+        projects_configured_oidc.count(),
     )
 
-    # Metric for count of critical projects using OIDC
+    # Metric for count of critical projects that have configured OIDC.
     metrics.gauge(
-        "warehouse.oidc.total_critical_projects_using_oidc_publishers",
-        projects_using_oidc.where(Project.pypi_mandates_2fa.is_(True)).count(),
+        "warehouse.oidc.total_critical_projects_configured_oidc_publishers",
+        projects_configured_oidc.where(Project.pypi_mandates_2fa.is_(True)).count(),
+    )
+
+    # Need to check FileEvent.additional['publisher_url'] to determine which
+    # projects have successfully published via an OIDC publisher.
+    projects_published_with_oidc = (
+        request.db.query(Project.id)
+        .distinct()
+        .join(Project.releases)
+        .join(Release.files)
+        .join(File.events)
+        .where(File.Event.additional.op("->>")("publisher_url").is_not(None))
+    )
+
+    # Metric for count of all projects that have published via OIDC
+    metrics.gauge(
+        "warehouse.oidc.total_projects_published_with_oidc_publishers",
+        projects_published_with_oidc.count(),
+    )
+
+    # Metric for count of critical projects that have published via OIDC
+    metrics.gauge(
+        "warehouse.oidc.total_critical_projects_published_with_oidc_publishers",
+        projects_published_with_oidc.where(Project.pypi_mandates_2fa.is_(True)).count(),
     )
