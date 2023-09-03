d12deea005f6652862ad0b9d28ca8c3d39bc7402
Mike Fiedler <miketheman@gmail.com>
2023-09-03 06:42:18 -0400

    chore: revert celery and kombu upgrade (#14475)
    * Revert "chore: increase worker startup timeout (#14474)"
    
    This reverts commit a29912c633552eaf214926adf434289f35eee225.
    
    * chore: revert celery and kombu upgrade
    
    We're seeing some slowdowns and startup time alerts with the latest
    versions.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    ---------
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- warehouse/tasks.py
+++ warehouse/tasks.py
@@ -217,7 +217,6 @@ def includeme(config):
         task_routes={},
         task_serializer="json",
         worker_disable_rate_limits=True,
-        worker_proc_alive_timeout=10.0,
         REDBEAT_REDIS_URL=s["celery.scheduler_url"],
     )
     config.registry["celery.app"].Task = WarehouseTask
