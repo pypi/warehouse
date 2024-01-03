895bcbf08fb6a26dacb6ae8f0db30a890710407c
Mike Fiedler <miketheman@gmail.com>
2024-01-03 14:39:36 -0500

    feat: add configuration
    We don't have any other backends (yet), but now we can configure one if
    we wanted to.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- tests/unit/captcha/test_init.py
+++ tests/unit/captcha/test_init.py
@@ -32,3 +32,24 @@ def test_includeme_defaults_to_recaptcha():
             name="captcha",
         ),
     ]
+
+
+def test_include_with_custom_backend():
+    cache_class = pretend.stub(create_service=pretend.stub())
+    config = pretend.stub(
+        registry=pretend.stub(settings={"captcha.backend": "tests.CustomBackend"}),
+        maybe_dotted=pretend.call_recorder(lambda n: cache_class),
+        register_service_factory=pretend.call_recorder(
+            lambda factory, iface, name: None
+        ),
+    )
+    includeme(config)
+
+    assert config.maybe_dotted.calls == [pretend.call("tests.CustomBackend")]
+    assert config.register_service_factory.calls == [
+        pretend.call(
+            cache_class.create_service,
+            interfaces.ICaptchaService,
+            name="captcha",
+        )
+    ]
