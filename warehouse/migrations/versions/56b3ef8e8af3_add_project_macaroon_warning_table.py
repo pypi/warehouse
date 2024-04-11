f3a7c59fe8be8a3eeace1198c60edf85a54d884c
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2024-04-11 17:33:54 +0200

    update migration script

--- warehouse/migrations/versions/56b3ef8e8af3_add_project_macaroon_warning_table.py
+++ warehouse/migrations/versions/56b3ef8e8af3_add_project_macaroon_warning_table.py
@@ -13,7 +13,7 @@
 add_project_macaroon_warning_table
 
 Revision ID: 56b3ef8e8af3
-Revises: 1fdecaf73541
+Revises: 34c3175f4bea
 Create Date: 2024-03-11 19:41:22.997939
 """
 
@@ -22,7 +22,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "56b3ef8e8af3"
-down_revision = "1fdecaf73541"
+down_revision = "34c3175f4bea"
 
 
 def upgrade():
