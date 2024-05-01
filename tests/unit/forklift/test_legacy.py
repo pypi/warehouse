7c93a28780ea728ac07b51d03a1c6b7db45899d9
Dustin Ingram <di@users.noreply.github.com>
2024-05-01 11:27:30 -0400

    Stop sending emails/warnings on PGP signature uploads (#15896)

--- tests/unit/forklift/test_legacy.py
+++ tests/unit/forklift/test_legacy.py
@@ -3695,7 +3695,7 @@ class TestFileUpload:
             ("example", "1.0", "add source file example-1.0.tar.gz", user),
         ]
 
-    def test_upload_succeeds_with_signature(
+    def test_upload_succeeds_with_gpg_signature_field(
         self, pyramid_config, db_request, metrics, project_service, monkeypatch
     ):
         user = UserFactory.create()
@@ -3729,20 +3729,9 @@ class TestFileUpload:
         }.get(svc)
         db_request.user_agent = "warehouse-tests/6.6.6"
 
-        send_email = pretend.call_recorder(lambda *a, **kw: None)
-        monkeypatch.setattr(legacy, "send_gpg_signature_uploaded_email", send_email)
-
         resp = legacy.file_upload(db_request)
 
         assert resp.status_code == 200
-        assert resp.body == (
-            b"GPG signature support has been removed from PyPI and the provided "
-            b"signature has been discarded."
-        )
-
-        assert send_email.calls == [
-            pretend.call(db_request, user, project_name="example"),
-        ]
 
     def test_upload_succeeds_without_two_factor(
         self, pyramid_config, db_request, metrics, project_service, monkeypatch
