32e4da4a8ffa6e75861f481cba6a7f096222fad4
Ee Durbin <ewdurbin@gmail.com>
2024-06-08 10:14:28 -0400

    upgrade to postgres 16.1 (#16072)

--- tests/conftest.py
+++ tests/conftest.py
@@ -255,7 +255,7 @@ def database(request):
     pg_port = config.get("port") or os.environ.get("PGPORT", 5432)
     pg_user = config.get("user")
     pg_db = config.get("db", "tests")
-    pg_version = config.get("version", 14.11)
+    pg_version = config.get("version", 16.1)
 
     janitor = DatabaseJanitor(
         user=pg_user,
