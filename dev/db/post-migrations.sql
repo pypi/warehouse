3e66478340579b568985880242ab8f82bc46829c
Ee Durbin <ewdurbin@gmail.com>
2024-07-10 16:22:13 -0400

    add a known TOTP secret to select user accounts

--- dev/db/post-migrations.sql
+++ dev/db/post-migrations.sql
@@ -1,2 +1,5 @@
 -- Set password to 'password' hashed with argon
 UPDATE users SET password = '$argon2id$v=19$m=1024,t=6,p=6$EiLE2Nsbo9S6N+acs/beGw$ccyZDCZstr1/+Y/1s3BVZHOJaqfBroT0JCieHug281c';
+
+-- Set TOTP secret to IU7UP3EMIPI7EBPQUUSEHEJUFNBIWOYG for select users
+UPDATE users SET totp_secret = '\x453f47ec8c43d1f205f0a5244391342b428b3b06' WHERE username IN ('ewdurbin', 'di', 'dstufft', 'miketheman');
