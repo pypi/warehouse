b8959cbdfe88106e4de24f9ad6b5a2f8050caeb3
William Woodruff <william@trailofbits.com>
2024-07-22 11:30:24 -0400

    tests/functional: assert R_OK/W_OK for XDG dirs
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- tests/functional/test_environment.py
+++ tests/functional/test_environment.py
@@ -16,11 +16,11 @@ import platformdirs
 
 def test_xdg_environment():
     # Backstop checks for Warehouse's (Dockerfile-configured) environment.
+    # NOTE: We don't check the exact paths here, because they vary between
+    # environments: they're in `$HOME` in local and CI builds, and in `/tmp`
+    # when deployed.
     user_data = platformdirs.user_data_dir()
     user_cache = platformdirs.user_cache_dir()
-
-    assert user_data == "/tmp/share"
-    assert user_cache == "/tmp/cache"
 
     assert os.access(user_data, os.R_OK | os.W_OK)
     assert os.access(user_cache, os.R_OK | os.W_OK)
