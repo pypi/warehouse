10c0a90d674b052f8537047e40076414fdcbfb16
William Woodruff <william@trailofbits.com>
2024-07-22 11:45:52 -0400

    tests/functional: ensure_exists for platformdirs
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- tests/functional/test_environment.py
+++ tests/functional/test_environment.py
@@ -19,8 +19,8 @@ def test_xdg_environment():
     # NOTE: We don't check the exact paths here, because they vary between
     # environments: they're in `$HOME` in local and CI builds, and in `/tmp`
     # when deployed.
-    user_data = platformdirs.user_data_dir()
-    user_cache = platformdirs.user_cache_dir()
+    user_data = platformdirs.user_data_dir(ensure_exists=True)
+    user_cache = platformdirs.user_cache_dir(ensure_exists=True)
 
     assert os.access(user_data, os.R_OK | os.W_OK)
     assert os.access(user_cache, os.R_OK | os.W_OK)
