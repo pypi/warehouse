1bff60afad2ed758605ed5b1d5e3e61d85e01f88
Mike Fiedler <miketheman@gmail.com>
2024-08-07 15:43:04 -0400

    test: update to allow jwk decoding
    As updated in https://github.com/jpadilla/pyjwt/pull/886
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- tests/unit/oidc/test_services.py
+++ tests/unit/oidc/test_services.py
@@ -972,15 +972,14 @@ class TestPyJWTBackstop:
 
         assert decoded == {"foo": "bar"}
 
-    def test_decodes_token_typeerror_on_pyjwk(self):
+    def test_decodes_token_pyjwk(self):
         privkey_jwk = PyJWK.from_json(algorithms.RSAAlgorithm.to_jwk(self._privkey))
         pubkey_jwk = PyJWK.from_json(algorithms.RSAAlgorithm.to_jwk(self._pubkey))
 
         token = jwt.encode({"foo": "bar"}, privkey_jwk.key, algorithm="RS256")
+        decoded = jwt.decode(token, pubkey_jwk, algorithms=["RS256"])
 
-        # Passing a `PyJWK` directly into `jwt.decode` does not work.
-        with pytest.raises(TypeError, match=r"Expecting a PEM-formatted key\."):
-            jwt.decode(token, pubkey_jwk, algorithms=["RS256"])
+        assert decoded == {"foo": "bar"}
 
     def test_decode_strict_aud(self):
         token = jwt.encode(
