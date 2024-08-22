e8195e40986d63f9b72926a67eb6acfd43b36932
Dustin Ingram <di@users.noreply.github.com>
2024-08-22 16:12:28 -0400

    Add some releases to the Simple API functional test (#16556)
    * Restructure functional test directory
    
    * Add some releases to the Simple API functional test
    
    * Update tests/functional/api/test_simple.py
    
    Co-authored-by: Mike Fiedler <miketheman@gmail.com>
    
    * Use HTTPStatus everywhere instead of integers
    
    ---------
    
    Co-authored-by: Mike Fiedler <miketheman@gmail.com>

--- tests/functional/forklift/test_legacy.py
+++ tests/functional/forklift/test_legacy.py
@@ -10,12 +10,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from http import HTTPStatus
+
 import pytest
+
+
+def test_incorrect_post_redirect(webtest):
+    """
+    Per issue #8104, we should issue an HTTP-308 for a POST
+    in /legacy and point the user to the correct endpoint,
+    /legacy/
+
+    See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/308
+    """
+    resp = webtest.post("/legacy", status=HTTPStatus.PERMANENT_REDIRECT)
+    assert resp.status == (
+        "308 An upload was attempted to /legacy but the expected upload URL is "
+        "/legacy/ (with a trailing slash)"
+    )
+
+    assert "location" in resp.headers
+    assert resp.headers["location"] == "http://localhost/legacy/"
 
 
 @pytest.mark.parametrize("action", ["submit", "submit_pkg_info"])
 def test_removed_upload_apis(webtest, action):
-    resp = webtest.post(f"/legacy/?:action={action}", status=410)
+    resp = webtest.post(f"/legacy/?:action={action}", status=HTTPStatus.GONE)
     assert resp.status == (
         "410 Project pre-registration is no longer required or supported, "
         "upload your files instead."
@@ -23,13 +43,8 @@ def test_removed_upload_apis(webtest, action):
 
 
 def test_remove_doc_upload(webtest):
-    resp = webtest.post("/legacy/?:action=doc_upload", status=410)
+    resp = webtest.post("/legacy/?:action=doc_upload", status=HTTPStatus.GONE)
     assert resp.status == (
         "410 Uploading documentation is no longer supported, we recommend "
         "using https://readthedocs.org/."
     )
-
-
-def test_doap(webtest):
-    resp = webtest.get("/pypi?:action=doap&name=foo&version=1.0", status=410)
-    assert resp.status == "410 DOAP is no longer supported."
