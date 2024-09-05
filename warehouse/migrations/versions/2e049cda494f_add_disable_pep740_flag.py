4965e79495776dff1ffd5886446d492cc45b87df
Alexis <alexis.challande@trailofbits.com>
2024-09-05 17:17:29 +0200

    Add a new flag to disable PEP 740 support.

---
+++ warehouse/migrations/versions/2e049cda494f_add_disable_pep740_flag.py
@@ -0,0 +1,44 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+add DISABLE_PEP740 flag
+
+Revision ID: 2e049cda494f
+Revises: dcf1e3986782
+Create Date: 2024-09-05 15:15:58.703955
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+revision = "2e049cda494f"
+down_revision = "dcf1e3986782"
+
+
+def upgrade():
+    op.execute(
+        """
+        INSERT INTO admin_flags(id, description, enabled, notify)
+        VALUES (
+            'disable-pep740-support',
+            'Disable PEP 740 support.',
+            TRUE,
+            FALSE
+        )
+    """
+    )
+
+
+def downgrade():
+    op.drop_table("provenance")
+    op.execute("DELETE FROM admin_flags WHERE id = 'disable-pep740-support'")
