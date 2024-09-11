cc0b96def5fdc33f520802d100f3ed02021bc1ac
William Woodruff <william@trailofbits.com>
2024-09-11 10:24:50 -0400

    warehouse: PEP 740 models (#16625)
    * warehouse: PEP 740 models
    
    This is a breakout from https://github.com/pypi/warehouse/pull/16624,
    to reduce the complexity/headache of zippered reverts.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * tests: AttestationFactory helper
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * tests: attestation coverage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * switch to a Provenance model
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * provenance model carries a digest
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * remove unused factory
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * use SHA-256 for provenance digest
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * mark provenance column as deferred
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * Add a ProvenanceFactory object and a simple test.
    
    * Revert "Add a ProvenanceFactory object and a simple test."
    
    This reverts commit 34ec6611f5d716095dd4ea05a835f6e122ff5b4e.
    
    * Update warehouse/packaging/models.py
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    Co-authored-by: Alexis <alexis.challande@trailofbits.com>
    Co-authored-by: Dustin Ingram <di@users.noreply.github.com>

---
+++ warehouse/attestations/models.py
@@ -0,0 +1,52 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import typing
+
+from uuid import UUID
+
+from sqlalchemy import ForeignKey, orm
+from sqlalchemy.dialects.postgresql import CITEXT, JSONB
+from sqlalchemy.orm import Mapped, mapped_column
+
+from warehouse import db
+
+if typing.TYPE_CHECKING:
+    from warehouse.packaging.models import File
+
+
+class Provenance(db.Model):
+    """
+    A table for PEP 740 provenance objects.
+
+    Provenance objects contain one or more attestation objects.
+    These attestation objects are grouped into "bundles," each of which
+    contains one or more attestations along with the Trusted Publisher
+    identity that produced them.
+    """
+
+    __tablename__ = "provenance"
+
+    file_id: Mapped[UUID] = mapped_column(
+        ForeignKey("release_files.id", onupdate="CASCADE", ondelete="CASCADE"),
+    )
+    file: Mapped[File] = orm.relationship(back_populates="provenance")
+
+    # This JSONB has the structure of a PEP 740 provenance object.
+    provenance: Mapped[dict] = mapped_column(JSONB, nullable=False, deferred=True)
+
+    # The SHA-2/256 digest of the provenance object stored in this row.
+    # Postgres uses a compact binary representation under the hood and is
+    # unlikely to provide a permanently stable serialization, so this is the
+    # hash of the RFC 8785 serialization.
+    provenance_digest: Mapped[str] = mapped_column(CITEXT)
