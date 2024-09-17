a36ae299d043bb4a770d6fd0f4e73b8e99dd6461
Mike Fiedler <miketheman@gmail.com>
2024-09-17 12:07:10 -0400

    test(functional): log in, change password (#16724)
    * test: prerequisites for running functional webtests
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    * test: log in, change password
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    * refactor: replace static secret with factory one
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    * refactor: externalize password generation
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    * Update tests/functional/manage/test_views.py
    
    * tests(ci): tell gha to also run redis
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    * remove unneeded fixture
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    ---------
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -394,7 +394,11 @@ def app_config(database):
 @pytest.fixture(scope="session")
 def app_config_dbsession_from_env(database):
     nondefaults = {
-        "warehouse.db_create_session": lambda r: r.environ.get("warehouse.db_session")
+        "warehouse.db_create_session": lambda r: r.environ.get("warehouse.db_session"),
+        "breached_passwords.backend": "warehouse.accounts.services.NullPasswordBreachedService",  # noqa: E501
+        "token.two_factor.secret": "insecure token",
+        # A running redis service is required for functional web sessions
+        "sessions.url": "redis://redis:0/",
     }
 
     return get_app_config(database, nondefaults)
@@ -697,7 +701,7 @@ class _TestApp(_webtest.TestApp):
 
 
 @pytest.fixture
-def webtest(app_config_dbsession_from_env):
+def webtest(app_config_dbsession_from_env, remote_addr):
     """
     This fixture yields a test app with an alternative Pyramid configuration,
     injecting the database session and transaction manager into the app.
@@ -727,6 +731,7 @@ def webtest(app_config_dbsession_from_env):
                 "warehouse.db_session": _db_session,
                 "tm.active": True,  # disable pyramid_tm
                 "tm.manager": tm,  # pass in our own tm for the app to use
+                "REMOTE_ADDR": remote_addr,  # set the same address for all requests
             },
         )
         yield testapp
