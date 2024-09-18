69cffd59b07f96c7fee31e556e48f6798ef774fc
Ee Durbin <ewdurbin@gmail.com>
2024-09-18 14:35:33 -0400

    Make migration to redis as a broker possible.
    Temporarily hard-codes a BROKER_URL in a separate worker process.
    
    If not present, REDIS_URL is used for broker.

--- warehouse/tasks.py
+++ warehouse/tasks.py
@@ -195,7 +195,10 @@ def includeme(config):
 
     broker_transport_options = {}
 
-    broker_url = s["celery.broker_url"]
+    broker_url = s.get("celery.broker_url")
+    if broker_url is None:
+        broker_url = s["celery.broker_redis_url"]
+
     if broker_url.startswith("sqs://"):
         parsed_url = parse_url(broker_url)
         parsed_query = urllib.parse.parse_qs(parsed_url.query)
