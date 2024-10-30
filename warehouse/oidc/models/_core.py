7dceb20ec60bee97b71465c75cc2bb48d3e3cad6
William Woodruff <william@trailofbits.com>
2024-10-30 16:47:14 -0400

    models/_core: clarify verify_url_from_reference logic
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- warehouse/oidc/models/_core.py
+++ warehouse/oidc/models/_core.py
@@ -17,7 +17,6 @@ from typing import TYPE_CHECKING, Any, TypedDict, TypeVar, Unpack
 
 import rfc3986
 import sentry_sdk
-
 from sigstore.verify.policy import VerificationPolicy
 from sqlalchemy import ForeignKey, String, orm
 from sqlalchemy.dialects.postgresql import UUID
@@ -110,29 +109,36 @@ def verify_url_from_reference(*, reference_url: str, url: str) -> bool:
         - the same scheme
         - the same authority
 
-    Finally, that the URL is a sub-path of the reference
+    Finally, that the URL is a sub-path of the reference.
     """
     reference_uri = rfc3986.api.uri_reference(reference_url).normalize()
     user_uri = rfc3986.api.uri_reference(url).normalize()
 
+    # Fail fast if the URLs' schemes and authorities don't match.
+    if (
+        reference_uri.scheme != user_uri.scheme
+        or reference_uri.authority != user_uri.authority
+    ):
+        return False
+
+    # If the URL being verified doesn't have a path but the reference URL
+    # does, then it can't possibly be a subpath of the reference URL.
     if not user_uri.path and reference_uri.path:
         return False
 
-    # A reference path can be a prefix of a user path only if it ends
-    # with a forward slash ("/"). E.g: `my/path` is a prefix of both
-    # `/my/path/user` and `my/path_user`, but only the first one should
-    # pass verification.
-    reference_path_prefix = (
-        "" if reference_uri.path is None else reference_uri.path.rstrip("/") + "/"
-    )
-    is_subpath = reference_uri.path == user_uri.path or user_uri.path.startswith(
-        reference_path_prefix
-    )
-    return (
-        reference_uri.scheme == user_uri.scheme
-        and reference_uri.authority == user_uri.authority
-        and is_subpath
-    )
+    if reference_uri.path:
+        # If the reference URL has a path, then the user URL's path should
+        # be exactly equivalent or a subpath.
+        # The reference URL's path is normalized with a final `/` to ensure
+        # that `/foo_bar` isn't treated as a subpath of `/foo`.
+        reference_path_prefix = reference_uri.path.rstrip("/") + "/"
+        return reference_uri.path == user_uri.path or user_uri.path.startswith(
+            reference_path_prefix
+        )
+    else:
+        # If the reference URL has no path, the user URL's path can be anything
+        # (since the URL covers the entire domain).
+        return True
 
 
 class OIDCPublisherProjectAssociation(db.Model):
