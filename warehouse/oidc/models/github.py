77ec5bba797bf3552788c3aaecf6d42af5f39bab
dm <alexis.challande@trailofbits.com>
2024-11-14 16:45:08 +0100

    Ignore case when verifying GitLab/GitHub URLs (#16899)
    * Fixes #16666.
    
    * Also consider pages on github.io .
    
    * Fix test name
    
    * Remove global variable for OIDCMixin.
    
    * Update warehouse/oidc/models/gitlab.py
    
    Simplify code.
    
    Co-authored-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>
    
    * Add missing test
    
    * Update warehouse/oidc/models/gitlab.py
    
    Co-authored-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>
    
    * Fix linting issue
    
    * Fix linting issues
    
    ---------
    
    Co-authored-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>
    Co-authored-by: Dustin Ingram <di@users.noreply.github.com>

--- warehouse/oidc/models/github.py
+++ warehouse/oidc/models/github.py
@@ -361,13 +361,24 @@ class GitHubPublisher(GitHubPublisherMixin, OIDCPublisher):
         The suffix `.git` in repo URLs is ignored, since `github.com/org/repo.git`
         always redirects to `github.com/org/repo`. This does not apply to subpaths,
         like `github.com/org/repo.git/issues`, which do not redirect to the correct URL.
-        """
-        url_for_generic_check = url.removesuffix("/").removesuffix(".git")
 
+        GitHub uses case-insensitive owner/repo slugs - so we perform a case-insensitive
+        comparison.
+        """
+        docs_url = (
+            f"https://{self.repository_owner}.github.io/{self.repository_name}".lower()
+        )
+
+        normalized_url_prefixes = (self.publisher_base_url.lower(), docs_url)
+        for prefix in normalized_url_prefixes:
+            if url.lower().startswith(prefix):
+                url = prefix + url[len(prefix) :]
+                break
+
+        url_for_generic_check = url.removesuffix("/").removesuffix(".git")
         if super().verify_url(url_for_generic_check):
             return True
 
-        docs_url = f"https://{self.repository_owner}.github.io/{self.repository_name}"
         docs_uri = rfc3986.api.uri_reference(docs_url).normalize()
         user_uri = rfc3986.api.uri_reference(url).normalize()
 
