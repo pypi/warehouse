f9ef3516f4e8290e0334254bcfb8057a8c79fd35
Dustin Ingram <di@users.noreply.github.com>
2024-12-02 11:20:52 -0500

    Update warehouse/forklift/metadata.py

--- warehouse/forklift/metadata.py
+++ warehouse/forklift/metadata.py
@@ -34,7 +34,6 @@ from webob.multidict import MultiDict
 from warehouse.utils import http
 
 SUPPORTED_METADATA_VERSIONS = {"1.0", "1.1", "1.2", "2.1", "2.2", "2.3", "2.4"}
-assert SUPPORTED_METADATA_VERSIONS.issubset(_VALID_METADATA_VERSIONS)
 
 # Mapping of fields on a Metadata instance to any limits on the length of that
 # field. Fields without a limit will naturally be unlimited in length.
