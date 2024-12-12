d7b266faaf4f1debdf27983640d9b722957d99c4
Dustin Ingram <di@users.noreply.github.com>
2024-12-12 10:49:02 -0500

    Serialize a DisclosureOrigin to/from dict for task (#17276)

--- tests/unit/integration/secrets/test_tasks.py
+++ tests/unit/integration/secrets/test_tasks.py
@@ -15,24 +15,23 @@ import pretend
 from warehouse.integrations.secrets import tasks, utils
 
 
-def test_analyze_disclosure_task(monkeypatch):
+def test_analyze_disclosure_task(monkeypatch, someorigin):
     analyze_disclosure = pretend.call_recorder(lambda *a, **k: None)
     monkeypatch.setattr(utils, "analyze_disclosure", analyze_disclosure)
 
     request = pretend.stub()
     disclosure_record = pretend.stub()
-    origin = pretend.stub()
 
     tasks.analyze_disclosure_task(
         request=request,
         disclosure_record=disclosure_record,
-        origin=origin,
+        origin=someorigin.to_dict(),
     )
 
     assert analyze_disclosure.calls == [
         pretend.call(
             request=request,
             disclosure_record=disclosure_record,
-            origin=origin,
+            origin=someorigin,
         )
     ]
