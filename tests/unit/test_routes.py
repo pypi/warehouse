c8b88370b29553b5e0ff8aa05e9cff07e2f5d1f7
Robin5605 <robinjefferson123@gmail.com>
2025-01-04 19:24:46 -0600

    Add route tests

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -623,6 +623,18 @@ def test_routes(warehouse):
             factory="warehouse.legacy.api.json.release_factory",
             domain=warehouse,
         ),
+        pretend.call(
+            "legacy.api.json.user",
+            "/user/{username}/json",
+            factory="warehouse.legacy.api.json.user_factory",
+            domain=warehouse,
+        ),
+        pretend.call(
+            "legacy.api.json.user_slash",
+            "/user/{username}/json/",
+            factory="warehouse.legacy.api.json.user_factory",
+            domain=warehouse,
+        ),
         pretend.call("legacy.docs", docs_route_url),
     ]
 
