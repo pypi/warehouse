pypi/warehouse

commit 4775901c02d759ce2391083d8e5dfed0ca0feb7a

Improve error messages when a pending Trusted Publisher's project name already exists (#17405)
* Improve error messages when creating projects and publishers

This change improves the error messages when uploading a new project
or creating a pending Trusted Publisher, when the new project's name
already exists or is too similar to an existing project.

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

* Fix test error

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

* Fix name checking

Fix the check for projects with too-similar names when
there is more than one existing project with the same
ultranormalized name.

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

* Remove similar name from error messages

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

* Move errors back to interfaces.py

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

* De-parametrize tests

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

* Use exceptions instead of errors

Also change the TooSimilar error to store the conflicting
project name instead of a reference to the entire project.

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

* Address review comments

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

---------

Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

warehouse/oidc/forms/_core.py
@@ -13,7 +13,13 @@ import markupsafe
 import wtforms
 
 from warehouse.i18n import localize as _
-from warehouse.packaging.interfaces import ProjectNameUnavailableReason
+from warehouse.packaging.interfaces import (
+    ProjectNameUnavailableExistingError,
+    ProjectNameUnavailableInvalidError,
+    ProjectNameUnavailableProhibitedError,
+    ProjectNameUnavailableSimilarError,
+    ProjectNameUnavailableStdlibError,
+)
 from warehouse.utils.project import PROJECT_NAME_RE
 
 
@@ -31,38 +37,48 @@ class PendingPublisherMixin:
         project_name = field.data
 
         match self._check_project_name(project_name):
-            case ProjectNameUnavailableReason.Invalid:
+            case ProjectNameUnavailableInvalidError():
                 raise wtforms.validators.ValidationError(_("Invalid project name"))
-            case ProjectNameUnavailableReason.AlreadyExists:
-                url_params = {name: value for name, value in self.data.items() if value}
-                url_params["provider"] = {self.provider}
-                url = self._route_url(
-                    "manage.project.settings.publishing",
-                    project_name=project_name,
-                    _query=url_params,
-                )
+            case ProjectNameUnavailableExistingError(existing_project=existing_project):
+                # If the user owns the existing project, the error message includes a
+                # link to the project settings that the user can modify.
+                if self._user in existing_project.owners:
+                    url_params = {
+                        name: value for name, value in self.data.items() if value
+                    }
+                    url_params["provider"] = {self.provider}
+                    url = self._route_url(
+                        "manage.project.settings.publishing",
+                        project_name=project_name,
+                        _query=url_params,
+                    )
 
-                # We mark the error message as safe, so that the HTML hyperlink is
-                # not escaped by Jinja
-                raise wtforms.validators.ValidationError(
-                    markupsafe.Markup(
-                        _(
-                            "This project already exists: use the project's publishing"
-                            " settings <a href='${url}'>here</a> to create a Trusted"
-                            " Publisher for it.",
-                            mapping={"url": url},
+                    # We mark the error message as safe, so that the HTML hyperlink is
+                    # not escaped by Jinja
+                    raise wtforms.validators.ValidationError(
+                        markupsafe.Markup(
+                            _(
+                                "This project already exists: use the project's "
+                                "publishing settings <a href='${url}'>here</a> to "
+                                "create a Trusted Publisher for it.",
+                                mapping={"url": url},
+                            )
                         )
                     )
-                )
-            case ProjectNameUnavailableReason.Prohibited:
+                else:
+                    raise wtforms.validators.ValidationError(
+                        _("This project already exists.")
+                    )
+
+            case ProjectNameUnavailableProhibitedError():
                 raise wtforms.validators.ValidationError(
                     _("This project name isn't allowed")
                 )
-            case ProjectNameUnavailableReason.TooSimilar:
+            case ProjectNameUnavailableSimilarError():
                 raise wtforms.validators.ValidationError(
                     _("This project name is too similar to an existing project")
                 )
-            case ProjectNameUnavailableReason.Stdlib:
+            case ProjectNameUnavailableStdlibError():
                 raise wtforms.validators.ValidationError(
                     _(
                         "This project name isn't allowed (conflict with the Python"

tests/unit/oidc/forms/test_activestate.py
@@ -19,7 +19,7 @@ from requests import ConnectionError, HTTPError, Timeout
 from webob.multidict import MultiDict
 
 from warehouse.oidc.forms import activestate
-from warehouse.packaging.interfaces import ProjectNameUnavailableReason
+from warehouse.packaging.interfaces import ProjectNameUnavailableExistingError
 
 fake_username = "some-username"
 fake_org_name = "some-org"
@@ -50,6 +50,7 @@ class TestPendingActiveStatePublisherForm:
             MultiDict(data),
             route_url=route_url,
             check_project_name=check_project_name,
+            user=pretend.stub(),
         )
 
         # Test built-in validations
@@ -61,20 +62,28 @@ class TestPendingActiveStatePublisherForm:
         assert form._route_url == route_url
         assert form.validate()
 
-    def test_validate_project_name_already_in_use(self, pyramid_config):
+    def test_validate_project_name_already_in_use_owner(self, pyramid_config):
         route_url = pretend.call_recorder(lambda *args, **kwargs: "")
+        user = pretend.stub()
+        owners = [user]
 
         def check_project_name(name):
-            return ProjectNameUnavailableReason.AlreadyExists
+            return ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            )
 
         form = activestate.PendingActiveStatePublisherForm(
             route_url=route_url,
             check_project_name=check_project_name,
+            user=user,
         )
 
         field = pretend.stub(data="some-project")
         with pytest.raises(wtforms.validators.ValidationError):
             form.validate_project_name(field)
+
+        # The project settings URL is only shown in the error message if
+        # the user is the owner of the project
         assert route_url.calls == [
             pretend.call(
                 "manage.project.settings.publishing",
@@ -82,6 +91,28 @@ class TestPendingActiveStatePublisherForm:
                 _query={"provider": {"activestate"}},
             )
         ]
+
+    def test_validate_project_name_already_in_use_not_owner(self, pyramid_config):
+        route_url = pretend.call_recorder(lambda *args, **kwargs: "")
+        user = pretend.stub()
+        owners = []
+
+        def check_project_name(name):
+            return ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            )
+
+        form = activestate.PendingActiveStatePublisherForm(
+            route_url=route_url,
+            check_project_name=check_project_name,
+            user=user,
+        )
+
+        field = pretend.stub(data="some-project")
+        with pytest.raises(wtforms.validators.ValidationError):
+            form.validate_project_name(field)
+
+        assert route_url.calls == []
 
 
 class TestActiveStatePublisherForm:

tests/unit/oidc/forms/test_github.py
@@ -18,12 +18,19 @@ from requests import ConnectionError, HTTPError, Timeout
 from webob.multidict import MultiDict
 
 from warehouse.oidc.forms import github
-from warehouse.packaging.interfaces import ProjectNameUnavailableReason
+from warehouse.packaging.interfaces import (
+    ProjectNameUnavailableExistingError,
+    ProjectNameUnavailableInvalidError,
+    ProjectNameUnavailableProhibitedError,
+    ProjectNameUnavailableSimilarError,
+    ProjectNameUnavailableStdlibError,
+)
 
 
 class TestPendingGitHubPublisherForm:
     def test_validate(self, monkeypatch):
         route_url = pretend.stub()
+        user = pretend.stub()
 
         def check_project_name(name):
             return None  # Name is available.
@@ -41,6 +48,7 @@ class TestPendingGitHubPublisherForm:
             api_token=pretend.stub(),
             route_url=route_url,
             check_project_name=check_project_name,
+            user=user,
         )
 
         # We're testing only the basic validation here.
@@ -49,20 +57,29 @@ class TestPendingGitHubPublisherForm:
 
         assert form._check_project_name == check_project_name
         assert form._route_url == route_url
+        assert form._user == user
         assert form.validate()
 
-    def test_validate_project_name_already_in_use(self, pyramid_config):
+    def test_validate_project_name_already_in_use_owner(self, pyramid_config):
         route_url = pretend.call_recorder(lambda *args, **kwargs: "")
+        user = pretend.stub()
+        owners = [user]
 
         form = github.PendingGitHubPublisherForm(
             api_token="fake-token",
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableReason.AlreadyExists,
+            check_project_name=lambda name: ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            ),
+            user=user,
         )
 
         field = pretend.stub(data="some-project")
         with pytest.raises(wtforms.validators.ValidationError):
             form.validate_project_name(field)
+
+        # The project settings URL is only shown in the error message if
+        # the user is the owner of the project
         assert route_url.calls == [
             pretend.call(
                 "manage.project.settings.publishing",
@@ -71,12 +88,42 @@ class TestPendingGitHubPublisherForm:
             )
         ]
 
-    @pytest.mark.parametrize("reason", list(ProjectNameUnavailableReason))
+    def test_validate_project_name_already_in_use_not_owner(self, pyramid_config):
+        route_url = pretend.call_recorder(lambda *args, **kwargs: "")
+        user = pretend.stub()
+        owners = []
+
+        form = github.PendingGitHubPublisherForm(
+            api_token="fake-token",
+            route_url=route_url,
+            check_project_name=lambda name: ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            ),
+            user=user,
+        )
+
+        field = pretend.stub(data="some-project")
+        with pytest.raises(wtforms.validators.ValidationError):
+            form.validate_project_name(field)
+
+        assert route_url.calls == []
+
+    @pytest.mark.parametrize(
+        "reason",
+        [
+            ProjectNameUnavailableExistingError(pretend.stub(owners=[pretend.stub()])),
+            ProjectNameUnavailableInvalidError(),
+            ProjectNameUnavailableStdlibError(),
+            ProjectNameUnavailableProhibitedError(),
+            ProjectNameUnavailableSimilarError(similar_project_name="pkg_name"),
+        ],
+    )
     def test_validate_project_name_unavailable(self, reason, pyramid_config):
         form = github.PendingGitHubPublisherForm(
             api_token="fake-token",
             route_url=pretend.call_recorder(lambda *args, **kwargs: ""),
             check_project_name=lambda name: reason,
+            user=pretend.stub(),
         )
 
         field = pretend.stub(data="some-project")

tests/unit/oidc/forms/test_gitlab.py
@@ -17,12 +17,13 @@ import wtforms
 from webob.multidict import MultiDict
 
 from warehouse.oidc.forms import gitlab
-from warehouse.packaging.interfaces import ProjectNameUnavailableReason
+from warehouse.packaging.interfaces import ProjectNameUnavailableExistingError
 
 
 class TestPendingGitLabPublisherForm:
     def test_validate(self, monkeypatch):
         route_url = pretend.stub()
+        user = pretend.stub()
 
         def check_project_name(name):
             return None  # Name is available.
@@ -36,25 +37,37 @@ class TestPendingGitLabPublisherForm:
             }
         )
         form = gitlab.PendingGitLabPublisherForm(
-            MultiDict(data), route_url=route_url, check_project_name=check_project_name
+            MultiDict(data),
+            route_url=route_url,
+            check_project_name=check_project_name,
+            user=user,
         )
 
         assert form._route_url == route_url
         assert form._check_project_name == check_project_name
+        assert form._user == user
         # We're testing only the basic validation here.
         assert form.validate()
 
-    def test_validate_project_name_already_in_use(self, pyramid_config):
+    def test_validate_project_name_already_in_use_owner(self, pyramid_config):
+        user = pretend.stub()
+        owners = [user]
         route_url = pretend.call_recorder(lambda *args, **kwargs: "my_url")
 
         form = gitlab.PendingGitLabPublisherForm(
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableReason.AlreadyExists,
+            check_project_name=lambda name: ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            ),
+            user=user,
         )
 
         field = pretend.stub(data="some-project")
         with pytest.raises(wtforms.validators.ValidationError):
             form.validate_project_name(field)
+
+        # The project settings URL is only shown in the error message if
+        # the user is the owner of the project
         assert route_url.calls == [
             pretend.call(
                 "manage.project.settings.publishing",
@@ -62,6 +75,25 @@ class TestPendingGitLabPublisherForm:
                 _query={"provider": {"gitlab"}},
             )
         ]
+
+    def test_validate_project_name_already_in_use_not_owner(self, pyramid_config):
+        user = pretend.stub()
+        owners = []
+        route_url = pretend.call_recorder(lambda *args, **kwargs: "my_url")
+
+        form = gitlab.PendingGitLabPublisherForm(
+            route_url=route_url,
+            check_project_name=lambda name: ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            ),
+            user=user,
+        )
+
+        field = pretend.stub(data="some-project")
+        with pytest.raises(wtforms.validators.ValidationError):
+            form.validate_project_name(field)
+
+        assert route_url.calls == []
 
 
 class TestGitLabPublisherForm:
@@ -138,7 +170,6 @@ class TestGitLabPublisherForm:
         ["invalid.git", "invalid.atom", "invalid--project"],
     )
     def test_reserved_project_names(self, project_name):
-
         data = MultiDict(
             {
                 "namespace": "some",
@@ -160,7 +191,6 @@ class TestGitLabPublisherForm:
         ],
     )
     def test_reserved_organization_names(self, namespace):
-
         data = MultiDict(
             {
                 "namespace": namespace,

tests/unit/oidc/forms/test_google.py
@@ -17,12 +17,13 @@ import wtforms
 from webob.multidict import MultiDict
 
 from warehouse.oidc.forms import google
-from warehouse.packaging.interfaces import ProjectNameUnavailableReason
+from warehouse.packaging.interfaces import ProjectNameUnavailableExistingError
 
 
 class TestPendingGooglePublisherForm:
     def test_validate(self, monkeypatch):
         route_url = pretend.stub()
+        user = pretend.stub()
 
         def check_project_name(name):
             return None  # Name is available.
@@ -35,23 +36,35 @@ class TestPendingGooglePublisherForm:
             }
         )
         form = google.PendingGooglePublisherForm(
-            MultiDict(data), route_url=route_url, check_project_name=check_project_name
+            MultiDict(data),
+            route_url=route_url,
+            check_project_name=check_project_name,
+            user=user,
         )
 
         assert form._check_project_name == check_project_name
         assert form._route_url == route_url
+        assert form._user == user
         assert form.validate()
 
-    def test_validate_project_name_already_in_use(self, pyramid_config):
+    def test_validate_project_name_already_in_use_owner(self, pyramid_config):
+        user = pretend.stub()
+        owners = [user]
         route_url = pretend.call_recorder(lambda *args, **kwargs: "my_url")
         form = google.PendingGooglePublisherForm(
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableReason.AlreadyExists,
+            check_project_name=lambda name: ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            ),
+            user=user,
         )
 
         field = pretend.stub(data="some-project")
         with pytest.raises(wtforms.validators.ValidationError):
             form.validate_project_name(field)
+
+        # The project settings URL is only shown in the error message if
+        # the user is the owner of the project
         assert route_url.calls == [
             pretend.call(
                 "manage.project.settings.publishing",
@@ -59,6 +72,24 @@ class TestPendingGooglePublisherForm:
                 _query={"provider": {"google"}},
             )
         ]
+
+    def test_validate_project_name_already_in_use_not_owner(self, pyramid_config):
+        user = pretend.stub()
+        owners = []
+        route_url = pretend.call_recorder(lambda *args, **kwargs: "my_url")
+        form = google.PendingGooglePublisherForm(
+            route_url=route_url,
+            check_project_name=lambda name: ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            ),
+            user=user,
+        )
+
+        field = pretend.stub(data="some-project")
+        with pytest.raises(wtforms.validators.ValidationError):
+            form.validate_project_name(field)
+
+        assert route_url.calls == []
 
 
 class TestGooglePublisherForm:

commit b2a286eb9fc9fd8a86f5c216128e0a3be21602d4

resolve new errors introduced in #17405
changing from return value to exception was not caught due to stubs.

it's unclear to me how the changes in #17405 were expected to work, or if they were intended to be surfaced in the UI.

warehouse/oidc/forms/_core.py
@@ -36,55 +36,54 @@ class PendingPublisherMixin:
     def validate_project_name(self, field):
         project_name = field.data
 
-        match self._check_project_name(project_name):
-            case ProjectNameUnavailableInvalidError():
-                raise wtforms.validators.ValidationError(_("Invalid project name"))
-            case ProjectNameUnavailableExistingError(existing_project=existing_project):
-                # If the user owns the existing project, the error message includes a
-                # link to the project settings that the user can modify.
-                if self._user in existing_project.owners:
-                    url_params = {
-                        name: value for name, value in self.data.items() if value
-                    }
-                    url_params["provider"] = {self.provider}
-                    url = self._route_url(
-                        "manage.project.settings.publishing",
-                        project_name=project_name,
-                        _query=url_params,
-                    )
+        try:
+            self._check_project_name(project_name)
+        except ProjectNameUnavailableInvalidError:
+            raise wtforms.validators.ValidationError(_("Invalid project name"))
+        except ProjectNameUnavailableExistingError as e:
+            # If the user owns the existing project, the error message includes a
+            # link to the project settings that the user can modify.
+            if self._user in e.existing_project.owners:
+                url_params = {name: value for name, value in self.data.items() if value}
+                url_params["provider"] = {self.provider}
+                url = self._route_url(
+                    "manage.project.settings.publishing",
+                    project_name=project_name,
+                    _query=url_params,
+                )
 
-                    # We mark the error message as safe, so that the HTML hyperlink is
-                    # not escaped by Jinja
-                    raise wtforms.validators.ValidationError(
-                        markupsafe.Markup(
-                            _(
-                                "This project already exists: use the project's "
-                                "publishing settings <a href='${url}'>here</a> to "
-                                "create a Trusted Publisher for it.",
-                                mapping={"url": url},
-                            )
+                # We mark the error message as safe, so that the HTML hyperlink is
+                # not escaped by Jinja
+                raise wtforms.validators.ValidationError(
+                    markupsafe.Markup(
+                        _(
+                            "This project already exists: use the project's "
+                            "publishing settings <a href='${url}'>here</a> to "
+                            "create a Trusted Publisher for it.",
+                            mapping={"url": url},
                         )
                     )
-                else:
-                    raise wtforms.validators.ValidationError(
-                        _("This project already exists.")
-                    )
+                )
+            else:
+                raise wtforms.validators.ValidationError(
+                    _("This project already exists.")
+                )
 
-            case ProjectNameUnavailableProhibitedError():
-                raise wtforms.validators.ValidationError(
-                    _("This project name isn't allowed")
+        except ProjectNameUnavailableProhibitedError:
+            raise wtforms.validators.ValidationError(
+                _("This project name isn't allowed")
+            )
+        except ProjectNameUnavailableSimilarError:
+            raise wtforms.validators.ValidationError(
+                _("This project name is too similar to an existing project")
+            )
+        except ProjectNameUnavailableStdlibError:
+            raise wtforms.validators.ValidationError(
+                _(
+                    "This project name isn't allowed (conflict with the Python"
+                    " standard library module name)"
                 )
-            case ProjectNameUnavailableSimilarError():
-                raise wtforms.validators.ValidationError(
-                    _("This project name is too similar to an existing project")
-                )
-            case ProjectNameUnavailableStdlibError():
-                raise wtforms.validators.ValidationError(
-                    _(
-                        "This project name isn't allowed (conflict with the Python"
-                        " standard library module name)"
-                    )
-                )
+            )
 
     @property
     def provider(self) -> str:  # pragma: no cover

tests/unit/oidc/forms/test_activestate.py
@@ -68,7 +68,7 @@ class TestPendingActiveStatePublisherForm:
         owners = [user]
 
         def check_project_name(name):
-            return ProjectNameUnavailableExistingError(
+            raise ProjectNameUnavailableExistingError(
                 existing_project=pretend.stub(owners=owners)
             )
 
@@ -98,7 +98,7 @@ class TestPendingActiveStatePublisherForm:
         owners = []
 
         def check_project_name(name):
-            return ProjectNameUnavailableExistingError(
+            raise ProjectNameUnavailableExistingError(
                 existing_project=pretend.stub(owners=owners)
             )
 

tests/unit/oidc/forms/test_github.py
@@ -65,12 +65,15 @@ class TestPendingGitHubPublisherForm:
         user = pretend.stub()
         owners = [user]
 
+        def check_project_name(name):
+            raise ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            )
+
         form = github.PendingGitHubPublisherForm(
             api_token="fake-token",
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableExistingError(
-                existing_project=pretend.stub(owners=owners)
-            ),
+            check_project_name=check_project_name,
             user=user,
         )
 
@@ -93,12 +96,15 @@ class TestPendingGitHubPublisherForm:
         user = pretend.stub()
         owners = []
 
+        def check_project_name(name):
+            raise ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            )
+
         form = github.PendingGitHubPublisherForm(
             api_token="fake-token",
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableExistingError(
-                existing_project=pretend.stub(owners=owners)
-            ),
+            check_project_name=check_project_name,
             user=user,
         )
 
@@ -119,10 +125,13 @@ class TestPendingGitHubPublisherForm:
         ],
     )
     def test_validate_project_name_unavailable(self, reason, pyramid_config):
+        def check_project_name(name):
+            raise reason
+
         form = github.PendingGitHubPublisherForm(
             api_token="fake-token",
             route_url=pretend.call_recorder(lambda *args, **kwargs: ""),
-            check_project_name=lambda name: reason,
+            check_project_name=check_project_name,
             user=pretend.stub(),
         )
 

tests/unit/oidc/forms/test_gitlab.py
@@ -54,11 +54,14 @@ class TestPendingGitLabPublisherForm:
         owners = [user]
         route_url = pretend.call_recorder(lambda *args, **kwargs: "my_url")
 
+        def check_project_name(name):
+            raise ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            )
+
         form = gitlab.PendingGitLabPublisherForm(
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableExistingError(
-                existing_project=pretend.stub(owners=owners)
-            ),
+            check_project_name=check_project_name,
             user=user,
         )
 
@@ -81,11 +84,14 @@ class TestPendingGitLabPublisherForm:
         owners = []
         route_url = pretend.call_recorder(lambda *args, **kwargs: "my_url")
 
+        def check_project_name(name):
+            raise ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            )
+
         form = gitlab.PendingGitLabPublisherForm(
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableExistingError(
-                existing_project=pretend.stub(owners=owners)
-            ),
+            check_project_name=check_project_name,
             user=user,
         )
 

tests/unit/oidc/forms/test_google.py
@@ -51,11 +51,15 @@ class TestPendingGooglePublisherForm:
         user = pretend.stub()
         owners = [user]
         route_url = pretend.call_recorder(lambda *args, **kwargs: "my_url")
+
+        def check_project_name(name):
+            raise ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            )
+
         form = google.PendingGooglePublisherForm(
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableExistingError(
-                existing_project=pretend.stub(owners=owners)
-            ),
+            check_project_name=check_project_name,
             user=user,
         )
 
@@ -77,11 +81,15 @@ class TestPendingGooglePublisherForm:
         user = pretend.stub()
         owners = []
         route_url = pretend.call_recorder(lambda *args, **kwargs: "my_url")
+
+        def check_project_name(name):
+            raise ProjectNameUnavailableExistingError(
+                existing_project=pretend.stub(owners=owners)
+            )
+
         form = google.PendingGooglePublisherForm(
             route_url=route_url,
-            check_project_name=lambda name: ProjectNameUnavailableExistingError(
-                existing_project=pretend.stub(owners=owners)
-            ),
+            check_project_name=check_project_name,
             user=user,
         )
 