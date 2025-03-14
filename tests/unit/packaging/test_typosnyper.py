617d4361dc609fa77f6fff2b58257bc095dffa0b
Dustin Ingram <di@users.noreply.github.com>
2025-03-14 15:29:38 +0000

    linting

--- tests/unit/packaging/test_typosnyper.py
+++ tests/unit/packaging/test_typosnyper.py
@@ -10,7 +10,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pretend
 import pytest
 
 from warehouse.packaging import typosnyper
