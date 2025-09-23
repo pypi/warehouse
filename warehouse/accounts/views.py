6fad4f706aa45feac4707ca2a57d5e14d29e64ef
Dustin Ingram <di@users.noreply.github.com>
2025-09-23 19:31:09 +0000

    Update error message

--- warehouse/accounts/views.py
+++ warehouse/accounts/views.py
@@ -1192,7 +1192,12 @@ def confirm_login(request):
         return _error(request._("Invalid login attempt."))
 
     if unique_login.ip_address != request.remote_addr:
-        return _error(request._("Device details didn't match, please try again"))
+        return _error(
+            request._(
+                "Device details didn't match, please try again from the device "
+                "you originally used to log in."
+            )
+        )
 
     unique_login.status = UniqueLoginStatus.CONFIRMED
 
