ed1fd3eee49fcaa9fbf575c365ce23856027381a
Dustin Ingram <di@users.noreply.github.com>
2025-10-20 16:45:56 -0400

    Normalize both sides of case-insensitive fields for GitLab publisher lookup query (#18905)
    * Add a failing test
    
    * Normalize both sides of the query

--- tests/unit/oidc/models/test_gitlab.py
+++ tests/unit/oidc/models/test_gitlab.py
@@ -73,19 +73,36 @@ class TestGitLabPublisher:
         ):
             gitlab.GitLabPublisher.lookup_by_claims(pretend.stub(), signed_claims)
 
-    def test_lookup_succeeds_with_mixed_case_project_path(self, db_request):
+    @pytest.mark.parametrize(
+        ("configured_namespace", "configured_project", "project_path"),
+        [
+            (
+                "Foo",
+                "Bar",
+                "foo/bar",
+            ),
+            (
+                "foo",
+                "bar",
+                "Foo/Bar",
+            ),
+        ],
+    )
+    def test_lookup_succeeds_with_mixed_case_project_path(
+        self, db_request, configured_namespace, configured_project, project_path
+    ):
         # Test that we find a matching publisher when the project_path claims match
         # even if the case is different.
         stored_publisher = GitLabPublisherFactory(
-            namespace="Foo",
-            project="Bar",
+            namespace=configured_namespace,
+            project=configured_project,
             workflow_filepath=".gitlab-ci.yml",
             environment="",
         )
 
         signed_claims = {
-            "project_path": "foo/bar",  # different case than stored publisher
-            "ci_config_ref_uri": ("gitlab.com/foo/bar//.gitlab-ci.yml@refs/heads/main"),
+            "project_path": project_path,
+            "ci_config_ref_uri": "gitlab.com/foo/bar//.gitlab-ci.yml@refs/heads/main",
             "environment": "some_environment",
         }
 
