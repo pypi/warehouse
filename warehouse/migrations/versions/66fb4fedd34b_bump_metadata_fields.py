9067294d6e6aac2124ada79fca8c94a709cafd3b
William Woodruff <william@astral.sh>
2026-01-23 14:53:40 -0500

    use ALTER TYPE
    Signed-off-by: William Woodruff <william@astral.sh>

--- warehouse/migrations/versions/66fb4fedd34b_bump_metadata_fields.py
+++ warehouse/migrations/versions/66fb4fedd34b_bump_metadata_fields.py
@@ -15,48 +15,11 @@ down_revision = "31ac9b5e1e8b"
 
 
 def upgrade():
-    op.sync_enum_values(
-        enum_schema="public",
-        enum_name="release_dynamic_fields",
-        new_values=[
-            "Platform",
-            "Supported-Platform",
-            "Summary",
-            "Description",
-            "Description-Content-Type",
-            "Keywords",
-            "Home-Page",
-            "Download-Url",
-            "Author",
-            "Author-Email",
-            "Maintainer",
-            "Maintainer-Email",
-            "License",
-            "License-Expression",
-            "License-File",
-            "Classifier",
-            "Requires-Dist",
-            "Requires-Python",
-            "Requires-External",
-            "Project-Url",
-            "Provides-Extra",
-            "Provides-Dist",
-            "Obsoletes-Dist",
-            "Requires",
-            "Provides",
-            "Obsoletes",
-            "Import-Name",
-            "Import-Namespace",
-        ],
-        affected_columns=[
-            TableReference(
-                table_schema="public",
-                table_name="releases",
-                column_name="dynamic",
-                column_type=ColumnType.ARRAY,
-            )
-        ],
-        enum_values_to_rename=[],
+    op.execute(
+        "ALTER TYPE public.release_dynamic_fields ADD VALUE IF NOT EXISTS 'Import-Name'"
+    )
+    op.execute(
+        "ALTER TYPE public.release_dynamic_fields ADD VALUE IF NOT EXISTS 'Import-Namespace'"
     )
 
 
