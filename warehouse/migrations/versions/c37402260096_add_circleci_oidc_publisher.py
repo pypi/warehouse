d7a0226f9b6d69330480072108193bea1914d8e0
meeech <4623+meeech@users.noreply.github.com>
2026-02-22 19:36:22 -0500

    update revises field

--- warehouse/migrations/versions/c37402260096_add_circleci_oidc_publisher.py
+++ warehouse/migrations/versions/c37402260096_add_circleci_oidc_publisher.py
@@ -3,7 +3,7 @@
 Add CircleCI OIDC publisher
 
 Revision ID: c37402260096
-Revises: ee66c00f12e6
+Revises: 28c1e0646708
 Create Date: 2026-01-27 02:31:38.265440
 """
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "c37402260096"
-down_revision = "ee66c00f12e6"
+down_revision = "28c1e0646708"
 
 # Note: It is VERY important to ensure that a migration does not lock for a
 #       long period of time and to ensure that each individual migration does
