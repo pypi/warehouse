27765fd4d70c1321d9fe66d05c94e824e1521313
Mike Fiedler <miketheman@gmail.com>
2026-03-09 12:45:35 -0400

    fix: correctly record macaroon deletion events
    The test mock and code were not using the correct embedded JSON
    structure, so project-scoped macaroon deletion events were being
    dropped.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- warehouse/manage/views/__init__.py
+++ warehouse/manage/views/__init__.py
@@ -1026,12 +1026,12 @@ class ProvisionMacaroonViews:
                 request=self.request,
                 additional={"macaroon_id": form.macaroon_id.data},
             )
-            if "projects" in macaroon.permissions_caveat:
+            if "projects" in macaroon.permissions_caveat.get("permissions", {}):
                 projects = [
                     project
                     for project in self.request.user.projects
                     if project.normalized_name
-                    in macaroon.permissions_caveat["projects"]
+                    in macaroon.permissions_caveat["permissions"]["projects"]
                 ]
                 for project in projects:
                     project.record_event(
