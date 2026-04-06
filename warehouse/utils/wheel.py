2bf6b6875423b0f3cf7dfb7b1e288d0997be7ef4
Hood Chatham <roberthoodchatham@gmail.com>
2026-04-06 15:57:22 -0700

    Fix lint

--- warehouse/utils/wheel.py
+++ warehouse/utils/wheel.py
@@ -57,7 +57,7 @@ _PLATFORMS = [
     (
         re.compile(r"^pyemscripten_(\d+)_(\d+)_wasm32$"),
         lambda m: f"PyEmscripten {m.group(1)}.{m.group(2)} wasm32",  # noqa: E501
-    )
+    ),
 ]
 
 _ARCHS = {
