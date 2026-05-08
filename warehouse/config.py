25e9db806a7e4934f3020079898985b8692b5d4d
Jacob Coffee <jacob@z7x.org>
2026-05-08 12:12:33 -0500

    register the setting for org projcreate rates

--- warehouse/config.py
+++ warehouse/config.py
@@ -586,6 +586,12 @@ def configure(settings=None):
     )
     maybe_set(
         settings,
+        "warehouse.packaging.project_create_org_ratelimit_string",
+        "PROJECT_CREATE_ORG_RATELIMIT_STRING",
+        default="20 per hour",
+    )
+    maybe_set(
+        settings,
         "warehouse.search.ratelimit_string",
         "SEARCH_RATELIMIT_STRING",
         default="5 per second",
