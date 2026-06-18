3b3bfbe45f57bdfc19da5dd9a0a0bc4907f2b227
Jacob Coffee <jacob@z7x.org>
2026-06-18 11:41:36 -0500

    no prtend

--- tests/unit/admin/views/test_includes.py
+++ tests/unit/admin/views/test_includes.py
@@ -1,9 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
-import pretend
-
 from warehouse.admin.views import includes
 
+from ....common.db.accounts import UserFactory
 from ....common.db.organizations import OrganizationFactory
 from ....common.db.packaging import ProjectFactory
 
@@ -19,9 +18,9 @@ def test_administer_project_include_returns_project(db_request):
     }
 
 
-def test_administer_user_include_returns_user():
-    user = pretend.stub()
-    assert includes.administer_user_include(user, pretend.stub()) == {"user": user}
+def test_administer_user_include_returns_user(db_request):
+    user = UserFactory.create()
+    assert includes.administer_user_include(user, db_request) == {"user": user}
 
 
 def test_administer_organization_include_returns_organization(db_request):
