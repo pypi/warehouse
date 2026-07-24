c7d9fb0bc8686589164a3546b6ba2c1abc6eb04c
Dustin Ingram <di@users.noreply.github.com>
2026-07-24 14:51:42 +0000

    Implement the inconsistent-provenance release provenance state

--- tests/unit/attestations/test_models.py
+++ tests/unit/attestations/test_models.py
@@ -1,9 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
+import pretend
 import pypi_attestations
 
 from tests.common.db.oidc import GitHubPublisherFactory
 from tests.common.db.packaging import FileFactory, ProvenanceFactory
+from warehouse.attestations.models import get_file_provenance_sources
 
 
 def test_provenance_as_model(db_request, integrity_service, dummy_attestation):
@@ -44,3 +46,50 @@ def test_provenance_factory_with_predicate_types(db_request):
         assert (
             claims["1.3.6.1.4.1.57264.1.12"] == "https://github.com/example-org/example"
         )
+
+
+def test_get_file_provenance_sources_none():
+    file = pretend.stub(provenance=None)
+    repos, workflows = get_file_provenance_sources(file)
+    assert repos == set()
+    assert workflows == set()
+
+
+def test_get_file_provenance_sources_github():
+    file = pretend.stub(
+        provenance=pretend.stub(
+            as_model=pretend.stub(
+                attestation_bundles=[
+                    pretend.stub(
+                        publisher=pretend.stub(
+                            repository="foo/bar", workflow="publish.yml"
+                        )
+                    )
+                ]
+            )
+        )
+    )
+    repos, workflows = get_file_provenance_sources(file)
+    assert repos == {"foo/bar"}
+    assert workflows == {"publish.yml"}
+
+
+def test_get_file_provenance_sources_missing_attrs():
+    file = pretend.stub(
+        provenance=pretend.stub(
+            as_model=pretend.stub(
+                attestation_bundles=[
+                    pretend.stub(
+                        publisher=pretend.stub(
+                            repository=None,
+                            workflow=None,
+                            workflow_filepath=None,
+                        )
+                    )
+                ]
+            )
+        )
+    )
+    repos, workflows = get_file_provenance_sources(file)
+    assert repos == set()
+    assert workflows == set()
