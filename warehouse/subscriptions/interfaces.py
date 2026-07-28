22dcf16e91a08484c97bafa255caf4da7877f7c9
Jacob Coffee <jacob@z7x.org>
2026-07-28 16:26:30 -0500

    docs

--- warehouse/subscriptions/interfaces.py
+++ warehouse/subscriptions/interfaces.py
@@ -21,8 +21,7 @@ class IGenericBillingService(Interface):
 
     def retrieve_subscription(subscription_id):
         """
-        Fetch the Subscription resource from the Billing API, or None if it
-        no longer exists
+        Fetch the Subscription resource from the Billing API
         """
 
     def create_customer(name, description):
