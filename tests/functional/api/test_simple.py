29eca51d88324c5d644dd634b27aeca67f666480
William Woodruff <william@yossarian.net>
2026-08-11 10:53:16 -0400

    Add snapshot tests for the HTML representation freeze
    Signed-off-by: William Woodruff <william@yossarian.net>

--- tests/functional/api/test_simple.py
+++ tests/functional/api/test_simple.py
@@ -11,6 +11,8 @@ from ...common.db.packaging import (
     ReleaseFactory,
 )
 
+from inline_snapshot import snapshot
+
 
 def test_simple_api_html(webtest):
     resp = webtest.get("/simple/", status=HTTPStatus.OK)
@@ -81,3 +83,62 @@ def test_simple_api_has_provenance(webtest):
             f"http://localhost/integrity/{file.release.project.normalized_name}/"
             f"{file.release.version}/{file.filename}/provenance"
         )
+
+
+def test_pep833_simple_api_base_html_frozen(webtest):
+    """
+    WARNING! PEP 833 freezes the HTML representation of the simple API;
+    this test backstops that freeze by ensuring that we don't accidentally
+    the HTML representation.
+
+    If you're *intentionally* changing the HTML representation, even
+    just the whitespace, make sure you have a good reason for doing do!
+    """
+
+    resp = webtest.get("/simple/", status=HTTPStatus.OK)
+
+    assert resp.text == snapshot("""\
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="pypi:repository-version" content="1.4">
+    <title>Simple index</title>
+  </head>
+  <body>
+</body>
+</html>\
+""")
+
+
+def test_pep833_simple_api_detail_html_frozen(webtest):
+    """
+    WARNING! PEP 833 freezes the HTML representation of the simple API;
+    this test backstops that freeze by ensuring that we don't accidentally
+    the HTML representation.
+
+    If you're *intentionally* changing the HTML representation, even
+    just the whitespace, make sure you have a good reason for doing do!
+    """
+
+    project = ProjectFactory.create(name="example")
+    release = ReleaseFactory.create(project=project, version="1.0.0")
+    FileFactory.create(
+        filename="example-1.0.0.tar.gz", release=release, packagetype="sdist"
+    )
+
+    resp = webtest.get(f"/simple/{project.normalized_name}/", status=HTTPStatus.OK)
+
+    assert resp.text == snapshot("""\
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="pypi:repository-version" content="1.4">
+<meta name="pypi:project-status" content="active">    <title>Links for example</title>
+  </head>
+  <body>
+    <h1>Links for example</h1>
+<a href="http://localhost:7000/#sha256=7e657eb56fc128fcfbbc8c6c613f4b67ae589e93871c96aeb502754bbf24987f" >example-1.0.0.tar.gz</a><br />
+</body>
+</html>
+<!--SERIAL 0-->\
+""")
