7093e26eb83468de8508dd8d583a4a19a623f85a
Jacob Coffee <jacob@z7x.org>
2026-08-20 12:47:00 -0500

    add base route for managing account security

--- warehouse/routes.py
+++ warehouse/routes.py
@@ -247,6 +247,9 @@ def includeme(config):
         "manage.account.publishing", "/manage/account/publishing/", domain=warehouse
     )
     config.add_route(
+        "manage.account.security", "/manage/account/security/", domain=warehouse
+    )
+    config.add_route(
         "manage.account.security-history",
         "/manage/account/security-history/",
         domain=warehouse,
