0113df9f5c1c3a797aa0e691ca7fd7735b9c8b99
Jacob Coffee <jacob@z7x.org>
2026-08-20 21:31:28 -0500

    update test for
    cleanup old stuff

--- tests/unit/manage/test_views.py
+++ tests/unit/manage/test_views.py
@@ -394,12 +394,7 @@ class TestManageAccount:
     )
     def test_default_response(self, monkeypatch, public_email, expected_public_email):
         breach_service = pretend.stub()
-        account_associations = pretend.stub()
-        user_service = pretend.stub(
-            get_account_associations=pretend.call_recorder(
-                lambda user_id: account_associations
-            )
-        )
+        user_service = pretend.stub()
         organization_service = pretend.stub()
         name = pretend.stub()
         user_id = pretend.stub()
@@ -431,11 +426,9 @@ class TestManageAccount:
             "add_email_form": add_email_obj,
             "active_projects": view.active_projects,
             "sole_organizations": view.sole_organizations,
-            "account_associations": account_associations,
         }
         assert view.request == request
         assert view.user_service == user_service
-        assert user_service.get_account_associations.calls == [pretend.call(user_id)]
         assert save_account_cls.calls == [
             pretend.call(
                 name=name,
@@ -1376,6 +1369,27 @@ class TestManageAccountSecurity:
         assert record_event.call_args_list == []
         assert db_request.db.flush.call_args_list == []
         assert db_request.db.refresh.call_args_list == []
+
+
+class TestManageAccountConnectedAccounts:
+    def test_manage_account_connected_accounts(self, db_request, mocker):
+        user = UserFactory.create()
+        db_request.user = user
+
+        associations = mocker.sentinel.associations
+        user_service = mocker.Mock(
+            get_account_associations=mocker.Mock(return_value=associations)
+        )
+        db_request.find_service = lambda iface, **kw: {IUserService: user_service}[
+            iface
+        ]
+
+        response = views.manage_account_connected_accounts(db_request)
+
+        assert response == {"account_associations": associations}
+        assert user_service.get_account_associations.call_args_list == [
+            mocker.call(user.id)
+        ]
 
 
 class TestManageAccountSecurityHistory:
