81294d3f8f880274fcb6bc23509f18cc128afbf3
Jacob Coffee <jacob@z7x.org>
2026-09-10 14:53:11 -0500

    Update warehouse/events/tags.py
    Co-authored-by: Ee Durbin <ewdurbin@gmail.com>

--- warehouse/events/tags.py
+++ warehouse/events/tags.py
@@ -89,7 +89,7 @@ class EventTag:
         RoleInvite = "account:role:invite"
         RoleRemove = "account:role:remove"
         RoleRevokeInvite = "account:role:revoke_invite"
-        SetProjectCreateRateLimit = "admin:account:set_project_create_ratelimit"
+        ProjectCreateRateLimitChange = "account:project_create_ratelimit:change"
         TeamRoleAdd = "account:team_role:add"
         TeamRoleRemove = "account:team_role:remove"
         TwoFactorDeviceRemembered = "account:two_factor:device_remembered"
